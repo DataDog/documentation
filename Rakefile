372158268245c48e363d67f915942f006f6c02d9
Matt Singleton <matt@datadoghq.com>
2011-12-12 15:12:46 -0500

    initial commit

---
+++ Rakefile
@@ -0,0 +1,6 @@
+begin
+  require 'nanoc3/tasks'
+rescue LoadError
+  require 'rubygems'
+  require 'nanoc3/tasks'
+end
---
+++ lib/default.rb
@@ -0,0 +1,40 @@
+# All files in the 'lib' directory will be loaded
+# before nanoc starts compiling.
+
+STATUS_CODES = {
+  200 => '200 OK',
+  201 => '201 Created',
+  204 => '204 No Content',
+  301 => '301 Moved Permanently',
+  304 => '304 Not Modified',
+  401 => '401 Unauthorized',
+  403 => '403 Forbidden',
+  404 => '404 Not Found',
+  409 => '409 Conflict',
+  422 => '422 Unprocessable Entity',
+  500 => '500 Server Error'
+}
+
+def request_headers(headers={})
+  lines = []
+  headers.each do |key, value|
+    lines << ["#{key}: #{value}"]
+  end
+  return "<pre class=\"http-headers\">#{lines.join("\n")}</pre>"
+end
+
+def response_headers(status, headers={})
+  lines = ["Status: #{STATUS_CODES[status]}"]
+  headers.each do |key, value|
+    lines << ["#{key}: #{value}"]
+  end
+  return "<pre class=\"http-headers\">#{lines.join("\n")}</pre>"
+end
+
+def http_body(json)
+  return "<pre class=\"http-body\"><code class=\"language-javascript\">#{json}</code></pre>"
+end
+
+def request(verb, uri)
+  return "<pre><span class=\"request-verb\">#{verb}</span> #{uri}</pre>"
+end
