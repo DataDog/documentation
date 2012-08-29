51b5c37b811fa75dc38885eaa7f455758b7d8b47
Matt Perpick <matthewperpick@gmail.com>
2012-08-29 14:04:20 -0400

    Adding stubbed out dashboard code.

---
+++ code_snippets/api-dashboard-get-response.sh
@@ -0,0 +1,23 @@
+{
+    "dash": {
+        "description": "created by mattp@datadoghq.com", 
+        "graphs": [
+            {
+                "definition": {
+                    "events": [], 
+                    "requests": [
+                        {
+                            "q": "avg:system.mem.free{*}"
+                        }
+                    ], 
+                    "viz": "timeseries"
+                }, 
+                "title": "Average Memory Free"
+            }
+        ], 
+        "id": 1585, 
+        "title": "Free Memory"
+    }, 
+    "resource": "/api/v1/dash/1585", 
+    "url": "/dash/dash/1585"
+}
