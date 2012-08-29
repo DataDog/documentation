014394c0f950110e6ee6dda06140688928cad09d
Matt Perpick <matthewperpick@gmail.com>
2012-08-29 16:45:38 -0400

    Updating api key and event key.

--- code_snippets/api-dashboard-create.py
+++ code_snippets/api-dashboard-create.py
@@ -1,8 +1,7 @@
 from dogapi import dog_http_api as api
 
-api.api_key = 'apikey_2'
-api.application_key = '01a39483d351f4f827571cb7091a39e82f86edcc'
-api.api_host = 'https://app.datad0g.com'
+api.api_key = '9775a026f1ca7d1c6c5af9d94d9595a4'
+api.application_key = '87ce4a24b5553d2e482ea8a8500e71b8ad4554ff'
 
 title = "My Dashboard"
 description = "An informative dashboard."
