a46b319d44e3ceb925fb6682daf5b1bc02415cd3
Matt Perpick <matthewperpick@gmail.com>
2012-08-29 15:03:20 -0400

    Finishing dashboard Python examples.

--- code_snippets/api-dashboard-delete.py
+++ code_snippets/api-dashboard-delete.py
@@ -4,5 +4,5 @@ api.api_key = 'apikey_2'
 api.application_key = '01a39483d351f4f827571cb7091a39e82f86edcc'
 api.api_host = 'https://app.datad0g.com'
 
-dash_id = 1589
-print api.dashboard(dash_id)
+dash_id = 1588
+print api.delete_dashboard(dash_id)
--- code_snippets/api-dashboard-get-response.py
+++ code_snippets/api-dashboard-get-response.py
@@ -1,1 +1,15 @@
-FIXME
+{'graphs': [
+    {
+        'definition': {
+            'viz': 'timeseries',
+            'requests': [
+                {'q': 'avg:system.mem.free{*} by {host}'}
+            ],
+            'events': []
+        },
+        'title': 'Average Memory Free By Host'
+    }],
+    'title': 'My Dashboard',
+    'description': 'A new and improved dashboard!',
+    'id': 1589
+}
