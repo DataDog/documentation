d6d873fdf974642418bfaf2e605ac99f982f5436
Matt Perpick <matthewperpick@gmail.com>
2012-08-29 14:46:31 -0400

    Adding python dashboard example.

--- code_snippets/api-dashboard-create.py
+++ code_snippets/api-dashboard-create.py
@@ -1,1 +1,20 @@
-FIXME
+from dogapi import dog_http_api as api
+
+api.api_key = 'apikey_2'
+api.application_key = '01a39483d351f4f827571cb7091a39e82f86edcc'
+api.api_host = 'https://app.datad0g.com'
+
+title = "My Dashboard"
+description = "An informative dashboard."
+graphs =  [{
+    "definition": {
+        "events": [],
+        "requests": [
+            {"q": "avg:system.mem.free{*}"}
+        ],
+    "viz": "timeseries"
+    },
+    "title": "Average Memory Free"
+}]
+
+print api.create_dashboard(title, description, graphs)
