dd2c0420c331a5dc5a6fbcbbd13eea35a97259a1
Matt Perpick <matthewperpick@gmail.com>
2012-08-30 15:54:27 -0400

    Adding shell examples for metrics and events.

---
+++ code_snippets/api-events-post-response.sh
@@ -0,0 +1,16 @@
+{
+    "event": {
+        "date_happened": 1346355252,
+        "handle": null,
+        "id": 1377281704830403917,
+        "priority": "normal",
+        "related_event_id": null,
+        "tags": [
+            "environment:test"
+        ],
+        "text": null,
+        "title": "Did you hear the news today?",
+        "url": "https://app.datadoghq.com/event/jump_to?event_id=1377281704830403917"
+    },
+    "status": "ok"
+}
--- code_snippets/api-metrics-post.sh
+++ code_snippets/api-metrics-post.sh
@@ -1,1 +1,9 @@
-echo FIXME
+curl  -X POST -H "Content-type: application/json" \
+-d '{ "series" :
+         [{"metric":"test.metric",
+          "points":[[1346340794, 20]],
+          "type":"counter",
+          "tags":["environment:test"]}
+        ]
+    }' \
+'https://app.datadoghq.com/api/v1/series?api_key=9775a026f1ca7d1c6c5af9d94d9595a4'
