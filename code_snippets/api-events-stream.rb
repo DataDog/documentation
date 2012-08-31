885bc44a288171e1ba3400e6672e0a8b2827e7bc
Matt Perpick <matthewperpick@gmail.com>
2012-08-31 18:36:01 -0400

    Adding ruby event examples.

--- code_snippets/api-events-stream.rb
+++ code_snippets/api-events-stream.rb
@@ -6,8 +6,7 @@ app_key='87ce4a24b5553d2e482ea8a8500e71b8ad4554ff'
 
 dog = Dogapi::Client.new(api_key, app_key)
 
-# submitting events doesn't require an application_key, so we don't bother
-# setting it
-dog = Dogapi::Client.new(api_key)
+start_time = 1346272158
+end_time = 1346274158
 
-dog.emit_event(Dogapi::Event.new('Testing done, FTW'))
+dog.stream(start_time, end_time, :priority=>"normal", :tags=>["application:web"])
--- code_snippets/results/result.api-events-post.rb
+++ code_snippets/results/result.api-events-post.rb
@@ -2,12 +2,12 @@
  {"status"=>"ok",
   "event"=>
    {"priority"=>"normal",
-    "date_happened"=>1346451938,
+    "date_happened"=>1346452418,
     "handle"=>nil,
     "title"=>"",
     "url"=>
-     "https://app.datadoghq.com/event/jump_to?event_id=1378903847105195379",
+     "https://app.datadoghq.com/event/jump_to?event_id=1378911893708573827",
     "text"=>nil,
     "tags"=>[],
     "related_event_id"=>nil,
-    "id"=>1378903847105195379}}]
+    "id"=>1378911893708573827}}]
