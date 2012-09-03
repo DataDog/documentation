1c378c5ee06475f2b5396bc3e0a490e2d0bb17b4
Matt Perpick <matthewperpick@gmail.com>
2012-09-03 06:23:55 -0400

    Adding a get dashboard curl example.

--- code_snippets/results/result.api-dashboard-get.sh
+++ code_snippets/results/result.api-dashboard-get.sh
@@ -1,5 +1,23 @@
 {
-    "errors": [
-        "No dashboard matches that dash_id."
-    ]
+    "dash": {
+        "description": "An informative dashboard.", 
+        "graphs": [
+            {
+                "definition": {
+                    "events": [], 
+                    "requests": [
+                        {
+                            "q": "avg:system.mem.free{*}"
+                        }
+                    ], 
+                    "viz": "timeseries"
+                }, 
+                "title": "Average Memory Free"
+            }
+        ], 
+        "id": 2473, 
+        "title": "My Dashboard"
+    }, 
+    "resource": "/api/v1/dash/2473", 
+    "url": "/dash/dash/2473"
 }
