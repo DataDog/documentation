acc1f4732e69e24b64ed79cec7838793c20bc007
Matt Perpick <matthewperpick@gmail.com>
2012-09-03 06:07:02 -0400

    Adding dashboard update shell example.

--- code_snippets/api-dashboard-update.sh
+++ code_snippets/api-dashboard-update.sh
@@ -1,1 +1,20 @@
-echo '{"FIXME":"FIXME"}'
+api_key=9775a026f1ca7d1c6c5af9d94d9595a4
+app_key=87ce4a24b5553d2e482ea8a8500e71b8ad4554ff
+dash_id=2532
+
+curl  -X PUT -H "Content-type: application/json" \
+-d '{
+      "graphs" : [{
+          "title": "Sum of Memory Free",
+          "definition": {
+              "events": [],
+              "requests": [
+                  {"q": "sum:system.mem.free{*}"}
+              ]
+          },
+          "viz": "timeseries"
+      }],
+      "title" : "Sum Memory Free Shell",
+      "description" : "A dashboard with memory info."
+    }' \
+"https://app.datadoghq.com/api/v1/dash/${dash_id}?api_key=${api_key}&application_key=${app_key}"
--- code_snippets/results/result.api-dashboard-update.sh
+++ code_snippets/results/result.api-dashboard-update.sh
@@ -1,3 +1,22 @@
 {
-    "FIXME": "FIXME"
+    "dash": {
+        "description": "A dashboard with memory info.", 
+        "graphs": [
+            {
+                "definition": {
+                    "events": [], 
+                    "requests": [
+                        {
+                            "q": "sum:system.mem.free{*}"
+                        }
+                    ]
+                }, 
+                "title": "Sum of Memory Free"
+            }
+        ], 
+        "id": 2532, 
+        "title": "Sum Memory Free Shell"
+    }, 
+    "resource": "/api/v1/dash/2532", 
+    "url": "/dash/dash/2532"
 }
