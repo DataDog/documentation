777ff2b129cd37c3506d6e19f3fb43d80a42f8c3
Matt Perpick <matthewperpick@gmail.com>
2012-09-03 07:57:58 -0400

    Adding ruby dashboard examples.

--- code_snippets/results/result.api-dashboard-create.rb
+++ code_snippets/results/result.api-dashboard-create.rb
@@ -1,1 +1,13 @@
-"FIXME"
+["200",
+ {"dash"=>
+   {"graphs"=>
+     [{"definition"=>
+        {"viz"=>"timeseries",
+         "events"=>[],
+         "requests "=>[{"q"=>"avg:system.mem.free{*}"}]},
+       "title"=>"Average Memory Free"}],
+    "description"=>"And they are marvelous.",
+    "id"=>2552,
+    "title"=>"My First Metrics"},
+  "url"=>"/dash/dash/2552",
+  "resource"=>"/api/v1/dash/2552"}]
