2f7e4a8396c7075384da10dc75a016060ef551ae
Matt Perpick <matthewperpick@gmail.com>
2012-09-03 07:20:58 -0400

    Fixing ruby commenting examples.

--- code_snippets/api-comment-create.rb
+++ code_snippets/api-comment-create.rb
@@ -8,9 +8,3 @@ dog = Dogapi::Client.new(api_key, app_key)
 
 # Make a comment.
 dog.comment("I have something to say.")
-
-# Make a comment as a particular user.
-dog.comment("Bob does too.", :handle => "bob@example.com")
-
-# Reply to a comment
-dog.comment("Its rebuttal time.", :related_event_id => "1234")
