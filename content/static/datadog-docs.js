42f21e687e39d8486c96164953005655d8bfc674
Matt Perpick <matthewperpick@gmail.com>
2012-09-04 10:49:14 -0400

    Adding page javascript.

--- content/static/datadog-docs.js
+++ content/static/datadog-docs.js
@@ -1,23 +1,30 @@
-$(function () {
+(function () {
 
-    $('.lang-tab').click(function (e) {
-        var el = $(this);
-        var lang = el.attr('lang');
-        $('.' + lang + '-lang-tab').tab('show');
-    });
+    var DD_docs = {};
 
-    $('.lang-btn').click(function (e) {
+    DD_docs.metricsGuidePage = function () {
+        $('.lang-tab').click(function (e) {
+            var el = $(this);
+            var lang = el.attr('lang');
+            $('.' + lang + '-lang-tab').tab('show');
+        });
+    };
 
-        var el = $(this);
+    DD_docs.apiPage = function () {
+        $('.lang-btn').click(function (e) {
+            var el = $(this);
 
-        // Highlight the active button.
-        $('.lang-btn').removeClass('active');
-        el.addClass('active');
+            // Highlight the active button.
+            $('.lang-btn').removeClass('active');
+            el.addClass('active');
 
-        // Show this language's code blocks.
-        var lang = el.attr('lang');
-        $('.code-block').hide();
-        $('.code-block-' + lang).fadeIn();
-    });
+            // Show this language's code blocks.
+            var lang = el.attr('lang');
+            $('.code-block').hide();
+            $('.code-block-' + lang).fadeIn();
+        });
+    };
 
-});
+    // Export to global scope.
+    window.DD_docs = DD_docs;
+})();
