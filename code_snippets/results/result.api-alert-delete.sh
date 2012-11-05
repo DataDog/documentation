43eb08c27aab95ecaef0f95f72a43c1b5d8bd546
Matt Singleton <matt@datadoghq.com>
2012-11-05 16:06:43 -0500

    alert docs (shell only)

---
+++ code_snippets/results/result.api-alert-delete.sh
@@ -0,0 +1,10 @@
+{
+    "creator": 3658, 
+    "event_object": "e7ac36c6531ccfa9e39c8988b0f28bdb", 
+    "message": "We may need to add web hosts if this is consistently high.", 
+    "name": "Bytes received on host0", 
+    "notify_no_data": false, 
+    "query": "sum(last_1d):sum:system.net.bytes_rcvd{host:host0} > 100", 
+    "silenced": true, 
+    "state": "OK"
+}
