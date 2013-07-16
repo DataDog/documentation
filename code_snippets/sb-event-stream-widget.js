05551598e92610590343b8355ccac8c7ec84afd5
Conor Branagan <conor.branagan@gmail.com>
2013-07-16 18:13:14 -0400

    Add Screenboard API docs.

---
+++ code_snippets/sb-event-stream-widget.js
@@ -0,0 +1,18 @@
+{
+    "type": "event_stream"
+
+    // Title
+    "title": false,
+
+    // Sizing
+    "height": 57,
+    "width": 59,
+
+    // Positioning
+    "y": 18,
+    "x": 84,
+
+    // Widgets Parameters
+    "query": "tags:release",
+    "timeframe": "1w" // Choose from: [1h, 4h, 1d, 2d, 1w]
+}
