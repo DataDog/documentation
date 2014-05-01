c284934b09c0ac64c906d8928090639625e7120f
Celene <celene@datadoghq.com>
2014-05-01 21:34:37 +0000

    fix small typo in agent check doc, fixes #138

--- code_snippets/guides-agentchecks-event.py
+++ code_snippets/guides-agentchecks-event.py
@@ -1,12 +1,12 @@
 {
     "timestamp": int, the epoch timestamp for the event,
-    "event_type": string, the event time name,
-    "api_key": string, the api key of the account to associate the event with,
+    "event_type": string, the event name,
+    "api_key": string, the api key for your account,
     "msg_title": string, the title of the event,
     "msg_text": string, the text body of the event,
-    "aggregation_key": string, a key to aggregate events on,
-    "alert_type": (optional) string, one of ('error', 'warning', 'success', 'info').
-        Defaults to 'info'.
+    "aggregation_key": string, a key to use for aggregating events,
+    "alert_type": (optional) string, one of ('error', 'warning', 'success', 'info');
+        defaults to 'info',
     "source_type_name": (optional) string, the source type name,
     "host": (optional) string, the name of the host,
     "tags": (optional) list, a list of tags to associate with this event
