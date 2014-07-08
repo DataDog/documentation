9e0da07b54de00644d0ac48dca171042343f19e9
celene <celene@datadoghq.com>
2014-07-08 19:58:53 +0000

    fix dashboard references to timeboards

--- code_snippets/api-dashboard-create.rb
+++ code_snippets/api-dashboard-create.rb
@@ -6,7 +6,7 @@ app_key='87ce4a24b5553d2e482ea8a8500e71b8ad4554ff'
 
 dog = Dogapi::Client.new(api_key, app_key)
 
-# Create a dashboard.
+# Create a timeboard.
 title = 'My First Metrics'
 description = 'And they are marvelous.'
 graphs = [{
--- code_snippets/api-dashboard-update.py
+++ code_snippets/api-dashboard-update.py
@@ -4,8 +4,8 @@ api.api_key='9775a026f1ca7d1c6c5af9d94d9595a4'
 api.application_key='87ce4a24b5553d2e482ea8a8500e71b8ad4554ff'
 
 dash_id = 2524
-title = "My Dashboard"
-description = "A new and improved dashboard!"
+title = "My Timeboard"
+description = "A new and improved timeboard!"
 graphs =  [{
     "definition": {
         "events": [],
