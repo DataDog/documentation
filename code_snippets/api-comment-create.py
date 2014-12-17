4bfe3a59246f82e904fee5376a62a2021e485feb
Technovangelist <m@technovangelist.com>
2014-12-17 13:34:31 -0500

    Modified the code sample for Python to accurately reflect the dogapi python wrapper. Handle is not optional but you can set it to None.

--- code_snippets/api-comment-create.py
+++ code_snippets/api-comment-create.py
@@ -4,7 +4,13 @@ api.api_key = '9775a026f1ca7d1c6c5af9d94d9595a4'
 api.application_key = '87ce4a24b5553d2e482ea8a8500e71b8ad4554ff'
 
 # Start a new discussion.
+# You can include a handle like this
 api.comment('matt@example.com', 'Should we use COBOL or Fortran?')
+
+# Or you can set it to None 
+# and the handle will default
+# to the owner of the application key
+api.comment(None, 'Should we use COBOL or Fortran?')
 
 # Reply to a discussion.
 api.comment('joe@example.com', 'Smalltalk?', related_event_id=1234)
