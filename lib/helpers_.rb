aa2e7af56a043aebda0023f7c7515cbd0a1943be
Jay HOTTA <fukuzo@cybertron.co.jp>
2015-04-02 15:27:04 +0900

    helper program added to display video example sub-index page

--- lib/helpers_.rb
+++ lib/helpers_.rb
@@ -5,21 +5,40 @@ include Nanoc::Toolbox::Helpers::TaggingExtra
 include Nanoc::Toolbox::Helpers::HtmlTag
 
 
+# general functions
 def example_items
-  @items.select { |item| item[:kind] == 'example'}
+  @items.select { |item| item[:kind] == 'example' && item[:language] == nil }
 end
 
 def video_items
-  @items.select { |item| item[:kind] == 'video'}
+  @items.select { |item| item[:kind] == 'video' && item[:language] == nil }
 end
 
 def integration_items
-  integrations = @items.select { |item| item[:kind] == 'integration'}
+  integrations = @items.select { |item| item[:kind] == 'integration' && item[:language] == nil }
   integrations.sort_by { |i| i[:integration_title].downcase }
 end
 
 def guide_items
-  guides = @items.select{ |item| item[:kind] == 'guide' && item[:listorder] != nil}
+  guides = @items.select{ |item| item[:kind] == 'guide' && item[:language] == nil }
   guides.sort_by { |item| item[:listorder] }
 end
 
+# Japanese specific functions
+def ja_example_items
+  @items.select { |item| item[:kind] == 'example' && item[:language] == 'ja' }
+end
+
+def ja_video_items
+  @items.select { |item| item[:kind] == 'video' && item[:language] == 'ja' }
+end
+
+def ja_integration_items
+  integrations = @items.select { |item| item[:kind] == 'integration' && item[:language] == 'ja' }
+  integrations.sort_by { |i| i[:integration_title].downcase }
+end
+
+def ja_guide_items
+  guides = @items.select{ |item| item[:kind] == 'guide' && item[:listorder] != nil && item[:language] == 'ja' }
+  guides.sort_by { |item| item[:listorder] }
+end
