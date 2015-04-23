2174c707b10385def6f26124007fd5acbe325acc
Technovangelist <m@technovangelist.com>
2015-04-23 16:49:23 -0400

    user suggested adding a wait here
    sometimes if you programmatically create a comment immediately after creating the event, the event won't be there in time to run the comment line.
    
    Signed-off-by: Technovangelist <m@technovangelist.com>

--- code_snippets/api-events-post.rb
+++ code_snippets/api-events-post.rb
@@ -11,3 +11,7 @@ dog = Dogapi::Client.new(api_key, app_key)
 dog = Dogapi::Client.new(api_key)
 
 dog.emit_event(Dogapi::Event.new('msg_text', :msg_title => 'Title'))
+
+# If you are programmatically adding a comment to this new event
+# you might want to insert a pause of .5 - 1 second to allow the
+# event to be available.
