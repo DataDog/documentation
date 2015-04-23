254b40b205f9793bb44c2d4fec3be571cfa99b39
Technovangelist <m@technovangelist.com>
2015-04-23 16:32:42 -0400

    Update python code samples
    There were a few python code samples that were broken and many were not using the standard bogus key. Many used hard coded id's and names that probably don't exist so those are updated to choose dynamic real id's etc from the users environment.
    
    Signed-off-by: Technovangelist <m@technovangelist.com>

--- code_snippets/api-monitor-edit.py
+++ code_snippets/api-monitor-edit.py
@@ -1,7 +1,11 @@
-from dogapi import dog_http_api as api
+from datadog import initialize,  api
 
-api.api_key = '9775a026f1ca7d1c6c5af9d94d9595a4'
-api.application_key = '87ce4a24b5553d2e482ea8a8500e71b8ad4554ff'
+options = {
+    'api_key': '9775a026f1ca7d1c6c5af9d94d9595a4',
+    'app_key': '87ce4a24b5553d2e482ea8a8500e71b8ad4554ff'
+}
+
+initialize(**options)
 
 # Edit an existing monitor
 api.Monitor.update(2081, query="avg(last_1h):sum:system.net.bytes_rcvd{host:host0} > 100", name="Bytes received on host0", message="We may need to add web hosts if this is consistently high.")
