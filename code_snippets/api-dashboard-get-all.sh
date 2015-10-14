f38989534746a11751dc4880ecc38a302b43c8d4
Andrew Vaughan <asdvaughan@gmail.com>
2015-10-14 15:23:52 -0700

    Deleted extra space that would cause 400 error if copy/pasted

--- code_snippets/api-dashboard-get-all.sh
+++ code_snippets/api-dashboard-get-all.sh
@@ -1,4 +1,4 @@
 api_key=9775a026f1ca7d1c6c5af9d94d9595a4
 app_key=87ce4a24b5553d2e482ea8a8500e71b8ad4554ff
 
-curl "https://app.datadoghq.com/api/v1/dash?api_key=${api_key}&application_key=${app_key} "
+curl "https://app.datadoghq.com/api/v1/dash?api_key=${api_key}&application_key=${app_key}"
