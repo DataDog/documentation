6d57670ac44b0375d2c28e8c97f1b493180cb2ff
Conor Branagan <conor.branagan@gmail.com>
2015-10-27 12:03:23 -0400

    Update downtime docs to include the spec for recurrence.
    - Update schedule snippets to set up a recurrence.
    - Update docs to give details of new fields.

--- code_snippets/api-monitor-schedule-downtime.rb
+++ code_snippets/api-monitor-schedule-downtime.rb
@@ -6,5 +6,17 @@ app_key='87ce4a24b5553d2e482ea8a8500e71b8ad4554ff'
 
 dog = Dogapi::Client.new(api_key, app_key)
 
+# Repeat for 3 hours (starting now) on every week day for 4 weeks.
+start_ts = Time.now.to_i
+end_ts = start_ts + (3 * 60 * 60)
+end_reccurrence_ts = start_ts + (4* 7 * 24 * 60 * 60) # 4 weeks from now
+
+recurrence = {
+  'type' => 'weeks',
+  'period' => 1,
+  'week_days' => ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+  'until_date' => end_reccurrence_ts
+}
+
 # Schedule downtime
-dog.schedule_downtime('env:testing', :start => Time.now.to_i)
+dog.schedule_downtime('env:testing', :start => start_ts, :end => end_ts, :recurrence => recurrence)
--- code_snippets/api-monitor-schedule-downtime.sh
+++ code_snippets/api-monitor-schedule-downtime.sh
@@ -1,11 +1,20 @@
 api_key=9775a026f1ca7d1c6c5af9d94d9595a4
 app_key=87ce4a24b5553d2e482ea8a8500e71b8ad4554ff
 
-currenttime=$(date +%s)
+start=$(date +%s)
+end=$(date -v+3H +%s)
+end_recurrence=$(date -v+21d +%s)
 
 curl -X POST -H "Content-type: application/json" \
--d "{
-      \"scope\": \"env:prod\",
-      \"start\": \"${currenttime}\"
-    }" \
+-d '{
+      "scope": "env:prod",
+      "start": '"${start}"',
+      "end": '"${end}"',
+      "recurrence": {
+        "type": "weeks",
+        "period": 1,
+        "week_days": ["Mon", "Tue", "Wed", "Thu", "Fri"],
+        "until_date": '"${end_recurrence}"'
+      }
+    }' \
     "https://app.datadoghq.com/api/v1/downtime?api_key=${api_key}&application_key=${app_key}"
