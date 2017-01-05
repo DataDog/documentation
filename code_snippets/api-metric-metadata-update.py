ccd48b4dd63cd6f3760ca4beb37d8b97ed658cea
Alexandra Grant <alexandra.grant@datadoghq.com>
2017-01-05 17:32:16 -0500

    1st pass at adding documentation for new metric metadata endpoint

---
+++ code_snippets/api-metric-metadata-update.py
@@ -0,0 +1,19 @@
+from datadog import initialize, api
+
+options = {
+    'api_key': '9775a026f1ca7d1c6c5af9d94d9595a4',
+    'app_key': '87ce4a24b5553d2e482ea8a8500e71b8ad4554ff'
+}
+
+initialize(**options)
+
+metric = 'system.net.bytes_sent'
+params = {
+    'description': 'My custom description',
+    'short name': 'bytes sent',
+    'type': 'gauge',
+    'unit': 'byte',
+    'per_unit': 'second'
+}
+
+print api.Metadata.update(metric_name=metric, **params)
