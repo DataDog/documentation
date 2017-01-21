fa46657c7d9edadcdc063b54ff8d849a6c9089b6
Jay Hotta <fukuzo@cybertron.co.jp>
2017-01-20 23:45:19 -0500

    outputs a list of jp files that need to review

--- lib/generate_ja_pages.rb
+++ lib/generate_ja_pages.rb
@@ -3,7 +3,7 @@ require 'pp'
 def generate_ja_pages
   english_pages = (@items.select { |item| !(item.identifier.match('/ja')) && !(item.identifier.match('tipuesearch')) && !(item.identifier.match('/static')) && !(item.identifier.match('images'))}).collect { |item| item.identifier}
   english_pages = english_pages - @config[:redirects].collect{|redirect| redirect[:from]}
-  japanese_pages = (@items.select { |item| (item.identifier.match('/ja')) && !(item.identifier.match('/static')) && !(item.identifier.match('images'))}).collect { |item| item.identifier.sub('/ja', '')}
+  japanese_pages = (@items.select { |item| (item.identifier.match('/ja')) && !(item.identifier.match('/static')) && !(item.identifier.match('images')) && !(item.identifier.match('/java'))}).collect { |item| item.identifier.sub('/ja', '')}
 
   missingpages = english_pages-japanese_pages
 
@@ -42,3 +42,41 @@ def generate_ja_pages
       )
   end
 end
+
+def list_update_pages
+  current_dir = File.split(File.expand_path(__FILE__))[0]
+  dir = current_dir.split('/')
+  dir.slice!(-1,1)
+  dir.push('content', 'ja')
+  ja_dir_path = dir.join('/')
+
+  base_pages = (@items.select { |item| (item.identifier.match('/ja')) && !(item.identifier.match('/static')) && !(item.identifier.match('images')) && !(item.identifier.match('videos')) && !(item.identifier.match('sitemap')) && !(item.identifier.match('integrations')) && !(item.identifier.match('examples')) && !(item.identifier.match('api')) && !(item.identifier.match('/java')) && !(item.identifier.match('api')) && !(item.identifier.match('error'))}).collect { |item| item.identifier.sub('/ja', ja_dir_path)}
+
+  File.open('updated_files.txt', 'w+') do |f|
+    base_pages.each do |page|
+      page = page.chop! + '.md'
+      begin
+        if File.ftype(page) != 'directory' then
+          begin
+            jp_timestamp = File.mtime(page)
+            en_page = page.split('/') - ['ja']
+            en_file_path = en_page.join('/')
+            en_timestamp = File.mtime(en_file_path)
+            td = en_timestamp - jp_timestamp
+            if td > 0 then
+              f.puts(en_file_path)
+              f.puts("[updated]\n\n")
+            end
+          rescue
+            f.puts(en_file_path)
+            f.puts("[mtime error, file may not be '.md'] \n\n")
+          end
+        end
+      rescue
+        f.puts(page)
+        f.puts("[file not exist]\n\n")
+      end
+    end
+  end
+end
+
