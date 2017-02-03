f6eba9828603a74b130513a57bb0e356c5ba5902
Jason Yee <jason.yee@datadoghq.com>
2017-02-03 12:15:53 -0800

    Added python results

---
+++ code_snippets/results/result.trace-api-spans.py
@@ -0,0 +1,6 @@
+# This will return a 200 if the trace was accepted
+# or a 500 if there was an error.
+print(response.status_code)
+
+# This will print the string "OK" or an error message.
+print(response.text)
