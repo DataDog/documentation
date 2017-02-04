876d919d88e01704e18b2ec7cd3be5f3ead0c1be
Jason Yee <jason.yee@datadoghq.com>
2017-02-03 18:39:45 -0800

    Go sample traces

--- code_snippets/trace-api-spans.go
+++ code_snippets/trace-api-spans.go
@@ -1,3 +1,58 @@
 package main
 
-import "fmt"
+import (
+    "bytes"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "math/rand"
+    "net/http"
+    "time"
+)
+
+type Span struct {
+    TraceId int `json:"trace_id"`
+    SpanId int `json:"span_id"`
+    Name string `json:"name"`
+    Resource string `json:"resource"`
+    Service string `json:"service"`
+    Type string `json:"type"`
+    Start int64 `json:"start"`
+    Duration int64 `json:"duration"`
+}
+
+type Trace []Span
+
+type Traces []Trace
+
+func main() {
+    // Create IDs.
+    trace_id := rand.Intn(1000000)
+    span_id := rand.Intn(1000000)
+
+    // Start a timer.
+    start := time.Now().UnixNano()
+
+    // Do things...
+    time.Sleep(2 * time.Second)
+
+    // Stop the timer.
+    duration := time.Now().UnixNano() - start
+
+    // Send the traces.
+    traces := Traces{ Trace{ Span{
+        TraceId: trace_id,
+        SpanId: span_id,
+        Name: "span_name",
+        Resource: "/home",
+        Service: "pseudo_service",
+        Type: "web",
+        Start: start,
+        Duration: duration}}}
+    jsonTraces, _ := json.Marshal(traces)
+
+    client := &http.Client{}
+    request, _ := http.NewRequest("PUT", "http://localhost:7777/v0.3/traces", bytes.NewBuffer(jsonTraces))
+    request.Header.Add("Content-Type", "application/json")
+    response, _ := client.Do(request)
+}
