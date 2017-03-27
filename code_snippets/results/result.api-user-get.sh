459daf6c25d501fa5d84aa8c7abc42dc6e506351
samanthadrago <samantha@datadoghq.com>
2017-03-27 14:16:57 -0400

    Add access_role field to user api doc and examples

--- code_snippets/results/result.api-user-get.sh
+++ code_snippets/results/result.api-user-get.sh
@@ -1,1 +1,1 @@
-{"user": {"handle": "test@datadoghq.com", "name": "test user", "verified": false, "disabled": false, "role": null, "is_admin": false, "email": "test@datadoghq.com"}}
+{"user": {"handle": "test@datadoghq.com", "name": "test user", "access_role": "st", "verified": false, "disabled": false, "role": null, "is_admin": false, "email": "test@datadoghq.com"}}
