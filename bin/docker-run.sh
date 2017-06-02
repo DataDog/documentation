5caafbfbea090fa10cc8ecca1fb4666c16f977e9
Peter Panagiotakos <peter.panagiotakos@datadoghq.com>
2017-06-02 14:55:41 -0400

    clean up files

--- bin/docker-run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-## The script executed by the Docker dev image
-
-nohup /usr/sbin/nginx -g 'daemon on;'
-bundle exec guard -i -G DockerGuardfile
--- fileext.py
+++
@@ -1,8 +0,0 @@
-import os,sys
-folder = 'content/ja/guides/basic_agent_usage'
-for filename in os.listdir(folder):
-       infilename = os.path.join(folder,filename)
-       if not os.path.isfile(infilename): continue
-       oldbase = os.path.splitext(filename)
-       newname = infilename.replace('.md', '.ja.md')
-       output = os.rename(infilename, newname)
