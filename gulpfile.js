3757fd338acfac3059f4ed2ef72e1300e3bde630
Michael Whetten <michaelwhetten@gmail.com>
2017-06-16 10:28:12 -0400

    bower

--- gulpfile.js
+++ gulpfile.js
@@ -202,8 +202,8 @@ gulp.task('watch', function () {
 // `gulp build` - Run all the build tasks but don't clean up beforehand.
 // Generally you should be running `gulp` instead of `gulp build`.
 gulp.task('build', function (callback) {
-  runSequence('scss',
-    'js',
+  runSequence('styles',
+    'scripts',
     callback);
 });
 
