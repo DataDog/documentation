0c8135d21d307ea388b5897cb4dc2b89bdad81d5
Michael Whetten <michaelwhetten@gmail.com>
2017-07-10 16:23:27 -0400

    use hosted docker image

--- gitlab/bin/sh/run-site.sh
+++ gitlab/bin/sh/run-site.sh
@@ -16,13 +16,13 @@ if [ ${RUN_SERVER} == true ]; then
 	echo "building hugo site..."
 	if [ ${FETCH_INTEGRATIONS} == true ]; then
 		echo "grabbing integrations. this takes forever."
-		integrations_sync.py --token "${GITHUB_TOKEN}"
+		integrations_sync.py --token "${GITHUB_TOKEN}" || true
 	fi
 	if [ ${CREATE_I18N_PLACEHOLDERS} == true ]; then
-		echo "createing i18n placeholder pages."
-		placehold_translations.py -c "config.yaml" -f "content/"
+		echo "creating i18n placeholder pages."
+		placehold_translations.py -c "config.yaml" -f "content/" || true
 	fi
-	hugo server --watch=true --renderToDisk --source="/src" --bind="0.0.0.0" "$@" || exit 1
+	hugo server --watch=true --renderToDisk --bind="0.0.0.0" "$@" || exit 1
 else
 	exit 0
 fi
