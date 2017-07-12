08bc5ddc60c79a9613259ecbc9288a93263ecec5
Michael Whetten <michaelwhetten@gmail.com>
2017-07-12 15:13:16 -0400

    fix shortcode for non-dd employees

--- gitlab/etc/trello_config.sh
+++ gitlab/etc/trello_config.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 card_text="$(cat /tmp/warn_links)"
-board_id="540f0e42ca9b4bba5c30dc3d"  # baklava board
 card_name="$(date +%Y%m%d)-corpsite-broken-links"
-list_id="5491af7616334493e3b8dcc4"  # parking lot
 members="5609ac1a7322b0fe59c8e58e"  # michael
