27cc9208dc44b4e797f764c5a65a4d0307046044
Nelson Onyibe <nelson.onyibe@datadoghq.com>
2017-08-08 15:52:18 -0400

    Adding 1st draft of Bill Usage API docs

---
+++ code_snippets/results/result.api-billing-usage-timeseries.sh
@@ -0,0 +1,19 @@
+{
+  usage: [
+  {
+      "num_custom_timeseries": 11,
+      "num_standard_timeseries": 21855,
+      "hour": "2017-05-01T14"
+    },
+    {
+      "num_custom_timeseries": 247743,
+      "num_standard_timeseries": 421266,
+      "hour": "2017-05-01T15"
+    }
+  ],
+  parameters: {
+    start_hr: "2017-07-24T00",
+    fill_gaps: false,
+    end_hr: "2017-07-25T00"
+  }
+}
---
+++ code_snippets/results/result.api-billing-usage-top-avg-metrics.sh
@@ -0,0 +1,27 @@
+{
+  usage: [
+      {
+      "metric_category": "standard",
+      "max_metric_hour": 7549,
+      "avg_metric_hour": 7479,
+      "metric_id": 8613358,
+      "metric_name": "aws.ec2.spot_history"
+    },
+    {
+      "metric_category": "standard",
+      "max_metric_hour": 6386,
+      "avg_metric_hour": 4760,
+      "metric_id": 5610,
+      "metric_name": "system.processes.number"
+    }
+  ],
+  parameters: {
+    "category": "standard",
+    "start_hr": "2017-05-01T00",
+    "names": [
+          "system.processes.number",
+          "aws.ec2.spot_history"
+    ],
+    "end_hr": "2017-05-26T00"
+  }
+}
