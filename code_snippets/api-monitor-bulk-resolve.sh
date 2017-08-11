ab0710c4dbe5c7d7d66bfbf8130c7c5223a7816c
Pierre Guceski <p.guceski@gmail.com>
2017-08-11 15:17:12 +0200

    Resolve API documentation

--- code_snippets/api-monitor-bulk-resolve.sh
+++ code_snippets/api-monitor-bulk-resolve.sh
@@ -13,7 +13,8 @@ curl -X POST -H "Content-type: application/json" \
 -d '{
 "resolve": [
           {"${monitor_id}": "${group_1}"},
-          {"${monitor_id}": "${group_2}"},
+          {"${monitor_id}": "${group_2}"}
       ]
     }' \
     "https://app.datadoghq.com/monitor/bulk_resolve?api_key=${api_key}&application_key=${app_key}"
+    
