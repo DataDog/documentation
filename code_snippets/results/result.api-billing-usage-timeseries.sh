e68acbc3a3d83a62489ca4f4bfd8a47e500b59df
Nelly <nelson.onyibe@datadoghq.com>
2017-08-14 13:26:10 -0400

    1st draft usage api

--- code_snippets/results/result.api-billing-usage-timeseries.sh
+++ code_snippets/results/result.api-billing-usage-timeseries.sh
@@ -2,12 +2,10 @@
   usage: [
   {
       "num_custom_timeseries": 11,
-      "num_standard_timeseries": 21855,
       "hour": "2017-05-01T14"
     },
     {
       "num_custom_timeseries": 247743,
-      "num_standard_timeseries": 421266,
       "hour": "2017-05-01T15"
     }
   ]
