c301be2432d62a298eebe26bc85a34185ec3fb21
Ilan Rabinovitch <ilan@fonz.net>
2017-08-24 12:51:38 -0400

    start time should be before end time

--- code_snippets/api-billing-usage-hosts.sh
+++ code_snippets/api-billing-usage-hosts.sh
@@ -2,7 +2,7 @@ api_key=9775a026f1ca7d1c6c5af9d94d9595a4
 app_key=87ce4a24b5553d2e482ea8a8500e71b8ad4554ff
 
 start_hr=$(date -v -10d +%Y-%m-%dT%H)
-end_hr=$(date -v -10d +%Y-%m-%dT%H)
+end_hr=$(date -v +%Y-%m-%dT%H)
 
 curl -G \
     "https://app.datadoghq.com/api/v1/usage/hosts" \
