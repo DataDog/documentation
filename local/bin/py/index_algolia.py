8732c2aa175ae982be73aa06411d01e34a6b8579
Michael Whetten <michaelwhetten@gmail.com>
2017-08-25 18:46:08 -0400

    update workflow for ja

--- local/bin/py/index_algolia.py
+++ local/bin/py/index_algolia.py
@@ -19,104 +19,105 @@ def content_location():
 
 
 def index_algolia(app_id, api_key, content_path=None):
-    client = algoliasearch.Client(app_id, api_key)
+    dirs_exclude = ['js', 'images', 'fonts', 'en', 'css', 'search', 'matts quick tips']
+    languages = ['ja']
 
-    index_english_build = client.init_index(DOCS_BUILD_ENGLISH)
-    index_japanese_build = client.init_index(DOCS_BUILD_JAPANESE)
-
-    index_english_articles = []
-    index_japanese_articles = []
-
-    dirs_exclude = ('js', 'images', 'fonts', 'en', 'css', 'search', 'matts quick tips')
-    files_to_exclude = ('404.html')
+    files_to_exclude = ['404.html']
 
     docs_host = 'https://docs.datadoghq.com'
 
-    try:
+    # instantiate the algolia api
+    client = algoliasearch.Client(app_id, api_key)
+    index_english_build = client.init_index(DOCS_BUILD_ENGLISH)
+    index_japanese_build = client.init_index(DOCS_BUILD_JAPANESE)
 
-        content_path = content_path or content_location()
-        content_location_exists = os.path.exists(content_path)
+    # set the root path (default)
+    content_path = content_path or content_location()
 
-        if not content_location_exists:
+    def crawl_site(path, exclusions):
+        articles = []
+
+        if not os.path.exists(path):
             raise ValueError('Content folder path incorrect')
         else:
 
-            for (dirpath, dirnames, filenames) in os.walk(content_path):
-                dirnames[:] = [d for d in dirnames if d not in dirs_exclude]
+            for (dirpath, dirnames, filenames) in os.walk(path):
+                dirnames[:] = [d for d in dirnames if d not in exclusions]
                 filenames[:] = [f for f in filenames if f.endswith('.html') and f not in files_to_exclude]
 
                 for filename in filenames:
 
-                    lang = ''
-
-                    if dirpath.startswith("public/ja"):
-                        lang = 'ja'
-
                     article = {}
 
                     with open(os.path.join(dirpath, filename), 'rt', encoding='utf-8') as myfile:
+                        try:
+                            html = BeautifulSoup(myfile, "html.parser")
 
-                        html = BeautifulSoup(myfile, "html.parser")
+                            # title
+                            title = html.title.string
+                            if not title:
+                                title = html.select('h1')[0].text.strip()
 
-                        main = html.find("div", {"main"})
+                            if title and '://' not in title:
+                                # description
+                                desc_text = ""
+                                main = html.find("div", {"main"})
+                                if main:
+                                    main_text = main.text
+                                    desc_text = main_text.split()[:25]
+                                fm_description = desc = html.findAll(attrs={"name": "description"})
 
-                        # title
-                        title = html.title.string
-                        if not title:
-                            title = html.select('h1')[0].text.strip()
+                                if fm_description:
+                                    description = desc[0]['content'].encode('utf-8')
+                                else:
+                                    desc_text = " ".join(desc_text)
+                                    description = desc_text
 
-                        # description
-                        desc_text = ""
+                                # create url
+                                url_relpermalink = [item["data-relpermalink"] for item in html.find_all() if
+                                                    "data-relpermalink" in item.attrs]
+                                url = docs_host + str(url_relpermalink[0]) if url_relpermalink else docs_host
 
-                        if main:
-                            main_text = main.text
-                            desc_text = main_text.split()[:25]
-                        fm_description = desc = html.findAll(attrs={"name": "description"})
+                                # create article object
+                                article['objectID'] = dirpath + '/' + filename
+                                article['URL'] = url
+                                article['title'] = title
+                                article['body'] = description
+                                article['file'] = dirpath + '/' + filename
 
-                        if fm_description:
-                            description = desc[0]['content'].encode('utf-8')
-                        else:
-                            desc_text = " ".join(desc_text)
-                            description = desc_text
+                                articles.append(article)
 
-                        # create url
-                        url_relpermalink = [item["data-relpermalink"] for item in html.find_all() if
-                                            "data-relpermalink" in item.attrs]
-                        url = docs_host + str(url_relpermalink[0]) if url_relpermalink else docs_host
+                        except AttributeError:
+                            print('skipping %s, invalid html' % os.path.join(dirpath, filename))
+                            pass
 
-                        # create article object
-                        article['objectID'] = dirpath + '/' + filename
-                        article['URL'] = url
-                        article['title'] = title
-                        article['body'] = description
-                        article['file'] = dirpath + '/' + filename
+        return articles
 
-                        # i18n
-                        if lang == 'ja':
-                            index_japanese_articles.append(article)
-                        else:
-                            index_english_articles.append(article)
+    index_english_articles = crawl_site(content_path, dirs_exclude + languages)
+    index_japanese_articles = crawl_site(content_path + '/ja', dirs_exclude)
 
-            # save object
-            if index_english_articles:
-                index_english_build.clear_index()
-                try:
-                    index_english_build.save_objects(index_english_articles)
-                    client.copy_index(DOCS_BUILD_ENGLISH, DOCS_LIVE_ENGLISH)
-                    print('Algolia Docs Build objects have been successfully updated.')
-                except algoliasearch.AlgoliaException as e:
-                    print(e)
-                    exit(1)
+    # save to algolia
+    try:
+        if index_english_articles:
+            index_english_build.clear_index()
+            try:
+                index_english_build.save_objects(index_english_articles)
+                client.copy_index(DOCS_BUILD_ENGLISH, DOCS_LIVE_ENGLISH)
+                print('Algolia Docs Build objects have been successfully updated.')
+            except algoliasearch.AlgoliaException as e:
+                print(e)
+                exit(1)
 
-            if index_japanese_articles:
-                index_japanese_build.clear_index()
-                try:
-                    index_japanese_build.save_objects(index_japanese_articles)
-                    client.copy_index(DOCS_BUILD_JAPANESE, DOCS_LIVE_JAPANESE)
-                    print('Algolia JA Docs Build objects have been successfully updated.')
-                except algoliasearch.AlgoliaException as e:
-                    print(e)
-                    exit(1)
+        if index_japanese_articles:
+            index_japanese_build.clear_index()
+            try:
+                index_japanese_build.save_objects(index_japanese_articles)
+                client.copy_index(DOCS_BUILD_JAPANESE, DOCS_LIVE_JAPANESE)
+                print('Algolia JA Docs Build objects have been successfully updated.')
+            except algoliasearch.AlgoliaException as e:
+                print(e)
+                exit(1)
+        pass
 
     except ValueError as e:
         exit(str(e))
