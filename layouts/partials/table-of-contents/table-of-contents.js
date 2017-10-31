05a86e55e108a70c0126848331e7a66b22c1bbaf
david.jones <david.jones@datadoghq.com>
2017-10-31 08:00:03 -0700

    more nav work

--- layouts/partials/table-of-contents/table-of-contents.js
+++ layouts/partials/table-of-contents/table-of-contents.js
@@ -1,82 +1,68 @@
 $(document).ready(function () {
 
-    // when page ready collect mapping of link to headers so we aren't checking the dom all the time
-    var mapping = [];
+    if($('#TableOfContents ul').length) {
+        // when page ready collect mapping of link to headers so we aren't checking the dom all the time
+        var mapping = [];
 
-    function buildMap() {
-        mapping = [];
-        var link = null;
-        $('#TableOfContents ul a').each(function() {
-            var href = $(this).attr('href');
-            var id = href.replace('#', '').replace(' ','-');
-            var header = $('[id="'+id+'"]');
-            var navParentLinks = $(this).parents('#TableOfContents').find('ul > li').has($(this)).find('> a');
+        function buildMap() {
+            mapping = [];
+            var link = null;
+            $('#TableOfContents ul a').each(function() {
+                var href = $(this).attr('href');
+                var id = href.replace('#', '').replace(' ','-');
+                var header = $('[id="'+id+'"]');
+                var navParentLinks = $(this).parents('#TableOfContents').find('ul > li').has($(this)).find('> a');
 
-            if(header.length) {
-                mapping.push({
-                    'navLink': $(this),
-                    'navLinkPrev': link,
-                    'navParentLinks': navParentLinks,
-                    'id': id,
-                    'header': header,
-                    'isH2': header.is('h2'),
-                    'isH3': header.is('h3')
-                });
-            } else {
-                //console.log('could not find h2[id="'+id+'"]');
-            }
-            link = $(this);
-        });
-    }
+                if(header.length) {
+                    mapping.push({
+                        'navLink': $(this),
+                        'navLinkPrev': link,
+                        'navParentLinks': navParentLinks,
+                        'id': id,
+                        'header': header,
+                        'isH2': header.is('h2'),
+                        'isH3': header.is('h3')
+                    });
+                } else {
+                    //console.log('could not find h2[id="'+id+'"]');
+                }
+                link = $(this);
+            });
+        }
 
-    function onScroll() {
-        var winTop = $(window).scrollTop();
-        var localOffset = 120;
+        function onScroll() {
+            var winTop = $(window).scrollTop();
+            var localOffset = 120;
 
-        $('.toc_open').removeClass('toc_open');
-        for(var i = 0; i < mapping.length; i++) {
-            var obj = mapping[i];
-            var j = i+1;
-            if(j > mapping.length) { j = 0; }
-            var nextobj = mapping[j];
-            //console.log(winTop, obj.headerTop - localOffset, winTop >= obj.headerTop - localOffset);
-            obj.navLink.removeClass('toc_scrolled');
+            $('.toc_open').removeClass('toc_open');
+            for(var i = 0; i < mapping.length; i++) {
+                var obj = mapping[i];
+                var j = i+1;
+                if(j > mapping.length) { j = 0; }
+                var nextobj = mapping[j];
+                //console.log(winTop, obj.headerTop - localOffset, winTop >= obj.headerTop - localOffset);
+                obj.navLink.removeClass('toc_scrolled');
 
-            if( (winTop >= obj.header.offset().top - localOffset) && (winTop < nextobj.header.offset().top - localOffset) ) {
-                //console.log(obj.navLink);
-                obj.navLink.addClass('toc_scrolled');
-                // add toc open to parents of this toc_scrolled
-                //console.log(obj.navLink.parents('ul'));
-                obj.navParentLinks.each(function() {
-                    $(this).addClass('toc_open');
-                });
+                if( (winTop >= obj.header.offset().top - localOffset) && (winTop < nextobj.header.offset().top - localOffset) ) {
+                    //console.log(obj.navLink);
+                    obj.navLink.addClass('toc_scrolled');
+                    // add toc open to parents of this toc_scrolled
+                    //console.log(obj.navLink.parents('ul'));
+                    obj.navParentLinks.each(function() {
+                        $(this).addClass('toc_open');
+                    });
+                }
             }
         }
 
-        /*$('.sidenav ul li a').each(function() {
-            var el = $(this);
-            var elTop = el.offset().top;
-            var winTop = $(window).scrollTop();
-            var localOffset = 120;
-            var href = $(this).attr('href').replace('#', '').replace(' ','-');
-            var header = $('h2[id="'+href+'"]');
-            console.log(href, header);
-            if(header.length) {
-                var headerTop = header.offset().top;
-                if(winTop >= headerTop - localOffset){
-                    el.addClass('toc_scrolled').siblings().removeClass('toc_scrolled');
-                } else {
-                    el.removeClass('toc_scrolled');
-                }
-            }
-        });*/
-    }
+        $(window).on('resize scroll',function(e) {
+            onScroll();
+        }).trigger('scroll');
 
-    $(window).on('resize scroll',function(e) {
+
+        buildMap();
         onScroll();
-    }).trigger('scroll');
-
-
-    buildMap();
-    onScroll();
+    } else {
+        $('.toc-container > div').hide();
+    }
 });
