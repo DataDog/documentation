1fc72658e1727e46c916821403da9b03f2182a34
david.jones <david.jones@datadoghq.com>
2017-10-31 14:57:46 -0700

    get rid of dependency on jquery pajinate

--- content/search.md
+++ content/search.md
@@ -1,9 +1,13 @@
 ---
 title: Search Results
 kind: search
+disable_toc: true
 ---
 
 <div id="tipue_search_content">
     <div class="content"></div>
-    <div class="page_navigation"></div>
+    <div class="page_navigation">
+        <a class="btn btn-sm-tag btn-outline-secondary" href="#" id="btn_prev">Prev</a>
+        <a class="btn btn-sm-tag btn-outline-secondary" href="#" id="btn_next">Next</a>
+    </div>
 </div>
--- src/js/datadog-docs.js
+++ src/js/datadog-docs.js
@@ -94,14 +94,123 @@ $(document).ready(function () {
                 $('#tipue_search_content .content').html(formatted_results);
 
                 // load pagination
-                if (hits.length) {
-                    $('#tipue_search_content').pajinate({
-                        item_container_id: '.content',
-                        num_page_links_to_display: 9,
-                        items_per_page: 7,
-                        wrap_around: false,
-                        show_first_last: false
-                    });
+                if (hits.length)
+                {
+                    var current_page = 1;
+                    var num_page_links_to_display = 9;
+                    var items_per_page = 7;
+                    var btn_next = document.getElementById("btn_next");
+                    var btn_prev = document.getElementById("btn_prev");
+                    var listing_table = document.getElementsByClassName('content')[0];
+                    var page_navigation = document.getElementsByClassName('page_navigation')[0];
+                    var page_span = document.getElementById("page");
+
+                    function numPages() {
+                        return Math.ceil(hits.length / items_per_page);
+                    }
+
+                    function prevPage() {
+                        if (current_page > 1) {
+                            current_page--;
+                            changePage(current_page);
+                        }
+                    }
+
+                    function nextPage() {
+                        if (current_page < numPages()) {
+                            current_page++;
+                            changePage(current_page);
+                        }
+                    }
+
+                    function setHandlers() {
+                        btn_next = document.getElementById("btn_next");
+                        btn_prev = document.getElementById("btn_prev");
+                        btn_prev.addEventListener('click', function(e) {
+                            e.preventDefault();
+                            prevPage();
+                            return false;
+                        });
+                        btn_next.addEventListener('click', function(e) {
+                            e.preventDefault();
+                            nextPage();
+                            return false;
+                        });
+                        var pagebtns = document.getElementsByClassName('page-num');
+                        for(var i = 0; i < pagebtns.length; i++) {
+                            pagebtns[i].addEventListener('click', function(e) {
+                                e.preventDefault();
+                                var page = parseInt(e.target.getAttribute('data-pagenum'));
+                                if (page > numPages()) {
+                                    page = numPages();
+                                } else if(page <= 0) {
+                                    page = 1;
+                                }
+                                current_page = page;
+                                changePage(current_page);
+                                return false;
+                            });
+                        }
+                    }
+
+                    function setNavigation() {
+                        var count = Math.min(numPages(), num_page_links_to_display);
+                        var html = '';
+                        var cls = '';
+                        html += '<a class="mr-1 btn btn-sm-tag btn-outline-secondary" href="#" id="btn_prev">Prev</a>';
+                        for(var i = 1; i < count+1; i++) {
+                            cls = (current_page === i) ? 'active' : '';
+                            html += '<a class="mr-1 page-num btn btn-sm-tag btn-outline-secondary '+cls+'" href="#" data-pagenum="'+i+'">'+i+'</a>';
+                        }
+                        if(count < numPages()) {
+                            html += '<a class="mr-1 btn btn-sm-tag btn-outline-secondary more" href="#">...</a>';
+                        }
+                        html += '<a class="mr-1 btn btn-sm-tag btn-outline-secondary" href="#" id="btn_next">Next</a>';
+                        page_navigation.innerHTML = html;
+                        setHandlers();
+                    }
+
+                    function changePage(page)
+                    {
+                        page_span = document.getElementById("page");
+
+                        // Validate page
+                        if (page < 1) page = 1;
+                        if (page > numPages()) page = numPages();
+
+                        listing_table.innerHTML = "";
+
+                        for (var i = (page-1) * items_per_page; i < (page * items_per_page) && i < hits.length; i++) {
+                            var formatted_results = '';
+                            formatted_results += '<div class="hit">';
+                            formatted_results += '<div class="tipue_search_content_title">' +
+                                '<a href="' + hits[i]["URL"] + '">' + hits[i]["title"] + '</a></div>';
+                            formatted_results += '<div class="tipue_search_content_url">' +
+                                '<a href="' + hits[i]["URL"] + '">' + hits[i]["URL"].replace('https://docs.datadoghq.com', '') + '</a></div>';
+                            var text = hits[i].page_description;
+                            formatted_results += '<div class="tipue_search_content_text">' +
+                                text + '</div>';
+                            formatted_results += '</div>';
+                            listing_table.innerHTML += formatted_results;
+                        }
+                        if(page_span) {
+                            page_span.innerHTML = page + "/" + numPages();
+                        }
+
+                        // set previous and next buttons class
+                        btn_prev.classList[page === 1 ? 'add' : 'remove']('disabled');
+                        btn_next.classList[page === numPages() ? 'add' : 'remove']('disabled');
+
+                        // set active button class
+                        var pagebtns = document.getElementsByClassName('page-num');
+                        for(var i = 0; i < pagebtns.length; i++) {
+                            pagebtns[i].classList[current_page === i+1 ? 'add' : 'remove']('active');
+                        }
+                    }
+
+                    // set first page
+                    setNavigation();
+                    changePage(1);
                 }
 
             }
