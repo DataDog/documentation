efc845f21e420daebf95bcf005755be3c5bbfc55
david.jones <david.jones@datadoghq.com>
2017-11-06 09:47:06 -0800

    make search input retain search value if in url

--- layouts/partials/sidenav/sidenav.js
+++ layouts/partials/sidenav/sidenav.js
@@ -1,3 +1,18 @@
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 $(document).ready(function () {
+
+    var searchParam = getParameterByName('s');
+    if(searchParam) {
+        $('.sidenav-search input[name="s"]').val(searchParam);
+    }
 
 });
