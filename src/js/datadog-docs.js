a60f29b5cf759258dcc72c40f9f12c0c909c392a
david.jones <david.jones@datadoghq.com>
2017-11-06 21:14:21 -0800

    store some selectors for api page

--- src/js/datadog-docs.js
+++ src/js/datadog-docs.js
@@ -11,45 +11,50 @@ $(document).ready(function () {
     });
 
     // API page
-    // When language buttons are clicked, show all the code snippets
-    // from that language.
-    $('.lang-btn').on('click', function (e) {
-        var el = $(this);
+    if($('.api').length) {
+        // When language buttons are clicked, show all the code snippets
+        // from that language.
+        var code_blocks = $('.code-block');
+        var lang_blocks = $('.lang-specific');
+        var hs = $('h2[id]');
+        $('.lang-btn').on('click', function (e) {
+            var el = $(this);
 
-        // Find the element currently in the view port
-        var scrollElement;
-        $('h2[id]').each(function () {
-            if ($(this).offset().top >= window.scrollY) {
-                scrollElement = $(this);
-                return false;
+            // Find the element currently in the view port
+            var scrollElement;
+            hs.each(function () {
+                if ($(this).offset().top >= window.scrollY) {
+                    scrollElement = $(this);
+                    return false;
+                }
+            });
+
+            // Highlight the active button.
+            $('.lang-btn').removeClass('active');
+            el.addClass('active');
+
+            // Show this language's code blocks and language-specific elements
+            var lang = el.data('lang');
+            code_blocks.hide();
+            $('.code-block-' + lang).fadeIn();
+            lang_blocks.hide();
+            $('.lang-specific-' + lang).fadeIn();
+
+            // Scroll to the element that was in the viewport (ie retain location).
+            if(scrollElement) {
+                var id = scrollElement.attr('id');
+                moveToAnchor(id, false);
             }
+
+            // Add the language selection to the current URL.
+            if (history.pushState) {
+                var url = window.location.href.replace(window.location.hash, '').replace(window.location.search, '');
+                history.pushState(null, null, url + '?lang=' + lang + window.location.hash)
+            }
+
+            return false;
         });
-
-        // Highlight the active button.
-        $('.lang-btn').removeClass('active');
-        el.addClass('active');
-
-        // Show this language's code blocks and language-specific elements
-        var lang = el.data('lang');
-        $('.code-block').hide();
-        $('.code-block-' + lang).fadeIn();
-        $('.lang-specific').hide();
-        $('.lang-specific-' + lang).fadeIn();
-
-        // Scroll to the element that was in the viewport (ie retain location).
-        if(scrollElement) {
-            var id = scrollElement.attr('id');
-            moveToAnchor(id, false);
-        }
-
-        // Add the language selection to the current URL.
-        if (history.pushState) {
-            var url = window.location.href.replace(window.location.hash, '').replace(window.location.search, '');
-            history.pushState(null, null, url + '?lang=' + lang + window.location.hash)
-        }
-
-        return false;
-    });
+    }
 
     // algolia
     if (window.location.href.indexOf('/search/') > -1) {
