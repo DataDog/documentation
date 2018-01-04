f15fa84fbffc8385b2261d3ce21161c6aa25105b
Pierre Guceski <p.guceski@gmail.com>
2018-01-04 11:52:33 +0100

    adding python example

--- content/api/tracing/code_snippets/send_trace.py
+++ content/api/tracing/code_snippets/send_trace.py
@@ -1,29 +1,30 @@
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-# This is not yet supported by the python Client for Datadog API
-# Consult the curl example 
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
+import random
+import time 
+import requests
+
+# Create IDs.
+TRACE_ID=random.randint(1,1000000)
+SPAN_ID=random.randint(1,1000000)
+
+# Start a timer.
+START=int(time.time() * 1000000)
+
+# Do things...
+time.sleep(2)
+
+# Stop the timer.
+DURATION= int(time.time() * 1000000) - START
+
+# Send the traces.
+r = requests.put('http://localhost:8126/v0.3/traces', data = {\
+	"trace_id": TRACE_ID,\
+	"span_id": SPAN_ID,\
+	"name": "span_name",\
+	"resource": "/home",\
+	"service": "service_name",\
+	"type": "web",\
+	"start": START,\
+	"duration": DURATION})
+
+
+
