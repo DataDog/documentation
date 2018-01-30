e364ae54eeee4a2c8ae4282191e8a16c73494737
david.jones <david.jones@datadoghq.com>
2018-01-30 07:44:46 -0800

    fix some positioning when hiding banner

--- layouts/partials/announcement_banner/announcement_banner.js
+++ layouts/partials/announcement_banner/announcement_banner.js
@@ -4,11 +4,13 @@ $(document).ready(function () {
 
     if (announcement_banner !== 'closed') {
         document.getElementsByClassName('announcement_banner')[0].classList.add('open');
+        $('html').addClass('banner');
     }
 
     $('.announcement_banner .icon').on('click', function (e) {
         sessionStorage.setItem('announcement_banner', 'closed');
         $('.announcement_banner').removeClass('open');
+        $('html').removeClass('banner');
         $(window).trigger('scroll');
     });
 });
