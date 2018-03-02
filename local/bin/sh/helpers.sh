4f965ace55282e1bf0ba64491e30c571f4524903
david.jones <david.jones@datadoghq.com>
2018-03-02 07:55:17 -0800

    run the real deal

--- local/bin/sh/helpers.sh
+++ local/bin/sh/helpers.sh
@@ -27,7 +27,8 @@ version_static_assets() {
     if [ -f "gulpfile.js" ]; then  # only compress assets if gulp is installed and configured
         echo "--------"
         test -d "node_modules" || (echo "cp missing node_modules from /etc/node_modules"; cp -r /etc/node_modules .)
-        npm install  # make sure everything is uptodate
+        #npm install  # make sure everything is uptodate
+        npm install https://s3.amazonaws.com/origin-static-assets/corp-node-packages/master/translate-latest.tgz
         if [[ "$(get_bucket)" == *"preview"* ]]; then
             gulp build || fail_step "${FUNCNAME}"
         else
