eb17a5edd17759692e424e5984fa1fef05d75c9d
david.jones <david.jones@datadoghq.com>
2018-03-02 20:22:06 -0800

    clean cache too for now

--- local/bin/sh/helpers.sh
+++ local/bin/sh/helpers.sh
@@ -27,8 +27,7 @@ version_static_assets() {
     if [ -f "gulpfile.js" ]; then  # only compress assets if gulp is installed and configured
         echo "--------"
         #test -d "node_modules" || (echo "cp missing node_modules from /etc/node_modules"; cp -r /etc/node_modules .)
-        npm install  # make sure everything is uptodate
-        #npm install https://s3.amazonaws.com/origin-static-assets/corp-node-packages/master/translate-latest.tgz
+        npm cache clean && npm install  # make sure everything is uptodate
         if [[ "$(get_bucket)" == *"preview"* ]]; then
             gulp build || fail_step "${FUNCNAME}"
         else
