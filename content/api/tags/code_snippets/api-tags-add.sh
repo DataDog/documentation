6e9a9dbeb4d218f467ab281c90e679f1f1754341
Pierre Guceski <p.guceski@gmail.com>
2018-03-10 12:00:27 +0100

    fixing parameter import

--- content/api/tags/code_snippets/api-tags-add.sh
+++ content/api/tags/code_snippets/api-tags-add.sh
@@ -14,4 +14,4 @@ curl  -X POST -H "Content-type: application/json" \
 -d "{
       \"tags\" : [\"environment:production\", \"role:webserver\"]
 }" \
-"https://app.datadoghq.com/api/v1/tags/hosts/${host_name}?api_key=${api_key}&application_key=${app_key}&source=$source"
+"https://app.datadoghq.com/api/v1/tags/hosts/${host_name}?api_key=${api_key}&application_key=${app_key}&source=${source}"
