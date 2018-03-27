affdaff97e338641126e773d4036b2779266bfcd
Jennifer Pinkham <jpinkham@cpan.org>
2018-03-27 09:47:47 -0400

    Updated curl version of user creation to use 'handle' rather than 'id' for the only required argument.  Internally the python and ruby wrappers convert user-supplied 'id' into request field 'handle'. This is of course not an option with curl, which must match what API expects. WARN: Because there are no separate docs for python/ruby/curl, the docs cannot be modified, just for curl, to reflect this change. The developer must note the difference for themselves, in the example request.

--- content/api/users/code_snippets/api-user-create.sh
+++ content/api/users/code_snippets/api-user-create.sh
@@ -2,7 +2,7 @@ api_key=9775a026f1ca7d1c6c5af9d94d9595a4
 app_key=87ce4a24b5553d2e482ea8a8500e71b8ad4554ff
 
 curl -X POST -H "Content-type: application/json" \
-    -d '{"id":"test@datadoghq.com","name":"test user"}' \
+    -d '{"handle":"test@datadoghq.com","name":"test user"}' \
     "https://app.datadoghq.com/api/v1/user?api_key=${api_key}&application_key=${app_key}"
 
 
