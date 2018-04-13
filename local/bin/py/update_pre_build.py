5be25708b33a9c908b1f0d32cee2e84084de53f1
Pierre Guceski <p.guceski@gmail.com>
2018-04-13 10:03:37 +0200

    merging core et swap into system

--- local/bin/py/update_pre_build.py
+++ local/bin/py/update_pre_build.py
@@ -153,6 +153,8 @@ class PreBuild:
             'kube_dns': {'action': 'discard', 'target': 'none', 'remove_header': False},
             'kubernetes_state': {'action': 'discard', 'target': 'none', 'remove_header': False},
             'stride': {'action': 'discard', 'target': 'none', 'remove_header': False},
+            'system_core': {'action': 'discard', 'target': 'system', 'remove_header': False},
+            'system_swap': {'action': 'discard', 'target': 'system', 'remove_header': False},
             'hbase_regionserver': {'action': 'merge', 'target': 'hbase_master', 'remove_header': False},
         })
         self.initial_integration_files = glob.glob('{}*.md'.format(self.content_integrations_dir))
