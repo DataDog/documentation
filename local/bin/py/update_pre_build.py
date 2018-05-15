8f9ed9e9eb4b5b56f6534b83c4d0eab64bc58e60
Pierre Guceski <p.guceski@gmail.com>
2018-05-15 13:47:01 +0200

    fixing file name

--- local/bin/py/update_pre_build.py
+++ local/bin/py/update_pre_build.py
@@ -145,6 +145,7 @@ class PreBuild:
             'activemq_xml': {'action': 'merge', 'target': 'activemq', 'remove_header': False},
             'cassandra_nodetool': {'action': 'merge', 'target': 'cassandra', 'remove_header': False},
             'datadog_checks_base': {'action': 'discard', 'target': 'none', 'remove_header': False},
+            'datadog_checks_tests_helper': {'action': 'discard', 'target': 'none', 'remove_header': False},
             'gitlab_runner': {'action': 'merge', 'target': 'gitlab', 'remove_header': False},
             'hdfs_datanode': {'action': 'merge', 'target': 'hdfs', 'remove_header': False},
             'hdfs_namenode': {'action': 'merge', 'target': 'hdfs', 'remove_header': False},
