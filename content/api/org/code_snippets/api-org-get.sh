ec1bfbbcf35aa3709e91acfa750e581c891d06fb
cswatt <cecilia.watt@datadoghq.com>
2018-08-07 17:12:45 -0400

    Update api-org-get.sh

--- content/api/org/code_snippets/api-org-get.sh
+++ content/api/org/code_snippets/api-org-get.sh
@@ -1,4 +1,4 @@
 api_key=<YOUR_API_KEY>
 app_key=<YOUR_APP_KEY>
 
-curl -X GET "https://app.datadoghq.com/api/v1/org?api_key=<api_key_here>&application_key=<app_key_here>"
+curl -X GET "https://api.datadoghq.com/api/v1/org?api_key=<api_key_here>&application_key=<app_key_here>"
