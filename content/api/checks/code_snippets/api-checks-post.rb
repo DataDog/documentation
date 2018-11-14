4685f2967981eca1f777f2912e2633198c865189
Ruth Naebeck <ruthnaebeck@gmail.com>
2018-11-14 16:37:16 -0500

    Add tags examples for post a check run

--- content/api/checks/code_snippets/api-checks-post.rb
+++ content/api/checks/code_snippets/api-checks-post.rb
@@ -6,8 +6,8 @@ app_key = '<YOUR_APP_KEY>'
 
 dog = Dogapi::Client.new(api_key, app_key)
 
-# submitting events doesn 't require an application_key, so we 
+# submitting events doesn 't require an application_key, so we
 # don't bother setting it
 dog = Dogapi::Client.new(api_key)
 
-dog.service_check('app.is_ok', 'app1', 0, :message => 'Response: 200 OK')
+dog.service_check('app.is_ok', 'app1', 0, :message => 'Response: 200 OK', :tags => ['env:test'])
