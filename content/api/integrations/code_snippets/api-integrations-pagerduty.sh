64011d94bcddba7f7bef11821c62ec867eace41a
Lewis Soto <lewis.soto@datadoghq.com>
2018-12-04 11:55:07 -0500

    add more curl exaples

--- content/api/integrations/code_snippets/api-integrations-pagerduty.sh
+++ content/api/integrations/code_snippets/api-integrations-pagerduty.sh
@@ -1,3 +1,4 @@
+# Create a new configuration.
 curl -v -X PUT -H "Content-type: application/json" \
 -d '{
       "services": [
@@ -16,6 +17,38 @@ curl -v -X PUT -H "Content-type: application/json" \
   }' \
 "https://api.datadoghq.com/api/v1/integration/pagerduty?api_key=${api_key}&application_key=${app_key}"
 
-curl -v "https://api.datadoghq.com/api/v1/integration/pagerduty?api_key=${api_key}&application_key=${app_key}"
+# Update an existing configration.
+# In this example, we are overriding `subdomain`, `api_token` and `schedules` values
+# with new values.
+curl -v -X PUT -H "Content-type: application/json" \
+-d '{
+      "subdomain": "<PAGERDUTY_SUB_DOMAIN>",
+      "schedules": ["<SCHEDULE_1>", "<SCHEDULE_2>"],
+      "api_token": "<PAGERDUTY_TOKEN>"
+  }' \
+"https://api.datadoghq.com/api/v1/integration/pagerduty?api_key=${api_key}&application_key=${app_key}"
 
-curl -v -X DELETE "https://api.datadoghq.com/api/v1/integration/pagerduty?api_key=${api_key}&application_key=${app_key}"
+# Append to the `services` and `schedules` list.
+curl -v -X POST -H "Content-type: application/json" \
+-d '{
+      "services": [
+        {
+          "service_name": "test_00",
+          "service_key": "<PAGERDUTY_SERVICE_KEY>"
+        },
+        {
+          "service_name": "test_01",
+          "service_key": "<PAGERDUTY_SERVICE_KEY>"
+        }
+      ],
+      "schedules": ["<SCHEDULE_1>", "<SCHEDULE_2>"],
+  }' \
+"https://api.datadoghq.com/api/v1/integration/pagerduty?api_key=${api_key}&application_key=${app_key}"
+
+# Get the integration configuration.
+curl -v -X GET \
+"https://api.datadoghq.com/api/v1/integration/pagerduty?api_key=${api_key}&application_key=${app_key}"
+
+# Delete the integration configuration.
+curl -v -X DELETE \
+"https://api.datadoghq.com/api/v1/integration/pagerduty?api_key=${api_key}&application_key=${app_key}"
