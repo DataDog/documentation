2655cb454109bd0936069ba9e9d4b5c8954896be
Siva Mahadevan <svmhdvn@gmail.com>
2018-12-10 14:25:24 -0500

    Testing githook

--- Makefile
+++ Makefile
@@ -134,8 +134,8 @@ hugpython/bin/activate: local/etc/requirements3.txt  ## start python virtual env
 source-helpers: hugpython  ## source the helper functions used in build, test, deploy.
 	@mkdir -p ${EXEDIR}
 	@find ${LOCALBIN}/*  -type f -exec cp {} ${EXEDIR} \;
-	@mkdir -p .git/hooks
 	@cp -r ${LOCALGITHOOKS}/* .git/hooks
+	@c++ -Wall -Werror -O2 local/etc/format-links.cpp -o local/bin/format-links
 
 start: clean source-helpers ## start the gulp/hugo server.
 	@echo "starting up..."
--- local/githooks/pre-commit.d/format-links.sh
+++ local/githooks/pre-commit.d/format-links.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 shortcodes=$(grep -FRl Inner layouts/shortcodes | sed -E -e 's/[^[:space:]]+\///' -e 's/\.html$//')
-changed=$(git diff --name-only --cached | grep '\.md$')
+changed=$(git diff --name-only --staged | grep '\.md$')
 for f in $changed
 do
     if local/bin/format-links "$f" "$shortcodes" > "$f.fixed"; then
         echo "$f: Successfully formatted links."
         mv "$f.fixed" "$f"
+        git add "$f"
     else
         echo "$f: Failed to format links, check stderr." 1>&2
         rm "$f.fixed"
