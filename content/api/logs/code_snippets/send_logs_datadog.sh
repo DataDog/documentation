5417fde5554ea773829f6fef5d9da7df9311e243
Pierre Guceski <p.guceski@gmail.com>
2018-12-21 16:23:10 +0100

    changing default example to DD US

--- content/api/logs/code_snippets/send_logs_datadog.sh
+++ content/api/logs/code_snippets/send_logs_datadog.sh
@@ -1,12 +1,12 @@
 ## Simple Raw Message
 
-curl -X POST https://http-intake.logs.datadoghq.eu/v1/input/<API_KEY> \
+curl -X POST https://http-intake.logs.datadoghq.com/v1/input/<API_KEY> \
      -H "Content-Type: text/plain" \
      -d 'hello world'
 
 ## Multi Raw Messages
 
-curl -X POST https://http-intake.logs.datadoghq.eu/v1/input/<API_KEY> \
+curl -X POST https://http-intake.logs.datadoghq.com/v1/input/<API_KEY> \
      -H "Content-Type: text/plain" \
      --data-binary @- <<BODY
 hello
@@ -15,13 +15,13 @@ BODY
 
 ## Simple JSON Message
 
-curl -X POST https://http-intake.logs.datadoghq.eu/v1/input/<API_KEY> \
+curl -X POST https://http-intake.logs.datadoghq.com/v1/input/<API_KEY> \
      -H "Content-Type: application/json" \
      -d '{"message":"hello wolrd"}'
 
 ## Multi JSON Messages
 
-curl -X POST https://http-intake.logs.datadoghq.eu/v1/input/<API_KEY> \
+curl -X POST https://http-intake.logs.datadoghq.com/v1/input/<API_KEY> \
      -H "Content-Type: application/json" \
      --data-binary @- <<BODY
 [
@@ -36,13 +36,13 @@ BODY
 
 ## Simple Logplex Message
 
-curl -X POST https://http-intake.logs.datadoghq.eu/v1/input/<API_KEY> \
+curl -X POST https://http-intake.logs.datadoghq.com/v1/input/<API_KEY> \
      -H "Content-Type: application/logplex-1" \
      -d 'hello world'
 
 ## Multi Logplex Messages
 
-curl -X POST https://http-intake.logs.datadoghq.eu/v1/input/<API_KEY> \
+curl -X POST https://http-intake.logs.datadoghq.com/v1/input/<API_KEY> \
      -H "Content-Type: application/logplex-1" \
      --data-binary @- <<BODY
 hello
