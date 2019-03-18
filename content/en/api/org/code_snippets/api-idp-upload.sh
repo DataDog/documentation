2d11948bfab2a5f4173b3a5cfd194dcd5761127e
Chris Linstid <chris.linstid@datadoghq.com>
2019-03-18 11:56:22 -0400

    Update Documentation for Upload Idp Metadata API
    - Add option for new XML body
    - Use the correct basename for code snippets

--- content/en/api/org/code_snippets/api-idp-upload.sh
+++ content/en/api/org/code_snippets/api-idp-upload.sh
@@ -2,5 +2,14 @@ api_key=<YOUR_API_KEY>
 app_key=<YOUR_APP_KEY>
 public_id=axd2s
 
-curl -X POST -H "Content-Type: multipart/form-data" -F "idp_file=@metadata.xml" \
-"https://api.datadoghq.com/api/v1/org/${public_id}/idp_metadata?api_key=${api_key}&application_key=${app_key}"
+# Upload IdP file using multipart/form-data
+curl -X POST -H "Content-Type: multipart/form-data" \
+    -F "idp_file=@metadata.xml" \
+    "https://api.datadoghq.com/api/v1/org/${public_id}/idp_metadata?api_key=${api_key}&application_key=${app_key}"
+
+# OR
+
+# Upload IdP file using application/xml
+curl -X POST -H "Content-Type: application/xml" \
+    --data-binary "@metadata.xml" \
+    "https://api.datadoghq.com/api/v1/org/${public_id}/idp_metadata?api_key=${api_key}&application_key=${app_key}"
