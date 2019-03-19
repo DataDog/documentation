b6470f348e92c13d720e7472274eb05c79a073a9
Pierre Cariou <46612673+pcariou-dd@users.noreply.github.com>
2019-03-19 13:25:00 +0100

    Update list_logs.sh

--- content/en/api/logs/code_snippets/list_logs.sh
+++ content/en/api/logs/code_snippets/list_logs.sh
@@ -5,6 +5,6 @@
 api_key=<DD_API_KEY>
 app_key=<DD_APP_KEY>
 
-curl -X GET https://http-intake.logs.datadoghq.com/api/v1/logs-queries/list?api_key=${api_key}&application_key=${app_key}
+curl -X POST https://app.datadoghq.com/api/v1/logs-queries/list?api_key=${api_key}&application_key=${app_key}
      -H "Content-Type: application/json" \
      -d '{query: "service:nginx query -method:POST Windows",time: {offset: -3600, from: "2001-01-01 12:00:00", to: "2001-01-01 14:00:00"} sort: "desc", limit: 50}'
