a9d961163bb4b790fb4a16bf48deea6c7b5e844e
Gus <pierre.guceski@datadoghq.com>
2019-03-20 14:58:37 +0100

    updating request

--- content/en/api/logs/code_snippets/list_logs.sh
+++ content/en/api/logs/code_snippets/list_logs.sh
@@ -5,6 +5,7 @@
 api_key=<DD_API_KEY>
 app_key=<DD_APP_KEY>
 
-curl -X POST https://app.datadoghq.com/api/v1/logs-queries/list?api_key=${api_key}&application_key=${app_key}
-     -H "Content-Type: application/json" \
-     -d '{query: "service:nginx query -method:POST Windows",time: {offset: -3600, from: "2001-01-01 12:00:00", to: "2001-01-01 14:00:00"} sort: "desc", limit: 50}'
+curl -X POST \
+  'https://api.datadoghq.com/api/v1/logs-queries/list?api_key=${api_key}&application_key=${app_key}' \
+  -H 'content-type: application/json' \
+  -d '{"query": "service:nginx -@http.method:POST","time": {"from": "2019-03-20T09:00Z", "to": "2019-03-20T10:00Z"}, "sort": "desc", "limit": 50}'
