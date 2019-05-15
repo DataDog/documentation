e02ffcb0a1110918da9ed1baa31d56b21243ae86
david.jones <david.jones@datadoghq.com>
2019-05-15 15:53:21 -0400

    add script

---
+++ local/bin/sh/push_translations.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+GUACBOT_EMAILS=("guacbot@users.noreply.github.com" "robot-guacbot-guacamole@datadoghq.com")
+
+# get email of user that did the commit
+EMAIL=$(git show -s --format="%ae" HEAD)
+
+# get list of files that changed and save to file
+git log -m -1 --name-only --pretty="format:" $CI_COMMIT_SHA > changelist.txt
+
+# determine if this was a guacbot commit
+if [[ " ${GUACBOT_EMAILS[*]} " == *"$EMAIL"* ]];
+then
+    echo "Skipping, guacbot commits"
+else
+    echo "Ready to send translated files.."
+fi
