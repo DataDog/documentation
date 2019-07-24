34936b72213dc71d58a1d6a442eb1aceda2413c0
Paul Bauer <paul@bauer.codes>
2019-07-24 14:37:50 -0400

    use GET instead of POST in logs-query/list example

--- content/en/api/logs/code_snippets/list_logs.sh
+++ content/en/api/logs/code_snippets/list_logs.sh
@@ -5,7 +5,6 @@
 api_key=<DD_API_KEY>
 app_key=<DD_APP_KEY>
 
-curl -X POST \
-  "https://api.datadoghq.com/api/v1/logs-queries/list?api_key=${api_key}&application_key=${app_key}" \
+curl "https://api.datadoghq.com/api/v1/logs-queries/list?api_key=${api_key}&application_key=${app_key}" \
   -H 'content-type: application/json' \
   -d '{"query": "service:nginx -@http.method:POST","time": {"from": "now - 1h", "to": "now"}, "sort": "desc", "limit": 50}'
