0f092b21231b0914a946b37cf3b52c803b47d015
Pierre Guceski <p.guceski@gmail.com>
2019-07-30 17:40:53 +0200

    removing test_id in benefits of public_id

--- content/en/api/synthetics/code_snippets/get_result.sh
+++ content/en/api/synthetics/code_snippets/get_result.sh
@@ -1,6 +1,6 @@
 api_key=<YOUR_API_KEY>
 app_key=<YOUR_APP_KEY>
-test_id=<TEST_ID>
+public_id=<SYNTHETICS_TEST_PUBLIC_ID>
 result_id=<TEST_RESULT_ID>
 
-curl "https://api.datadoghq.com/api/v1/synthetics/tests/${test_id}/results/${result_id}?api_key=${api_key}&application_key=${app_key}"
+curl "https://api.datadoghq.com/api/v1/synthetics/tests/${public_id}/results/${result_id}?api_key=${api_key}&application_key=${app_key}"
