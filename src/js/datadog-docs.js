73d617920aae2f9afac62d5b1d75e087f1187bc5
Zach Bayoff <zrbayoff@gmail.com>
2019-07-30 11:12:24 -0400

    integrations fix

--- src/js/datadog-docs.js
+++ src/js/datadog-docs.js
@@ -623,6 +623,10 @@ $(document).ready(function () {
     onScroll();
 
 
+    // TODO: move integrations code to own file after webpack update
+    initializeIntegrations();
+
+
 });
 
 function updateMainContentAnchors(){
@@ -998,6 +1002,7 @@ function hasParentLi(el){
 }
 
 function getPathElement(){
+    var domain = window.location.origin;
     var path = window.location.pathname;
     var activeMenus = document.querySelectorAll('.side .sidenav-nav .active, header .sidenav-nav .active');
     
@@ -1060,14 +1065,20 @@ function getPathElement(){
         maPath = document.querySelector('header [data-path*="account_management/billing"]');
     }
 
-    if (path.includes('monitors/monitor_type/trace_search')) {
-        aPath = document.querySelector('.side [data-path*="monitors/monitor_type/trace_search"]');
-        maPath = document.querySelector('header [data-path*="monitors/monitor_type/trace_search"]');
-    }
+    if (path.includes('monitors/monitor_types/trace_analytics')) {
+        aPath = document.querySelector('.side [data-path*="monitors/monitor_types/trace_analytics"]');
+        maPath = document.querySelector('header [data-path*="monitors/monitor_types/trace_analytics"]');
+    } 
 
     if (path.includes('developers/guide')) {
         aPath = document.querySelector('.side [data-path*="developers/guide"]');
         maPath = document.querySelector('header [data-path*="developers/guide"]');
+    }
+
+    // if url is domain + /integrations/**
+    if ((replaceURL(domain) + '/' + path).includes(replaceURL(domain) + '/integrations/')) {
+        aPath = document.querySelector('.side [data-path="integrations"]');
+        maPath = document.querySelector('header [data-path="integrations"]');
     }
 
     if(aPath){
@@ -1128,7 +1139,6 @@ function updateSidebar(event){
 };
 
 function loadPage(newUrl) {
-
     // scroll to top of page on new page load
     window.scroll(0, 0);
 
@@ -1161,7 +1171,6 @@ function loadPage(newUrl) {
         }
 
         var newContent = httpRequest.responseXML.getElementById("mainContent");
-        console.log('httpRequest.responseXML: ', httpRequest.responseXML);
         var newTOC = httpRequest.responseXML.querySelector(".toc-container");
 
         newContent.classList.add('loading');
@@ -1232,6 +1241,7 @@ function loadPage(newUrl) {
                 tocWidthUpdate();
                 updateMainContentAnchors();
                 reloadWistiaVidScripts(wistiaVidId);
+                initializeIntegrations();
         } else if (!newTOC.querySelector('#TableOfContents')) {
             if (document.querySelector('.toc-container #TableOfContents')) {
                 document.querySelector('.toc-container #TableOfContents').remove();
@@ -1297,7 +1307,7 @@ function reloadWistiaVidScripts(vidId){
 
 var sideNav = document.querySelector('.side');
 
-sideNav.addEventListener('click', function (e){
+sideNav.addEventListener('click', function (event){
     
     event.stopPropagation();
     // console.log('clicked on ');
@@ -1339,10 +1349,10 @@ sideNav.addEventListener('click', function (e){
     if (typeof domain !== "string" || newUrl.search(domain) !== 0) {
         event.preventDefault();
         window.open(newUrl, "_blank");
-    } else if (loadViaAjax(e.target)) {
+    } else if (loadViaAjax(event.target)) {
         loadPage(newUrl);
         event.preventDefault();
-        history.pushState(null /*stateObj*/, "" /*title*/, newUrl);
+        history.pushState({}, "" /*title*/, newUrl);
         updateSidebar(event);
     } else {
         window.location.href = newUrl;
@@ -1351,10 +1361,10 @@ sideNav.addEventListener('click', function (e){
 });
 
 function loadViaAjax(element){
-    var hasClassOpen = element.closest('li').classList.contains('js-load') ? true : false;
+    var hasClassLoad = element.closest('li').classList.contains('js-load') ? true : false;
     var parentHasClassOpen = element.parentElement.classList.contains('js-load') ? true : false;
 
-    if (hasClassOpen) {
+    if (hasClassLoad) {
         return true;
     } else if (parentHasClassOpen){
         return true;
@@ -1383,16 +1393,248 @@ function replaceURL(input_url) {
 
 window.addEventListener('popstate', function(event) {
 
-    // if page is /api, dont load via ajax
     var domain = replaceURL(window.location.origin);
-    if (!window.location.href.includes(domain + '/api')) {
+    if (event.state) {
         loadPage(window.location.href)
         closeNav();
         getPathElement();
-        
-    } 
+    }
 
 }, false);
+
+// TODO: move Integrations code to own file after webpack update
+
+function initializeIntegrations(){
+    var finder_state = 0;  // closed
+    var container = document.querySelector('[data-ref="container"]');
+
+    $(window).on('focus', function () {
+        if (finder_state) {
+            container.classList.remove('find');
+            finder_state = 0;
+        }
+    });
+
+    Mousetrap.bind(['command+f', 'control+f'], function (e) {
+        if (!finder_state) {
+            container.classList.add('find');
+            finder_state = 1;
+        }
+    });
+
+    var is_safari = navigator.userAgent.indexOf('Safari') !== -1;
+    var ref = document.querySelector('.integration-popper-button');
+    var pop = document.getElementById('integration-popper');
+    if(ref && pop) {
+        ref.addEventListener('click', function(e) {
+            pop.style.display = (pop.style.display === 'none') ? 'block' : 'none';
+            var p = new Popper(ref, pop, {
+                placement: "start-bottom",
+                modifiers: {
+                    preventOverflow: { enabled: false },
+                    hide: {
+                        enabled: false
+                    }
+                }
+            });
+            return false;
+        });
+    }
+
+
+    var mobileBtn = document.querySelector('#dropdownMenuLink');
+    var controls = document.querySelector('[data-ref="controls"]');
+    var filters = null;
+    var mobilecontrols = document.querySelector('[data-ref="mobilecontrols"]');
+    var mobilefilters = null;
+    var sorts = document.querySelectorAll('[data-ref="sort"]');
+    var container = document.querySelector('[data-ref="container"]');
+    var search = document.querySelector('[data-ref="search"]');
+    var items = window.integrations;
+
+    if(!container) return;
+
+    var collection = Array.prototype.slice.call(container.querySelectorAll('.mix'));
+
+    if(controls) {
+        filters = controls.querySelectorAll('[data-ref="filter"]');
+    }
+    if(mobilecontrols) {
+        mobilefilters = mobilecontrols.querySelectorAll('[data-ref="filter"]');
+    }
+
+    var config = {
+        animation: {
+            duration: 150
+        },
+        selectors: {
+            target: '[data-ref="item"]' // Query targets with an attribute selector to keep our JS and styling classes seperate
+        },
+        load: {
+            dataset: items // As we have pre-rendered targets, we must "prime" MixItUp with their underlying data
+        },
+        data: {
+            uidKey: 'id' // Our data model must have a unique id. In this case, its key is 'id'
+        }
+    };
+
+    if(parseInt($(window).width()) <= 575) {
+        config['animation']['enable'] = false;
+    }
+
+    var mixer = mixitup(container, config);
+
+    controls.addEventListener('click', function(e) {
+        //e.preventDefault();
+        handleButtonClick(e.target, filters);
+        // trigger same active on mobile
+        //$('.integrations-select').val('#'+e.target.getAttribute('href').substr(1));
+        var mobileBtn = controls.querySelector('[data-filter="'+e.target.getAttribute('data-filter')+'"]');
+        activateButton(mobileBtn, mobilefilters);
+        //return false;
+    });
+
+    mobilecontrols.addEventListener('click', function(e) {
+        e.stopPropagation();
+        //e.preventDefault();
+        handleButtonClick(e.target, mobilefilters);
+        // trigger same active on desktop
+        var desktopBtn = controls.querySelector('[data-filter="'+e.target.getAttribute('data-filter')+'"]');
+        activateButton(desktopBtn, filters);
+        //return false;
+        pop.style.display = 'none';
+        $(window).scrollTop(0);
+    });
+    
+    var searchTimer;
+
+    search.addEventListener('input', function(e) {
+        var allBtn = controls.querySelector('[data-filter="all"]');
+
+        // search only executes after user is finished typing
+        clearTimeout(searchTimer);
+        searchTimer = setTimeout(function() {
+            activateButton(allBtn, filters);
+            updateData(e.target.value.toLowerCase(), true)
+        }, 400);
+    });
+
+    function activateButton(activeButton, siblings) {
+        var button;
+        var i;
+
+        if(activeButton && siblings) {
+            for (i = 0; i < siblings.length; i++) {
+                button = siblings[i];
+                button.classList[button === activeButton ? 'add' : 'remove']('active');
+            }
+            mobileBtn.textContent = activeButton.textContent;
+        }
+    }
+
+    function handleButtonClick(button, filters) {
+        // clear the search input
+        search.value = "";
+        // If button is already active, or an operation is in progress, ignore the click
+        if (button.classList.contains('active') || !button.getAttribute('data-filter')) return;
+
+        var filter = button.getAttribute('data-filter');
+        activateButton(button, filters);
+        updateData(filter, false);
+
+        if (history.pushState) {
+            var href = button.getAttribute('href');
+            history.pushState({}, document.title, href);
+        }
+    }
+
+    function updateData(filter, isSearch) {
+        var show = [];
+        var hide = [];
+        for(var i = 0; i < window.integrations.length; i++) {
+            var item = window.integrations[i];
+            var domitem = document.getElementById('mixid_'+item.id);
+            if(filter === 'all' || filter === '#all' || (isSearch && !filter)) {
+                if(!is_safari) {
+                    domitem.classList.remove('grayscale');
+                }
+                show.push(item);
+            } else {
+                var name = item.name ? item.name.toLowerCase() : '';
+                var public_title = item.public_title ? item.public_title.toLowerCase() : '';
+
+                if (filter
+                    && (isSearch && (name.includes(filter) || public_title.includes(filter)))
+                    || (!isSearch && item.tags.indexOf(filter.substr(1)) !== -1)) {
+                    if(!is_safari) {
+                        domitem.classList.remove('grayscale');
+                    }
+                    show.push(item);
+                } else {
+                    if(!is_safari) {
+                        domitem.classList.add('grayscale');
+                    }
+                    hide.push(item);
+                }
+            }
+        }
+        var items = [].concat(show, hide);
+        mixer.dataset(items).then(function(state) {
+            if(is_safari) {
+                for(var i = 0; i < window.integrations.length; i++) {
+                    var item = window.integrations[i];
+                    var domitem = document.getElementById('mixid_'+item.id);
+                    if (filter === 'all' || filter === '#all' || (isSearch && !filter)) {
+                        domitem.classList.remove('grayscale');
+                        //show.push(item);
+                    } else {
+                        var name = item.name ? item.name.toLowerCase() : '';
+                        var public_title = item.public_title ? item.public_title.toLowerCase() : '';
+
+                        if (filter
+                            && (isSearch && (name.includes(filter) || public_title.includes(filter)))
+                            || (!isSearch && item.tags.indexOf(filter.substr(1)) !== -1)) {
+                            domitem.classList.remove('grayscale');
+                            //show.push(item);
+                        } else {
+                            domitem.classList.add('grayscale');
+                            //hide.push(item);
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    // Set controls the active controls on startup
+    activateButton(controls.querySelector('[data-filter="all"]'), filters);
+    activateButton(mobilecontrols.querySelector('[data-filter="all"]'), mobilefilters);
+
+    $(window).on('hashchange', function(){
+        var current_cat = "";
+        if (window.location.href.indexOf("#") > -1) {
+            current_cat = window.location.href.substring(window.location.href.indexOf("#"));
+        }
+        var current_selected = $('.controls .active').attr('href');
+        if(current_cat && current_selected) {
+            if (current_cat !== current_selected) {
+                $('a[href="' + current_cat + '"]').get(0).click();
+            }
+        }
+        if(current_cat === "") {
+            activateButton(controls.querySelector('[data-filter="all"]'), filters);
+            activateButton(mobilecontrols.querySelector('[data-filter="all"]'), mobilefilters);
+            updateData("all", false);
+        }
+    });
+
+    if (window.location.href.indexOf("#") > -1) {
+        $(window).trigger('hashchange');
+    }
+
+}
+
+
 
 
 
