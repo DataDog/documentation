f1b5ff25967060fefa15512b64739248c418b4cc
Pierre Cariou <46612673+pcariou-dd@users.noreply.github.com>
2019-08-07 09:58:23 +0200

    add example for pagination in logs list API

--- content/en/api/logs/code_snippets/list_logs.sh
+++ content/en/api/logs/code_snippets/list_logs.sh
@@ -8,4 +8,4 @@ app_key=<DD_APP_KEY>
 curl -X POST \
   "https://api.datadoghq.com/api/v1/logs-queries/list?api_key=${api_key}&application_key=${app_key}" \
   -H 'content-type: application/json' \
-  -d '{"query": "service:nginx -@http.method:POST","time": {"from": "now - 1h", "to": "now"}, "sort": "desc", "limit": 50}'
+  -d '{"query": "service:nginx -@http.method:POST","time": {"from": "now - 1h", "to": "now"}, "sort": "desc", "limit": 50, "startAt": "AAAAAWgN8Xwgr1vKDQAAAABBV2dOOFh3ZzZobm1mWXJFYTR0OA"}'
