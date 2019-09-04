d68cb30728724c0f67c602a14416f222283b4cbb
Zach Bayoff <zrbayoff@gmail.com>
2019-09-04 10:18:22 -0400

    test pa11y

--- local/bin/js/pa11y.js
+++ local/bin/js/pa11y.js
@@ -1,126 +1,114 @@
 const pa11y = require('pa11y');
-const path = require('path');
 const fs = require('fs');
 const asyncPool = require('tiny-async-pool');
 const { Parser } = require('json2csv');
+const glob = require('glob');
 
+// array containing all html files to test
 const fileArray = [];
-
-// some html files in public/ have weird characters, pa11y fails to read them, must exclude.
-const fileExclusions = [
-    // '&ap=434&fe=21625&dc=11210&at=GUdVQ18ZT08%3D&jsonp=NREUM.setToken',
-    // '%E2%80%8E',
-    // '%C2%A0%E2%80%A6',
-    // '%EF%BC%89%E4%B8%8A',
-    // '%20%5Bon%20our%20site%5D',
-    // 'content-assets'
-];
-
-// create array of all html files in starting folder
-fromDir('./public', '.html');
-
-function fromDir(startPath, filter) {
-    // console.log('Starting from dir '+startPath+'/');
-
-    if (!fs.existsSync(startPath)) {
-        console.log('no dir ', startPath);
-        return;
-    }
-
-    const files = fs.readdirSync(startPath);
-    for (let i = 0; i < files.length; i++) {
-        const filename = path.join(startPath, files[i]);
-        const stat = fs.lstatSync(filename);
-        if (stat.isDirectory()) {
-            fromDir(filename, filter); // recurse
-        } else if (filename.indexOf(filter) >= 0) {
-            const absFilePath = `./${filename}`;
-
-            const isValidHTML = fileExclusions.every(function(
-                currVal,
-                index,
-                arr
-            ) {
-                // console.log('curr val: ', currVal);
-                if (absFilePath.includes(currVal)) {
-                    return false;
-                }
-                return true;
-            });
-
-            if (
-                filename &&
-                typeof filename === 'string' &&
-                fs.existsSync(absFilePath) &&
-                isValidHTML
-            ) {
-                fileArray.push(absFilePath);
-            }
-        }
-    }
-}
-
-const testUrls = [
-    './public/about/press/index.html',
-    './public/about/team/index.html'
-];
 
 const pa11yConfig = {
     // rules to ignore
+    chromeLaunchConfig: {
+        ignoreHTTPSErrors: false,
+        args: ['--no-sandbox'] // for launching a "headless" chrome browser in CI
+    },
+    timeout: 200000,
     ignore: [
-        'WCAG2AA.Principle1.Guideline1_3.1_3_1.H49.AlignAttr', // align="left" attribute added by hugo in md tables. can't remove. 
+        'WCAG2AA.Principle1.Guideline1_3.1_3_1.H49.AlignAttr', // align="left" attribute added by hugo in md tables. can't remove.
         'WCAG2AA.Principle3.Guideline3_2.3_2_2.H32.2', // for mkto forms without 'submit' attribute
-        'WCAG2AA.Principle2.Guideline2_4.2_4_1.G1,G123,G124.NoSuchID' // we use id hashes for filtering, should change to query params in future
+        'WCAG2AA.Principle1.Guideline1_4.1_4_3.G18.Fail', // color contrast issues, too many incorrect results
+        'WCAG2AA.Principle4.Guideline4_1.4_1_2.H91.InputSearch.Name' // can't update Algolia's searchBox componenet to include this attribute
     ]
-}
+};
 
-// takes each url and runs pa11y command with config options.
-function pally(url){
-    return pa11y(url, pa11yConfig)
-}
+// some html files in dist have weird characters, pa11y fails to read them, must exclude.
+const fileExclusions = '';
 
+// remove directories with many html files that share the same layout and add only single files from each to speed up pa11y process
+// const directoryExlcusions = 'api|fr|ja|config|account_management|agent|api|basic_agent_usage|developers|examples|getting_started|graphing|guides|infrastructure|integrations|logs|monitors|synthetics|tracing|videos';
+const directoryExlcusions = 'api|fr|ja|config';
+
+// html files to include
+const includeUrls = [
+    './public/api/index.html'
+//     './public/account_management/api-app-keys/index.html',
+//     './public/agent/apm/index.html',
+//     './public/api/authentication/index.html',
+//     './public/basic_agent_usage/index.html',
+//     './public/developers/guide/index.html',
+//     './public/examples/aws-metric/index.html',
+//     './public/getting_started/agent/index.html',
+//     './public/graphing/dashboards/index.html',
+//     './public/guides/alerting/index.html',
+//     './public/infrastructure/index.html',
+//     './public/integrations/kubernetes/index.html',
+//     './public/logs/analytics/index.html',
+//     './public/monitors/check_summary/index.html',
+//     './public/synthetics/api_tests/index.html',
+//     './public/tracing/advanced/index.html',
+//     './public/videos/anomaly_detection/index.html'
+];
+
+const globPatterns = `./public/!(${directoryExlcusions}|${fileExclusions})/**/*.html`;
+
+fileArray.push(...glob.sync(globPatterns));
+fileArray.push(...includeUrls);
+
+// replace './public' with base url
+const absFileArray = fileArray.map(url => url.replace('./public', 'https://docs.datadoghq.com'));
 
 /**
-* Promise pool function to run pa11y command (which returns a promise)
-* on array of urls
-*
-* @param {Number} - concurrency limit
-* @param {Array} - array of urls on whic to run pa11y
-* @param {Function} - Iterator function to be called for each url
-*/ 
-asyncPool(3, fileArray, pally).then(results => {
+ * Promise pool function to run pa11y command (which returns a promise)
+ * on array of urls
+ *
+ * @param {Number} - concurrency limit
+ * @param {Array} - array of urls on which to run pa11y
+ * @param {Function} - Iterator function to be called for each url
+ */
+asyncPool(2, absFileArray, pally)
+    .then(results => {
+        const cleanResults = removeBadResults(results);
 
-    const cleanResults = removeBadResults(results);
-    const numIssues = cleanResults.reduce(
-        (accum, currVal) => accum + currVal.issues.length,
-        0
-    );
+        const numIssues = cleanResults.reduce(
+            (accum, currVal) => accum + currVal.issues.length,
+            0
+        );
 
-    console.log(
-        'Number of accessibility issues across all pages are: ',
-        numIssues
-    );
+        console.log(numIssues);
 
-    const fields = [
-        'documentTitle',
-        'pageUrl',
-        'issues.type',
-        'issues.code',
-        'issues.message',
-        'issues.context',
-        'issues.selector'
-    ];
-    const json2csvParser = new Parser({ fields, unwind: ['issues'] });
-    const csv = json2csvParser.parse(cleanResults);
+        const fields = [
+            'documentTitle',
+            'pageUrl',
+            'issues.type',
+            'issues.code',
+            'issues.message',
+            'issues.context',
+            'issues.selector'
+        ];
+        const json2csvParser = new Parser({
+            fields,
+            unwind: ['issues']
+        });
 
-    fs.writeFile('pa11y-output.csv', csv, function(err) {
-        // currently saves file to app's root directory
-        if (err) throw err;
-        console.log('file saved');
+        // choice to output pa11y results as csv or json.
+        const csvResults = json2csvParser.parse(cleanResults);
+        const jsonResults = JSON.stringify(cleanResults);
+
+        fs.writeFile('pa11y-output-docs.csv', csvResults, function(err) {
+            // currently saves file to app's root directory
+            if (err) throw err;
+        });
+    })
+    .catch(err => {
+        console.log(err);
     });
-}).catch(err => {
-    console.log(err);
-})
+
+// takes each url and runs pa11y command with config options.
+function pally(url) {
+    console.log('url: ', url);
+    return pa11y(url, pa11yConfig);
+}
 
 // sometimes results come back with null issues and a chrome error, need to filter
 function removeBadResults(results) {
