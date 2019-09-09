1dbd92af1cbb0a3d76483e403bc4f849bb6e9ff5
Pierre Guceski <p.guceski@gmail.com>
2019-09-09 08:45:39 +0200

    fixing script

--- local/bin/py/missing_metrics.py
+++ local/bin/py/missing_metrics.py
@@ -46,7 +46,7 @@ def get_dd_metrics(csv_metrics, keys):
   dd_metrics = {}
 
   try:
-    dd_metrics = api.Metric.list(from_time).get('metrics')
+    dd_metrics = api.Metric.list(from_time).get('metrics', {})
   except:
     print('\x1b[31mERROR\x1b[0m: There was an error with the Datadog API call, current value of dd_metrics is: \n {}'.format(dd_metrics))
 
@@ -84,7 +84,7 @@ def post_dd_metrics(metrics, keys):
 
 if __name__ == '__main__':
   print('\x1b[32mINFO\x1b[0m: Getting csv metrics from...')
-  print(tempdir)
+  #print(tempdir)
   csv_metrics = get_csv_metrics(tempdir)
   #print(csv_metrics)
   print('\x1b[32mINFO\x1b[0m: Parsing keys')
@@ -96,5 +96,9 @@ if __name__ == '__main__':
   options, args = parser.parse_args()
   print('\x1b[32mINFO\x1b[0m: Getting dd metrics...')
   metrics = get_dd_metrics(csv_metrics, options)
-  print('\x1b[32mINFO\x1b[0m: Posting dd metrics...')
-  post_dd_metrics(metrics, options)
+
+  if len(metrics) != 0:
+    print('\x1b[32mINFO\x1b[0m: Posting dd metrics...')
+    post_dd_metrics(metrics, options)
+  else:
+    print('\x1b[31mERROR\x1b[0m: List of metrics recovered was empty.')
