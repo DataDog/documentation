94b760d977eda36c6124d0cd679acb3a386a2621
Pierre Guceski <p.guceski@gmail.com>
2019-09-09 13:47:22 +0200

    last_reported_time is a unix epoch

--- content/en/api/hosts/code_snippets/result.api-hosts-totals.rb
+++ content/en/api/hosts/code_snippets/result.api-hosts-totals.rb
@@ -1,1 +1,1 @@
-["200", {"total_up":1750,"total_active":1759, "last_reported_time": "2019-06-08T13:20:00Z"}]
+["200", {"total_up":1750,"total_active":1759, "last_reported_time": 1560000000}]
--- content/en/api/hosts/code_snippets/result.api-hosts-totals.sh
+++ content/en/api/hosts/code_snippets/result.api-hosts-totals.sh
@@ -1,1 +1,1 @@
-{"total_up":1750,"total_active":1759, "last_reported_time": "2019-06-08T13:20:00Z"}
+{"total_up":1750,"total_active":1759, "last_reported_time": 1560000000}
