3ef733022baeaf1f6644635ae551bb0fbda15287
Ricky Thomas <ricky@datadoghq.com>
2019-11-05 15:41:41 -0500

    Add documentation for aws logs check services endpoint

---
+++ content/en/api/integrations_aws_logs/code_snippets/aws_logs_check_services.py
@@ -0,0 +1,13 @@
+from datadog import initialize, api
+
+options = {
+    'api_key': '<DD_API_KEY>',
+    'app_key': '<DD_APP_KEY>'
+}
+
+initialize(**options)
+
+account_id = "<AWS_ACCOUNT_ID>"
+lambda_arn = "arn:aws:lambda:<REGION>:<AWS_ACCOUNT_ID>:function:<FUNCTION_NAME>"
+
+api.AwsLogs.check_lambda(account_id=account_id, lambda_arn=lambda_arn)
