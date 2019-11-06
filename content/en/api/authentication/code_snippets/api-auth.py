18d27e281227721fb4d7c21d5846f54df613d2fd
ali-shaw <ali.shaw@datadoghq.com>
2019-11-06 10:18:52 +0000

    Update content/en/api/authentication/code_snippets/api-auth.py
    Co-Authored-By: Pierre Guceski <pierre.guceski@datadoghq.com>

--- content/en/api/authentication/code_snippets/api-auth.py
+++ content/en/api/authentication/code_snippets/api-auth.py
@@ -2,6 +2,6 @@ from datadog import initialize, api
 
 options = {'api_key': '<YOUR_API_KEY>',
            'app_key': '<YOUR_APP_KEY>',
-           'api_host': 'https://api.datadoghq.eu` #optional}
+           'api_host': 'https://api.datadoghq.com`}
 
 initialize(**options)
