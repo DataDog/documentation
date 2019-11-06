032a11208d764008f622e308283f828a54ecac8d
ali-shaw <ali.shaw@datadoghq.com>
2019-11-06 10:10:22 +0000

    update auth-api.py for EU
    Related to https://github.com/DataDog/documentation/pull/5846 updating to include `api_host` code example for EU customers

--- content/en/api/authentication/code_snippets/api-auth.py
+++ content/en/api/authentication/code_snippets/api-auth.py
@@ -1,6 +1,7 @@
 from datadog import initialize, api
 
 options = {'api_key': '<YOUR_API_KEY>',
-           'app_key': '<YOUR_APP_KEY>'}
+           'app_key': '<YOUR_APP_KEY>',
+           'api_host': 'https://api.datadoghq.eu` #optional}
 
 initialize(**options)
