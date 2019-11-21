a44b07e9d27ce4f9f0259fcb104abbb9a7438012
Pierre Guceski <p.guceski@gmail.com>
2019-11-21 10:41:08 +0100

    fixing date on non MacOS

--- content/en/api/graphs/code_snippets/api-graph-snapshot.sh
+++ content/en/api/graphs/code_snippets/api-graph-snapshot.sh
@@ -5,11 +5,11 @@
 api_key=<YOUR_API_KEY>
 app_key=<YOUR_APP_KEY>
 
-currenttime=$(date +%s)
-currenttime2=$(date -v -1d +%s)
+end_date=$(date +%s)
+start_date=$(($end_date - 86400))
 
 curl -X GET \
 -H "Content-type: application/json" \
 -H "DD-API-KEY: ${api_key}" \
 -H "DD-APPLICATION-KEY: ${app_key}" \
-"https://api.datadoghq.com/api/v1/graph/snapshot?metric_query=system.load.1\{*\}&start=${currenttime2}&end=${currenttime}"
+"https://api.datadoghq.com/api/v1/graph/snapshot?metric_query=system.load.1\{*\}&start=${start_date}&end=${end_date}"
