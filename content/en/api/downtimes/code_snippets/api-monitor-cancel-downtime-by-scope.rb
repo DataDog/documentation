3ab01c393607fcda531646fe2c191398085a3dde
Andrew McBurney <andrew@mcburney.email>
2019-11-27 10:16:33 -0500

    Run `rubocop -a` on the downtime code examples, and updated some snippets.

--- content/en/api/downtimes/code_snippets/api-monitor-cancel-downtime-by-scope.rb
+++ content/en/api/downtimes/code_snippets/api-monitor-cancel-downtime-by-scope.rb
@@ -1,2 +1,2 @@
 # This is not yet supported by the Ruby Client for Datadog API
-# Consult the curl example 
+# Consult the curl example
--- content/en/api/downtimes/code_snippets/result.api-monitor-schedule-downtime.rb
+++ content/en/api/downtimes/code_snippets/result.api-monitor-schedule-downtime.rb
@@ -1,24 +1,24 @@
 [
-    "200", {
-        "disabled" => false,
-        "canceled" => nil,
-        "active" => true,
-        "message" => nil,
-        "id" => 169267581,
-        "end" => 1445978819,
-        "parent_id" => nil,
-        "monitor_id" => nil,
-        "monitor_tags": ["*"],
-        "recurrence" => {
-            "until_date" => 1448387219,
-            "until_occurrences" => nil,
-            "week_days" => ["Mon", "Tue", "Wed", "Thu", "Fri"],
-            "type" => "weeks",
-            "period" => 1
-        },
-        "start" => 1445968019,
-        "creator_id" => 3658,
-        "scope" => ["env:testing"],
-        "updater_id" => nil
-    }
+  '200', {
+    'disabled' => false,
+    'canceled' => nil,
+    'active' => true,
+    'message' => nil,
+    'id' => 169_267_581,
+    'end' => 1_445_978_819,
+    'parent_id' => nil,
+    'monitor_id' => nil,
+    "monitor_tags": ['*'],
+    'recurrence' => {
+      'until_date' => 1_448_387_219,
+      'until_occurrences' => nil,
+      'week_days' => %w[Mon Tue Wed Thu Fri],
+      'type' => 'weeks',
+      'period' => 1
+    },
+    'start' => 1_445_968_019,
+    'creator_id' => 3658,
+    'scope' => ['env:testing'],
+    'updater_id' => nil
+  }
 ]
