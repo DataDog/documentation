335665aedd11d3e15d04b1904fbcea589f5d0867
Andrew McBurney <andrew@mcburney.email>
2019-11-27 14:43:19 -0500

    Add example for the monitor validation endpoint using the Python client.

--- content/en/api/monitors/code_snippets/api-monitor-validate.py
+++ content/en/api/monitors/code_snippets/api-monitor-validate.py
@@ -1,2 +1,19 @@
-# This is not yet supported by the Python Client for Datadog API
-# Consult the curl example
+from datadog import initialize, api
+
+options = {
+    "api_key": "<YOUR_API_KEY>",
+    "app_key": "<YOUR_APP_KEY>"
+}
+
+initialize(**options)
+
+monitor_type = "metric alert"
+query = "avg(last_1h):sum:system.net.bytes_rcvd{host:host0} > 200"
+monitor_options = {"thresholds": {"critical": 90.0}}
+
+# Validate a monitor's definition
+api.Monitor.validate(
+  type=monitor_type,
+  query=query,
+  options=monitor_options,
+)
