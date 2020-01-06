6278e4e54d045381603dabf4c38cbc39f21ef695
Pierre Guceski <p.guceski@gmail.com>
2020-01-06 13:32:15 +0100

    fixing example layout to be consistent

--- content/en/api/tags/code_snippets/api-tags-get-host.sh
+++ content/en/api/tags/code_snippets/api-tags-get-host.sh
@@ -1,8 +1,9 @@
 api_key=<YOUR_API_KEY>
 app_key=<YOUR_APP_KEY>
 
+host_name= "<HOSTNAME>"
 
 curl -X GET \
 -H "DD-API-KEY: ${api_key}" \
 -H "DD-APPLICATION-KEY: ${app_key}" \
-"https://api.datadoghq.com/api/v1/tags/hosts/<HOST_NAME>"
+"https://api.datadoghq.com/api/v1/tags/hosts/${host_name}"
