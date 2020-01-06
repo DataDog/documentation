136b3e52c2d61506da93775b93d442bd858ca79d
Pierre Guceski <p.guceski@gmail.com>
2020-01-06 13:48:34 +0100

    removing disclaimer

--- content/en/api/key_management/code_snippets/create_app_key.sh
+++ content/en/api/key_management/code_snippets/create_app_key.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-# Make sure you replace the <DATADOG_API_KEY> and <DATADOG_APPLICATION_KEY> key below
-# with the ones for your account
 
 api_key="<DATADOG_API_KEY>"
 app_key="<DATADOG_APPLICATION_KEY>"
