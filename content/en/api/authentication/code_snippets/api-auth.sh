eeb48fb88eacf6d4b8722fbe3fa9cddc0f1d0393
Pierre Guceski <p.guceski@gmail.com>
2020-01-07 08:01:46 +0100

    fixing placeholder

--- content/en/api/authentication/code_snippets/api-auth.sh
+++ content/en/api/authentication/code_snippets/api-auth.sh
@@ -1,3 +1,3 @@
 curl "https://api.datadoghq.com/api/v1/validate" \
     -H "DD-API-KEY: <DATADOG_API_KEY>" \
-    -H "DD-APPLICATION-KEY: <YOUR_APPLICATION_KEY>"
+    -H "DD-APPLICATION-KEY: <DATADOG_APPLICATION_KEY>"
