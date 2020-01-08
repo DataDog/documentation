7955017a340d8ffdb1febd5c7efca0b9ad958192
zbayoff <zrbayoff@gmail.com>
2020-01-08 09:47:53 -0500

    fix algoliajs for api page

--- src/scripts/components/algolia.js
+++ src/scripts/components/algolia.js
@@ -5,6 +5,19 @@ import configDocs from '../config/config-docs';
 const { env } = document.documentElement.dataset;
 const lang = document.documentElement.lang || 'en';
 
+// Set baseUrl based on environment
+let baseUrl = window.location.origin;
+const uri = window.location.pathname;
+const subdomain = baseUrl.split('.')[0];
+const previewPath = uri
+    .split('/')
+    .slice(0, 3)
+    .join('/');
+
+if (subdomain.includes('docs-staging')) {
+    baseUrl += previewPath;
+}
+
 function getConfig() {
     if (env === 'live') {
         return configDocs['production'];
@@ -19,104 +32,103 @@ const { algoliaConfig } = getConfig();
 
 let algoliaTimer;
 
-const searchDesktop = docsearch({
-    appId: algoliaConfig.appId,
-    apiKey: algoliaConfig.apiKey,
-    indexName: algoliaConfig.index,
-    inputSelector: '.docssearch-input',
-    algoliaOptions: {
-        facetFilters: [`language:${lang}`]
-    },
-    autocompleteOptions: {
-        autoselect: false
-    },
-    queryHook(query) {
-        // eslint-disable-next-line no-underscore-dangle
-        if (window._DATADOG_SYNTHETICS_BROWSER === undefined) {
-            clearTimeout(algoliaTimer);
-            algoliaTimer = setTimeout(function() {
-                if (query.length > 0) {
-                    datadogLogs.logger.log(
-                        'Algolia Search',
-                        {
-                            browser: {
-                                algolia: {
-                                    search: query.toLowerCase()
+// API page does not have desktop Algolia input selector, inputSelector: '.docssearch-input', and will throw an error.
+if (!document.body.classList.contains('api')) {
+    const searchDesktop = docsearch({
+        appId: algoliaConfig.appId,
+        apiKey: algoliaConfig.apiKey,
+        indexName: algoliaConfig.index,
+        inputSelector: '.docssearch-input',
+        algoliaOptions: {
+            facetFilters: [`language:${lang}`]
+        },
+        autocompleteOptions: {
+            autoselect: false
+        },
+        queryHook(query) {
+            // eslint-disable-next-line no-underscore-dangle
+            if (window._DATADOG_SYNTHETICS_BROWSER === undefined) {
+                clearTimeout(algoliaTimer);
+                algoliaTimer = setTimeout(function() {
+                    if (query.length > 0) {
+                        datadogLogs.logger.log(
+                            'Algolia Search',
+                            {
+                                browser: {
+                                    algolia: {
+                                        search: query.toLowerCase()
+                                    }
                                 }
-                            }
-                        },
-                        'info'
-                    );
-                }
-            }, 1000);
+                            },
+                            'info'
+                        );
+                    }
+                }, 1000);
+            }
+        },
+        debug: false // Set debug to true if you want to inspect the dropdown
+    });
+    let desktopEnableEnter = true;
+    const searchBtn = document.querySelector('.js-search-btn');
+    const searchContainer = document.querySelector('.search-container');
+
+    searchDesktop.autocomplete.on('keyup', function(e) {
+        if ($('.algolia-docsearch-suggestion').length > 0) {
+            if ($('.algolia-autocomplete')) {
+                $('.algolia-autocomplete').css('borderBottomLeftRadius', '0px');
+                $('.algolia-autocomplete').css(
+                    'borderBottomRightRadius',
+                    '0px'
+                );
+            }
+        } else {
+            $('.algolia-autocomplete').css('borderTopLeftRadius', '3px');
+            $('.algolia-autocomplete').css('borderBottomLeftRadius', '3px');
         }
-    },
-    debug: false // Set debug to true if you want to inspect the dropdown
-});
-let desktopEnableEnter = true;
-const searchBtn = document.querySelector('.js-search-btn');
-const searchContainer = document.querySelector('.search-container');
-
-// Set baseUrl based on environment
-let baseUrl = window.location.origin;
-const uri = window.location.pathname;
-const subdomain = baseUrl.split('.')[0];
-const previewPath = uri.split('/').slice(0,3).join('/');
-
-if ( subdomain.includes('docs-staging')) {
-    baseUrl += previewPath
-}
-
-searchDesktop.autocomplete.on('keyup', function(e) {
-    if ($('.algolia-docsearch-suggestion').length > 0) {
-        if ($('.algolia-autocomplete')) {
-            $('.algolia-autocomplete').css('borderBottomLeftRadius', '0px');
-            $('.algolia-autocomplete').css('borderBottomRightRadius', '0px');
-        }
-    } else {
-        $('.algolia-autocomplete').css('borderTopLeftRadius', '3px');
-        $('.algolia-autocomplete').css('borderBottomLeftRadius', '3px');
-    }
-    if (e.keyCode === 13 && desktopEnableEnter) {
-        window.location = `${baseUrl}/search/?s=${this.value}`;
-    }
-});
-
-// if user clicks search button
-if (searchBtn) {
-    searchBtn.addEventListener('click', function() {
-        const docsearchInput = document.querySelector(
-            '.docssearch-input.ds-input'
-        );
-        if (docsearchInput.value !== '') {
-            window.location = `${baseUrl}/search?s=${docsearchInput.value}`;
+        if (e.keyCode === 13 && desktopEnableEnter) {
+            window.location = `${baseUrl}/search/?s=${this.value}`;
         }
     });
-}
 
-searchDesktop.autocomplete.on('autocomplete:closed', function(e) {
-    if ($('.algolia-docsearch-suggestion').length > 0) {
-        if ($('.algolia-autocomplete')) {
-            $('.algolia-autocomplete').css('borderBottomLeftRadius', '0px');
-            $('.algolia-autocomplete').css('borderBottomRightRadius', '0px');
+    // if user clicks search button
+    if (searchBtn) {
+        searchBtn.addEventListener('click', function() {
+            const docsearchInput = document.querySelector(
+                '.docssearch-input.ds-input'
+            );
+            if (docsearchInput.value !== '') {
+                window.location = `${baseUrl}/search?s=${docsearchInput.value}`;
+            }
+        });
+    }
+
+    searchDesktop.autocomplete.on('autocomplete:closed', function(e) {
+        if ($('.algolia-docsearch-suggestion').length > 0) {
+            if ($('.algolia-autocomplete')) {
+                $('.algolia-autocomplete').css('borderBottomLeftRadius', '0px');
+                $('.algolia-autocomplete').css(
+                    'borderBottomRightRadius',
+                    '0px'
+                );
+            }
+        } else {
+            $('.algolia-autocomplete').css('borderTopLeftRadius', '3px');
+            $('.algolia-autocomplete').css('borderBottomLeftRadius', '3px');
         }
-    } else {
-        $('.algolia-autocomplete').css('borderTopLeftRadius', '3px');
-        $('.algolia-autocomplete').css('borderBottomLeftRadius', '3px');
-    }
-    if (e.keyCode === 13 && desktopEnableEnter) {
-        window.location = `${baseUrl}/search/?s=${this.value}`;
-    }
-});
-searchDesktop.autocomplete.on('autocomplete:cursorchanged', function() {
-    desktopEnableEnter = false;
-});
-searchDesktop.autocomplete.on('autocomplete:cursorremoved', function() {
-    desktopEnableEnter = true;
-});
+        if (e.keyCode === 13 && desktopEnableEnter) {
+            window.location = `${baseUrl}/search/?s=${this.value}`;
+        }
+    });
+    searchDesktop.autocomplete.on('autocomplete:cursorchanged', function() {
+        desktopEnableEnter = false;
+    });
+    searchDesktop.autocomplete.on('autocomplete:cursorremoved', function() {
+        desktopEnableEnter = true;
+    });
 
-if (searchContainer) {
-    document.querySelector('.search-container').style.display = 'block';
+    if (searchContainer) {
+        document.querySelector('.search-container').style.display = 'block';
+    }
 }
 
 const searchMobile = docsearch({
