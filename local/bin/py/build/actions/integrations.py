cde0bbdc35a6493804ed6dd478d3f31d54fc97f8
Pierre Guceski <p.guceski@gmail.com>
2020-01-09 15:51:42 +0100

    improving layout

--- local/bin/py/build/actions/integrations.py
+++ local/bin/py/build/actions/integrations.py
@@ -29,22 +29,20 @@ from os.path import (
     normpath,
 )
 
-
-class PreBuild:
-    def __init__(self, opts):
+class Integrations:
+    def __init__(self, source_file):
         super().__init__()
-        self.options = opts
         self.list_of_contents = []
         self.tempdir = (
             "./integrations_data"
         )
         self.data_dir = "{0}{1}{2}".format(
-            abspath(normpath(options.source)),
+            abspath(normpath(source_file)),
             sep,
             "data" + sep,
         )
         self.content_dir = "{0}{1}{2}".format(
-            abspath(normpath(options.source)),
+            abspath(normpath(source_file)),
             sep,
             "content" + sep + "en" + sep,
         )
@@ -61,7 +59,7 @@ class PreBuild:
             join(self.tempdir, "extracted") + sep
         )
         self.integration_datafile = "{0}{1}{2}".format(
-            abspath(normpath(self.options.source)),
+            abspath(normpath(source_file)),
             sep,
             "integrations.json",
         )
@@ -134,71 +132,6 @@ class PreBuild:
                         yaml_data, default_flow_style=False
                     )
                 )
-
-    def process(self):
-        """
-        This represents the overall workflow of the build of the documentation
-        """
-        print("\x1b[34mStarting Processing...\x1b[0m")
-
-        configuration = yaml.load(open(getenv("CONFIGURATION_FILE")))
-
-        self.list_of_contents = prepare_content(
-            configuration, self.options.token, self.extract_dir)
-
-        try:
-            self.process_filenames()
-        except:
-            if getenv("LOCAL") == 'True':
-                print(
-                    "\x1b[33mWARNING\x1b[0m: Local mode detected: Processing files failed, documentation is now in degraded mode.")
-            else:
-                print(
-                    "\x1b[31mERROR\x1b[0m: Processing files failed, stoping build.")
-                sys.exit(1)
-
-        try:
-            self.merge_integrations()
-        except:
-            if getenv("LOCAL") == 'True':
-                print(
-                    "\x1b[33mWARNING\x1b[0m: Local mode detected: Integration merge failed, documentation is now in degraded mode.")
-            else:
-                print(
-                    "\x1b[31mERROR\x1b[0m: Integration merge failed, stoping build.")
-                sys.exit(1)
-
-    def process_filenames(self):
-        """
-        Goes through the list_of_contents and for each content
-        triggers the right action to apply.
-        """
-        for content in self.list_of_contents:
-            try:
-                if content["action"] == "integrations":
-                    self.process_integrations(content)
-
-                elif (content["action"] == "pull-and-push-folder"):
-                    pull_and_push_folder(content, self.content_dir)
-
-                elif content["action"] == "pull-and-push-file":
-                    pull_and_push_file(content, self.content_dir)
-                elif content["action"] == "Not Available":
-                    if getenv("LOCAL") == 'True':
-                        print("\x1b[33mWARNING\x1b[0m: Processing of {} canceled, since content is not available. Documentation is in degraded mode".format(
-                            content["repo_name"]))
-                else:
-                    print(
-                        "\x1b[31mERROR\x1b[0m: Action {} unknown for {}".format(content["action"], content))
-                    raise ValueError
-            except:
-                if getenv("LOCAL") == 'True':
-                    print(
-                        "\x1b[33mWARNING\x1b[0m: Unsuccessful processing of {}".format(content))
-                else:
-                    print(
-                        "\x1b[31mERROR\x1b[0m: Unsuccessful processing of {}".format(content))
-                    raise ValueError
 
     def process_integrations(self, content):
         """
@@ -560,29 +493,6 @@ class PreBuild:
         return dependencies
 
 
-if __name__ == "__main__":
-    parser = OptionParser(
-        usage="usage: %prog [options] link_type"
-    )
-    parser.add_option(
-        "-t",
-        "--token",
-        help="github access token",
-        default=None,
-    )
-    parser.add_option(
-        "-s",
-        "--source",
-        help="location of src files",
-        default=curdir,
-    )
+if __name__ == "__main__":xz
 
-    options, args = parser.parse_args()
-    options.token = (
-        getenv("GITHUB_TOKEN", options.token)
-        if not options.token
-        else options.token
-    )
-
-    pre = PreBuild(options)
-    pre.process()
+    Integrations(source_file)
