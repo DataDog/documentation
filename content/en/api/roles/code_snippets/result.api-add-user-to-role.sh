431dcf970c30fe151821a1763025b08da839fe37
l0k0ms <pierre.guceski@datadoghq.com>
2020-01-31 14:31:28 +0100

    adding missing language results placeholder

--- content/en/api/roles/code_snippets/result.api-add-user-to-role.sh
+++ content/en/api/roles/code_snippets/result.api-add-user-to-role.sh
@@ -1,25 +1,22 @@
 {
-  "meta": {"page": {"total_count": 1}},
-  "data": [
-    {
-      "type": "users",
-      "id": "$USER_UUID",
-      "attributes": {
-        "name": "Example user",
-        "handle": "user@example.org",
-        "created_at": "2000-02-29T14:26:26.983187+00:00",
-        "email": "user@example.org",
-        "icon": "https://secure.gravatar.com/avatar/abc123abc123",
-        "title": null,
-        "verified": true,
-        "disabled": false,
-        "allowed_login_methods": [],
-        "status": "Active"
-      },
-      "relationships": {
-        "roles": {"data": [{"type": "roles", "id": "$ROLE_UUID"}]},
-        "org": {"data": {"type": "orgs", "id": "$ORG_UUID"}}
-      }
-    }
-  ]
+    "meta": {"page": {"total_count": 1}},
+    "data": [
+        {
+            "type": "users",
+            "id": "$USER_UUID",
+            "attributes": {
+                "name": "Example user",
+                "handle": "user@example.org",
+                "created_at": "2000-02-29T14:26:26.983187+00:00",
+                "email": "user@example.org",
+                "icon": "https://secure.gravatar.com/avatar/abc123abc123",
+                "title": null,
+                "verified": true,
+                "disabled": false,
+                "allowed_login_methods": [],
+                "status": "Active"
+            },
+            "relationships": {"roles": {"data": [{"type": "roles", "id": "$ROLE_UUID"}]}, "org": {"data": {"type": "orgs", "id": "$ORG_UUID"}}}
+        }
+    ]
 }
