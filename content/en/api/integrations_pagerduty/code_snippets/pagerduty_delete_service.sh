829999d082a14d1d35ae040e05437c1650ef6616
Yann MAHE <yann@datadoghq.com>
2020-03-17 12:15:37 -0400

    [pagerduty] add examples to delete service

--- content/en/api/integrations_pagerduty/code_snippets/pagerduty_delete_service.sh
+++ content/en/api/integrations_pagerduty/code_snippets/pagerduty_delete_service.sh
@@ -3,7 +3,7 @@
 api_key="<DATADOG_API_KEY>"
 app_key="<DATADOG_APPLICATION_KEY>"
 
-curl -v -X GET \
+curl -v -X DELETE \
 -H "Content-type: application/json" \
 -H "DD-API-KEY: ${api_key}" \
 -H "DD-APPLICATION-KEY: ${app_key}" \
