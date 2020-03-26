72fdc53b98af2c7e6a61a140f539fb3ade97af92
zbayoff <zrbayoff@gmail.com>
2020-03-26 12:33:05 -0400

    add dereference script to Github actions

--- src/scripts/dereference.js
+++ src/scripts/dereference.js
@@ -1,55 +1,13 @@
-const yaml = require('js-yaml');
 const fs = require('fs');
 const $RefParser = require('@apidevtools/json-schema-ref-parser');
 const safeJsonStringify = require('safe-json-stringify');
-
-
-
-const options = {
-    // dereference: {
-    //     circular: "ignore"
-    // }
-};
-
-const getCircularReplacer = () => {
-    const seen = new WeakSet();
-    return (key, value) => {
-      if (typeof value === "object" && value !== null) {
-        if (seen.has(value)) {
-          return;
-        }
-        seen.add(value);
-      }
-      return value;
-    };
-  };
 
 const apiV1FullSpecYaml = './data/api/v1/full_spec.yaml';
 const apiV2FullSpecYaml = './data/api/v2/full_spec.yaml';
 
 async function dereference(apiYaml, apiVersion) {
     try {
-        const dereferencedObject = await $RefParser.dereference(apiYaml, options);
-
-        // const safeJSON = safeJsonStringify(dereferencedObject);
-
-        const safeJSON = JSON.stringify(dereferencedObject, getCircularReplacer(), 2);
-
-        // console.log('dereferencedYaml: ', stringify(dereferencedObject))
-
-        // const newYaml = yaml.safeDump(JSON.parse(safeJSON), {
-        //     lineWidth: -1
-        // });
-
-        // const newYaml = yaml.safeDump(dereferencedObject, {
-        //     lineWidth: -1
-        // });
-
-        // fs.writeFileSync(
-        //     `./data/api/${apiVersion}/full_spec_deref.yaml`,
-        //     newYaml,
-        //     'utf8'
-        // );
+        const dereferencedObject = await $RefParser.dereference(apiYaml);
 
         fs.writeFileSync(
             `./data/api/${apiVersion}/full_spec_deref.json`,
@@ -57,12 +15,7 @@ async function dereference(apiYaml, apiVersion) {
             'utf8'
         );
 
-        // fs.writeFileSync(
-        //     `./data/api/${apiVersion}/full_spec_deref.json`,
-        //     JSON.stringify(dereferencedObject),
-        //     'utf8'
-        // );
-        console.log('successfully deferenced yaml file.');
+        console.log(`successfully deferenced ${apiYaml}`);
     } catch (err) {
         throw err;
     }
