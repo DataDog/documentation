94be95be5adf65ec5ce098d5b2a9db24c5226656
david.jones <david.jones@datadoghq.com>
2020-03-27 16:57:22 +0000

    update workflow to just run one script add to script building out a examples.json file per page update list.html to display json

--- src/scripts/build-api-pages.js
+++ src/scripts/build-api-pages.js
@@ -3,16 +3,18 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
 const slugify = require('slugify');
+const $RefParser = require('@apidevtools/json-schema-ref-parser');
+const safeJsonStringify = require('safe-json-stringify');
 
-const apiV1FullSpecYaml = yaml.safeLoad(
-    fs.readFileSync('./data/api/v1/full_spec.yaml', 'utf8')
-);
 
-const apiV2FullSpecYaml = yaml.safeLoad(
-    fs.readFileSync('./data/api/v2/full_spec.yaml', 'utf8')
-);
+async function updateApiSideNav(apiYaml, apiVersion) {
+    const dereferencedObject = await $RefParser.dereference(apiYaml);
+    fs.writeFileSync(
+        `./data/api/${apiVersion}/full_spec_deref.json`,
+        safeJsonStringify(dereferencedObject, null, 2),
+        'utf8'
+    );
 
-function updateApiSideNav(apiYaml, apiVersion) {
     const menuYaml = yaml.safeLoad(
         fs.readFileSync('./config/_default/menus/menus.en.yaml', 'utf8')
     );
@@ -50,6 +52,9 @@ function updateApiSideNav(apiYaml, apiVersion) {
         } catch (err) {
             throw err;
         }
+
+        // build resources for a page
+        buildResources(dereferencedObject, `./content/en/api/${apiVersion}/${newDirName}/`, tags.name);
 
         const indexMenuObject = {
             name: tags.name,
@@ -100,5 +105,93 @@ function updateApiSideNav(apiYaml, apiVersion) {
     }
 }
 
-updateApiSideNav(apiV1FullSpecYaml, 'v1');
-updateApiSideNav(apiV2FullSpecYaml, 'v2');
+/**
+ * Quick helper to checks if path has a matching tag
+ * @param {object} pathObj - object with path data
+ * @param {string} tag - string with name of tag
+ * returns boolean
+ */
+const isTagMatch = (pathObj, tag) => {
+  const [firstActionName] = Object.keys(pathObj);
+  return (pathObj[firstActionName].tags) ? pathObj[firstActionName].tags[0] === tag : false;
+};
+
+
+/**
+ * Takes a schema requestBody or response and filters the data to be example worthy
+ * It does this by order of preference of attributes "items" -> "example" -> "type"
+ * @param {object} data - object schema
+ * returns filtered data
+ */
+const filterExampleJson = (data) => {
+  if("properties" in data) {
+    return Object.keys(data.properties)
+      .map((propKey) => {
+        const property = data.properties[propKey];
+        return {[propKey]: (property.hasOwnProperty("items")) ? filterExampleJson(property.items) : (property["example"] || property["type"])};
+      })
+      .reduce((obj, item) => {
+        return {...obj, ...item}
+      }, {});
+  } else if ("items" in data) {
+    return filterExampleJson(data.items);
+  } else {
+    return data["example"] || data["type"] || {};
+  }
+};
+
+
+/**
+ * Builds resources needed for a hugo api page
+ * @param {object} dereferencedObject - deref object
+ * @param {string} pageDir - page dir
+ * @param {string} tagName - string name of tag
+ */
+const buildResources = (dereferencedObject, pageDir, tagName) => {
+  const pageExampleJson = {};
+
+  // just get this sections data
+  const data = Object.keys(dereferencedObject.paths)
+    .filter((path) => isTagMatch(dereferencedObject.paths[path], tagName))
+    .map((path) => dereferencedObject.paths[path]);
+
+  // build example json for request and each response
+  data.forEach((actionObj) => {
+    Object.entries(actionObj).forEach(([actionKey, action]) => {
+      // set some default for this operationid entry default
+      if(!(action.operationId in pageExampleJson)) {
+        pageExampleJson[action.operationId] = {"responses":{}, "request": {}};
+      }
+      // add request json
+      if(("requestBody" in action) && ("content" in action.requestBody) && ("application/json" in action.requestBody.content)) {
+        pageExampleJson[action.operationId]["request"] = filterExampleJson(action.requestBody.content["application/json"].schema);
+      }
+      // add response json
+      Object.entries(action.responses).forEach(([response_code, response]) => {
+        if(("content" in response) && ("application/json" in response.content)) {
+          pageExampleJson[action.operationId]["responses"][response_code] = filterExampleJson(response.content["application/json"].schema);
+        }
+      });
+    });
+  });
+
+  fs.writeFileSync(`${pageDir}examples.json`, safeJsonStringify(pageExampleJson, null, 2), 'utf-8');
+};
+
+
+/**
+ * Takes an array of spec file paths and processes them
+ * @param {array} specs - array of strings with path to spec e.g ['./data/api/v2/full_spec.yaml']
+ */
+const processSpecs = (specs) => {
+  specs
+    .forEach((spec) => {
+      const fileData = yaml.safeLoad(fs.readFileSync(spec, 'utf8'));
+      const version = spec.split('/')[3];
+      updateApiSideNav(fileData, version);
+    });
+};
+
+
+const specs = ['./data/api/v1/full_spec.yaml', './data/api/v2/full_spec.yaml'];
+processSpecs(specs);
