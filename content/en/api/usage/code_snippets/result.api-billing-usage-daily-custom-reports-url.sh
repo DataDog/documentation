9ef72df2bbd64175b5f0d08b24feed9f0a758ab4
Julian Leung <julian.leung@datadoghq.com>
2020-03-27 12:10:28 -0400

    changing layout

--- content/en/api/usage/code_snippets/result.api-billing-usage-daily-custom-reports-url.sh
+++ content/en/api/usage/code_snippets/result.api-billing-usage-daily-custom-reports-url.sh
@@ -11,9 +11,7 @@
 			"size": 57653199,
 			"tags": ["env", "service", "team"],
 			"location":
-				"https://an-s3-or-gs-bucket.s3.amazonaws.com/subscription_job_zipper/
-				1.0/2019/10/02/0/13/daily_report_2019-10-02.zip?AWSAccessKeyId=an_aws
-				_access_key_id&x-amz-security-token=an_amz_security_token",
+				"https://an-s3-or-gs-bucket.s3.amazonaws.com/subscription_job_zipper/1.0/2019/10/02/0/13/daily_report_2019-10-02.zip?AWSAccessKeyId=an_aws_access_key_id&x-amz-security-token=an_amz_security_token",
 			"computed_on": "2019-10-03T01:02:03+00:00"
 		},
 		"type": "reports",
