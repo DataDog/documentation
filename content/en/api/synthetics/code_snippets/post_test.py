52875ba3b55b2caa71bf15cda5593ef3741a544b
Siobhan Mahoney <siobhan.mahoney@datadoghq.com>
2020-04-07 17:45:48 -0400

    Fixing Python snippet
    Fixing Python snippet for `Create a Test` Synthetics endpoint

--- content/en/api/synthetics/code_snippets/post_test.py
+++ content/en/api/synthetics/code_snippets/post_test.py
@@ -11,22 +11,24 @@ initialize(**options)
 
 name = "test"
 api_test_type = "api"
-request = {"method": "GET", "url": "https://datadoghq.com", "timeout": 30, "headers": {"header1": "value1", "header2": "value2"}
-message = "test"
-api_test_options = {"tick_every": 60, "min_failure_duration": 0,
-    "min_location_failed": 1, "follow_redirects": true}
-assertions = [
+api_config = {
+  "assertions": [
             {"operator": "is", "type": "statusCode", "target": 403},
             {"operator": "is", "property": "content-type",
                 "type": "header", "target": "text/html"},
             {"operator": "lessThan", "type": "responseTime", "target": 2000}
-         ]
-location = ["aws:us-east-2", "aws:eu-central-1", "aws:ca-central-1",
+         ],
+  "request": {"method": "GET", "url": "https://datadoghq.com", "timeout": 30, "headers": {"header1": "value1", "header2": "value2"}}
+           }
+message = "test"
+api_test_options = {"tick_every": 60, "min_failure_duration": 0,
+    "min_location_failed": 1, "follow_redirects": True}
+locations = ["aws:us-east-2", "aws:eu-central-1", "aws:ca-central-1",
     "aws:eu-west-2", "aws:ap-northeast-1", "aws:us-west-2", "aws:ap-southeast-2"]
 tags = ["foo:bar"]
 
-api.Synthetics.create_test(name=name, type=api_test_type, request=request, options=api_test_options,
-                           message=message, assertions=assertions, location=location, tags=tags)
+api.Synthetics.create_test(name=name, type=api_test_type, config=api_config, options=api_test_options,
+                           message=message, locations=locations, tags=tags)
 
 # To create a browser test
 
