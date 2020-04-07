1152e806beb2e290d0c9ebaf9b1907e95c5f8d4d
Siobhan Mahoney <siobhan.mahoney@datadoghq.com>
2020-04-07 17:52:10 -0400

    Fixing Python snippet – Create a Test
    Fixing Python snippet for Synthetics Create a Test endpoint (Browser tests)

--- content/en/api/synthetics/code_snippets/post_test.py
+++ content/en/api/synthetics/code_snippets/post_test.py
@@ -13,13 +13,12 @@ name = "test"
 api_test_type = "api"
 api_config = {
   "assertions": [
-            {"operator": "is", "type": "statusCode", "target": 403},
-            {"operator": "is", "property": "content-type",
-                "type": "header", "target": "text/html"},
-            {"operator": "lessThan", "type": "responseTime", "target": 2000}
-         ],
+    {"operator": "is", "type": "statusCode", "target": 403},
+    {"operator": "is", "property": "content-type", "type": "header", "target": "text/html"},
+    {"operator": "lessThan", "type": "responseTime", "target": 2000}
+  ],
   "request": {"method": "GET", "url": "https://datadoghq.com", "timeout": 30, "headers": {"header1": "value1", "header2": "value2"}}
-           }
+}
 message = "test"
 api_test_options = {"tick_every": 60, "min_failure_duration": 0,
     "min_location_failed": 1, "follow_redirects": True}
@@ -34,13 +33,15 @@ api.Synthetics.create_test(name=name, type=api_test_type, config=api_config, opt
 
 name = "test"
 browser_test_type = "browser"
-request = {"method": "GET", "url": "https://example.com/"}
+browser_config = {
+  "request": {"method": "GET", "url": "https://example.com/"},
+  "assertions": []
+}
 message = "test"
 browser_test_options = {"device_ids": ["laptop_large"], "tick_every": 3600, "min_failure_duration": 0,
     "min_location_failed": 1, "monitor_options": {"renotify_interval": 30}, "retry": {"count": 2, "interval": 30}}
-assertions = []
-location = ["aws:ca-central-1", "aws:us-east-2"]
+locations = ["aws:ca-central-1", "aws:us-east-2"]
 tags = []
 
-api.Synthetics.create_test(name=name, type=browser_test_type, request=request, options=browser_test_options,
-                           message=message, assertions=assertions, location=location, tags=tags)
+api.Synthetics.create_test(name=name, type=browser_test_type, config=browser_config, options=browser_test_options,
+                           message=message, locations=locations, tags=tags)
