f8422b777dc4a462a1baa01e8cefed0c5eb5d73c
Siobhan Mahoney <siobhan.mahoney@datadoghq.com>
2020-04-07 17:37:37 -0400

    Fixing Synthetics API docs Python snippets
    Fixing `Create a Test` and `Edit a Test` Python snippets to align with endpoints' expected schema

--- content/en/api/synthetics/code_snippets/put_test.py
+++ content/en/api/synthetics/code_snippets/put_test.py
@@ -13,20 +13,21 @@ test_id='<SYNTHETICS_TEST_PUBLIC_ID>'
 
 name = "test"
 api_test_type = "api"
-request = {"method": "GET", "url": "https://datadoghq.com", "timeout": 30, "headers": {"header1": "value1", "header2": "value2"}
+api_config = {
+  "assertions": [
+            {"operator": "is", "type": "statusCode", "target": 403},
+            {"operator": "is", "property": "content-type", "type": "header", "target": "text/html"},
+            {"operator": "lessThan", "type": "responseTime", "target": 2000}
+         ], 
+  "request": {"method": "GET", "url": "https://datadoghq.com", "timeout": 30, "headers": {"header1": "value1", "header2": "value2"}}
+}
 message = "test-edited"
 api_test_options = {"tick_every": 60, "min_failure_duration": 0,
-    "min_location_failed": 1, "follow_redirects": true}
-assertions = [
-            {"operator": "is", "type": "statusCode", "target": 403},
-            {"operator": "is", "property": "content-type",
-                "type": "header", "target": "text/html"},
-            {"operator": "lessThan", "type": "responseTime", "target": 2000}
-         ]
-location = ["aws:us-east-2", "aws:eu-central-1", "aws:ca-central-1",
+    "min_location_failed": 1, "follow_redirects": True}
+locations = ["aws:us-east-2", "aws:eu-central-1", "aws:ca-central-1",
     "aws:eu-west-2", "aws:ap-northeast-1", "aws:us-west-2", "aws:ap-southeast-2"]
 tags = ["foo:bar"]
 
 
-api.Synthetics.edit_test(id=test_id,name=name, type=api_test_type, request=request, options=api_test_options,
-                           message=message, assertions=assertions, location=location, tags=tags)
+api.Synthetics.edit_test(id=test_id, name=name, type=api_test_type, config=api_config, options=api_test_options,
+                           message=message, locations=locations, tags=tags)
