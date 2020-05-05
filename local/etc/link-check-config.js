75d11da842cf27479d8dbf7cb14c33f9c00fb47b
zbayoff <zrbayoff@gmail.com>
2020-05-05 16:48:14 -0400

    test link checker

--- local/etc/link-check-config.js
+++ local/etc/link-check-config.js
@@ -1,30 +1,56 @@
 module.exports = {
-    "production": {
-        serverUrl: "https://docs.datadoghq.com/",
-        branches: ["master"],
-        slackChannel: "#guac-ops"
+    production: {
+        serverUrl: 'https://docs.datadoghq.com/',
+        slackChannel: '#link-checker-test'
     },
-    "preview": {
-        serverUrl: "https://docs-staging.datadoghq.com/",
-        slackChannel: "#guac-ops"
+    preview: {
+        serverUrl: 'https://docs-staging.datadoghq.com/',
+        slackChannel: '#link-checker-test'
     },
-    "staging": {
-        serverUrl: "https://docs-staging.datadoghq.com/",
-        slackChannel: "#guac-ops"
+    staging: {
+        serverUrl: 'https://docs-staging.datadoghq.com/',
+        slackChannel: '#link-checker-test'
     },
-    "linkCheckOptions": {
+    linkCheckOptions: {
+        method: 'get',
         filterLevel: 3,
-        maxSocketsPerHost: 90,
-        maxSockets: 250,
-        excludeExternalLinks: true,
+        maxSocketsPerHost: 75,
+        maxSockets: 300,
+        excludeExternalLinks: false,
         honorRobotExclusions: false,
-        cacheResponses: true,
-        includedKeywords: [
-            '*imgix*'
-        ],
+        brokenLinkSlackLimit: 10,
+        // cacheResponses: true,
+        includedKeywords: ['*docs.datadoghq*', '*help.datadoghq*'],
         excludedKeywords: [
-            '**/signup*'
+            '*ja/blog*',
+            // '*blog*',
+            // '*videos*',
+            // '*legal*',
+            // '*log-management*',
+            // '*product*',
+            // '*solutions*',
+            // '*dashboards*',
+            // '*case-studies*',
+            // '*ja*',
+            // '*resources*',
+            // '*jobs-*',
+            // '*about*',
+            '**/signup*',
+            '*app.datadoghq*',
+            '*corp-hugo/tree*',
+            '*linkedin.com/*',
+            '*reddit.com/submit*',
+            '*twitter.com/share*',
+            '*mailto:*',
+            '*tel:*',
+            '*irc:*',
+            '*googletagmanager*',
+            '*munchkin.marketo*',
+            '*streaming-corpsite-collector.datadoghq*',
+            '*app-sj16.marketo*',
+            '*eoig7v0a2o-dsn.algolia*',
+            '*.txt'
+            // '*docs.datadoghq.com/integrations/*'
         ]
-
     }
 };
