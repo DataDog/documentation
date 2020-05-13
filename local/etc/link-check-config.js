00c7f03ec54383768f53f55423f2345a0e6fd3e7
zbayoff <zrbayoff@gmail.com>
2020-05-13 09:30:09 -0400

    test higher maxSockets

--- local/etc/link-check-config.js
+++ local/etc/link-check-config.js
@@ -17,13 +17,13 @@ module.exports = {
     },
     linkCheckOptions: {
         filterLevel: 3,
-        maxSockets: 100,
+        maxSockets: 200,
         excludeExternalLinks: false,
         honorRobotExclusions: false,
         brokenLinkSlackLimit: 5,
         excludedKeywords: [
-            '**/ja/**',
-            '**/fr/**',
+            // '**/ja/**',
+            // '**/fr/**',
             '*wtime=*',
             '**/signup*',
             '*app.datadoghq*',
