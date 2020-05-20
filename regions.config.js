9a5b7239baa97b4d5d2bb09ee590105c9b53e6e9
zbayoff <zrbayoff@gmail.com>
2020-05-20 14:29:40 -0400

    move back region param file

--- regions.config.js
+++ regions.config.js
@@ -4,10 +4,6 @@ export default {
         us: 'datadoghq.com',
         eu: 'datadoghq.eu'
     },
-    dd_site_name: {
-        us: 'US site',
-        eu: 'EU site'
-    },
     tcp_endpoint: {
         us: 'intake.logs.datadoghq.com',
         eu: 'tcp-intake.logs.datadoghq.eu'
@@ -15,13 +11,5 @@ export default {
     tcp_endpoint_port: {
         us: '10514',
         eu: '1883'
-    },
-    intake_profile_endpoint: {
-        us: 'https://intake.profile.datadoghq.com',
-        eu: 'https://intake.profile.datadoghq.eu'
-    },
-    ip_ranges_endpoint: {
-        us: 'https://ip-ranges.datadoghq.com',
-        eu: 'https://ip-ranges.datadoghq.eu'
     }
 };
