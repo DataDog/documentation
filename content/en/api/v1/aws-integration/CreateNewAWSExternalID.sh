d1b3ded3c69a95307bf86eb3dbee5f906aef085a
Jiri Kuncar <jiri.kuncar@datadoghq.com>
2020-05-27 19:20:53 +0200

    Remove generated curl examples (#7525)

--- content/en/api/v1/aws-integration/CreateNewAWSExternalID.sh
+++
@@ -1,1 +0,0 @@
-curl -X PUT -H "DD-API-KEY: [[apiKey]]" -H "DD-APPLICATION-KEY: [[apiKey]]" "https://api.datadoghq.com/api/v1/integration/aws/generate_new_external_id"
--- content/en/api/v2/roles/ListRoleUsers.sh
+++
@@ -1,1 +0,0 @@
-curl -X GET -H "DD-API-KEY: [[apiKey]]" -H "DD-APPLICATION-KEY: [[apiKey]]" "https://api.datadoghq.com/api/v2/roles/{role_id}/users?page[size]=&page[number]=&sort=&filter="
