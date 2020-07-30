e4116fc00dfda5e0c71073feeb15b9e8f61d2485
Tanguy Le Barzic <tanguy.lebarzic@datadoghq.com>
2020-07-30 23:40:46 +0200

    Security Monitoring - load the message from a dedicated markdown file (#8157)
    * [security-rules] Remove unused code
    
    * [security-rules] The message is now extracted from a dedicated markdown file
    
    * [security-monitoring] Point to the right branch

--- local/bin/py/build/actions/security_rules.py
+++ local/bin/py/build/actions/security_rules.py
@@ -22,14 +22,6 @@ TEMPLATE = """\
 """
 
 
-def get_tag(tag_list, key):
-    result_tag = ""
-    for tag in tag_list:
-        if tag.startswith(f"{key}:"):
-            result_tag = tag.replace(f"{key}:", "")
-    return result_tag
-
-
 def security_rules(content, content_dir):
     """
     Takes the content from a file from a github repo and
@@ -40,13 +32,12 @@ def security_rules(content, content_dir):
     """
     logger.info("Starting security rules action...")
     for file_name in chain.from_iterable(glob.glob(pattern, recursive=True) for pattern in content["globs"]):
+        # Only loop over rules JSON files (not eg. Markdown files containing the messages)
+        if not file_name.endswith(".json"):
+            continue
         with open(file_name, mode="r+") as f:
             json_data = json.loads(f.read())
             p = Path(f.name)
-
-            # get the source tag for this file
-            source_tag = get_tag(json_data.get('tags', []), "source")
-            scope_tag = get_tag(json_data.get('tags', []), "scope")
 
             # delete file or skip if staged
             if json_data.get('isStaged', False) or json_data.get('isDeleted', False) or not json_data.get('isEnabled', True):
@@ -56,25 +47,31 @@ def security_rules(content, content_dir):
                 else:
                     logger.info(f"skipping file {p.name}")
             else:
-                page_data = {
-                    "title": f"{json_data.get('name', '')}",
-                    "kind": "documentation",
-                    "type": "security_rules",
-                    "disable_edit": True,
-                    "aliases": [f"{json_data.get('defaultRuleId', '').strip()}"]
-                }
+                # The message of a detection rule is located in a Markdown file next to the rule definition
+                message_file_name = file_name.rsplit(".", 1)[0] + ".md"
 
-                for tag in json_data.get('tags', []):
-                    key, value = tag.split(':')
-                    page_data[key] = value
+                with open(message_file_name, mode="r+") as message_file:
+                    message = message_file.read()
 
-                front_matter = yaml.dump(page_data, default_flow_style=False).strip()
-                output_content = TEMPLATE.format(front_matter=front_matter, content=json_data.get("message", "").strip())
+                    page_data = {
+                        "title": f"{json_data.get('name', '')}",
+                        "kind": "documentation",
+                        "type": "security_rules",
+                        "disable_edit": True,
+                        "aliases": [f"{json_data.get('defaultRuleId', '').strip()}"]
+                    }
 
-                dest_dir = Path(f"{content_dir}{content['options']['dest_path']}")
-                dest_dir.mkdir(exist_ok=True)
-                dest_file = dest_dir.joinpath(p.name).with_suffix('.md')
-                logger.info(dest_file)
-                with open(dest_file, mode='w', encoding='utf-8') as out_file:
-                    out_file.write(output_content)
+                    for tag in json_data.get('tags', []):
+                        key, value = tag.split(':')
+                        page_data[key] = value
+
+                    front_matter = yaml.dump(page_data, default_flow_style=False).strip()
+                    output_content = TEMPLATE.format(front_matter=front_matter, content=message.strip())
+
+                    dest_dir = Path(f"{content_dir}{content['options']['dest_path']}")
+                    dest_dir.mkdir(exist_ok=True)
+                    dest_file = dest_dir.joinpath(p.name).with_suffix('.md')
+                    logger.info(dest_file)
+                    with open(dest_file, mode='w', encoding='utf-8') as out_file:
+                        out_file.write(output_content)
 
