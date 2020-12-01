77fd74319221ee638ecd3afab2adb41f0d63f727
Ruth Naebeck <ruthnaebeck@gmail.com>
2020-12-01 16:16:16 -0500

    remove US3

--- regions.config.js
+++ regions.config.js
@@ -1,5 +1,5 @@
 export default {
-    allowedRegions: ['us', 'us3', 'eu'],
+    allowedRegions: ['us', 'eu'],
     dd_site: {
         us: 'datadoghq.com',
         us3: 'us3.datadoghq.com',
@@ -7,22 +7,18 @@ export default {
     },
     dd_full_site: {
         us: 'app.datadoghq.com',
-        us3: 'us3.datadoghq.com',
         eu: 'app.datadoghq.eu'
     },
     tcp_endpoint: {
         us: 'agent-intake.logs.datadoghq.com',
-        us3: 'agent-intake.logs.us3.datadoghq.com',
         eu: 'agent-intake.logs.datadoghq.eu'
     },
     tcp_endpoint_port: {
         us: '10514',
-        us3: '10514',
         eu: '443'
     },
     tcp_endpoint_port_ssl: {
         us: '10516',
-        us3: '10516',
         eu: '1883'
     }
 };
