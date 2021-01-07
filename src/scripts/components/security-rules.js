268b6328ef519421538e43ce9aaf00667f2ece29
bgdeutsch <bgdeutsch@gmail.com>
2021-01-07 12:28:59 -0500

    Refactors security rules JS, adds search+filtering functionality

--- src/scripts/components/security-rules.js
+++ src/scripts/components/security-rules.js
@@ -1,28 +1,34 @@
-import mixitup from 'mixitup';
-
 export function initializeSecurityRules() {
-    const containerEl = document.querySelector('#rules .list-group');
     const inputSearch = document.querySelector('[data-ref="search"]');
     const controls = document.querySelector('[data-ref="controls"]');
-    const ruleList = Array.prototype.slice.call(document.getElementsByClassName('rule-list') || []);
-    const allSingleRules = document.querySelectorAll('.single-rule');
+    const allGroupHeaders = Array.prototype.slice.call(document.getElementsByClassName('js-group-header') || []);
+    const allRules = document.querySelectorAll('.js-single-rule');
     const allRuleGroups = document.querySelectorAll('.js-group');
     const urlParams = new URLSearchParams(window.location.search);
+    const urlHash = window.location.hash;
     const keyword = urlParams.get('q');
     let keyupTimeout;
     let filters;
-    let mixer;
 
     if (controls) {
        filters = controls.querySelectorAll('[data-ref="filter"]');
     }
 
-    const filterByString = (searchValue) => {
-        if (searchValue && searchValue !== '') {
-            return document.querySelectorAll(`[data-name*="${searchValue}"]`);
+    // Returns array of results filtered by category and/or user search value
+    const filterResults = (filterCategoryValue, searchValue) => {
+        let results = [];
+
+        if (filterCategoryValue && filterCategoryValue !== 'all') {
+            results = document.querySelectorAll(`.${filterCategoryValue}`);
+        } else {
+            results = allRules;
         }
 
-        return allSingleRules;
+        if (searchValue && searchValue.length > 3) {
+            results = Array.from(results).filter(item => item.dataset.name.indexOf(searchValue) > -1);
+        }
+
+        return results;
     }
 
     const activateButton = (activeButton, siblings) => {
@@ -37,38 +43,55 @@ export function initializeSecurityRules() {
         }
 
         // enable all
-        ruleList.forEach(elm => {
+        allGroupHeaders.forEach(elm => {
             elm.classList.add('active');
         });
     }
 
-    const filterResults = (filteredResultsArray) => {
+    const showResults = (filteredResults) => {
+        // Hide all groups, headers, and rules.
+        allRules.forEach(element => {
+            element.style.display = 'none';
+        })
+
+        allGroupHeaders.forEach(element => {
+            element.style.display = 'none';
+        })
+
         allRuleGroups.forEach(element => {
             element.style.display = 'none';
         })
 
-        filteredResultsArray.forEach(element => {
-            const groupHeader = element.closest('.js-group').querySelector('.rule-list');
-            console.log(groupHeader)
-
-            if (!groupHeader.classList.contains('active')) {
-                groupHeader.classList.add('active');
-            } else {
-                groupHeader.classList.remove('active');
-            }
-
-            // element.style.display = 'block';
+        // Show headers and groups for all matches
+        filteredResults.forEach(element => {
+            const jsGroup = element.closest('.js-group');
+            const header = jsGroup.querySelector('.js-group-header');
+            jsGroup.style.display = 'block';
+            header.style.display = 'block';
+            element.style.display = 'inline';
         })
     }
 
+    const handleCategoryFilterClick = (event) => {       
+        // If button is already active, or an operation is in progress, ignore the click
+        if (event.target.classList.contains('active') || !event.target.getAttribute('data-filter'))
+            return;
+            
+        const searchValue = inputSearch.value.length > 3 ? inputSearch.value.toLowerCase().trim() : '';
+        const filtered = filterResults(event.target.dataset.filter, searchValue);
+        activateButton(event.target, filters);
+        showResults(filtered);
+    }
+
     const handleKeyup = () => {
-        const searchValue = inputSearch.value < 3 ? '' : inputSearch.value.toLowerCase().trim();
+        const searchValue = inputSearch.value.length > 3 ? inputSearch.value.toLowerCase().trim() : '';
+        const activeCategoryFilter = document.querySelector('.controls .active').dataset.filter;
         clearTimeout(keyupTimeout);
 
         keyupTimeout = setTimeout(() => {
             window.history.replaceState({q: searchValue}, '', `?q=${searchValue}`);
-            const filtered = filterByString(searchValue);
-            filterResults(filtered);
+            const filtered = filterResults(activeCategoryFilter, searchValue);
+            showResults(filtered);
         }, 350);
     }
 
@@ -76,52 +99,29 @@ export function initializeSecurityRules() {
         inputSearch.addEventListener('keyup', handleKeyup);
     }
 
-    ruleList.forEach(elm => {
+    allGroupHeaders.forEach(elm => {
         elm.addEventListener("click", e => {
             e.currentTarget.classList.toggle('active');
         });
     });
 
-        // if (controls) {
-        //     controls.addEventListener('click', function (e) {
-        //         inputSearch.value = '';
-        //         const url = window.location.href.replace(window.location.search, '');
-        //         window.history.replaceState({q: ''}, '', url);
-        //         // If button is already active, or an operation is in progress, ignore the click
-        //         if (e.target.classList.contains('active') || !e.target.getAttribute('data-filter'))
-        //             return;
-        //         activateButton(e.target, filters);
-        //     });
-        // }
+    // We cannot listen for a DOM loaded event due to the script loading async; this code should execute when the default rules content is loaded.
+    if (controls) {
+        controls.addEventListener('click', handleCategoryFilterClick);
+        let searchValue = '';
 
-        // Set controls the active controls on startup
-        // if(controls) {
-        //     activateButton(controls.querySelector('[data-filter="all"]'), filters);
-        // }
-        // if(keyword) { filterByString(keyword); }
+        if (keyword) {
+            searchValue = keyword;
+            inputSearch.value = keyword;
+        }
 
-        // $(window).on('hashchange', function() {
-        //     let currentCat = '';
-        //     if (window.location.href.indexOf('#') > -1) {
-        //         currentCat = window.location.href.substring(
-        //             window.location.href.indexOf('#')
-        //         );
-        //     }
-        //     const currentSelected = $('.controls .active').attr('href');
-        //     if (currentCat && currentSelected) {
-        //         if (currentCat !== currentSelected) {
-        //             $(`a[href="${currentCat}"]`).get(0).click();
-        //         }
-        //     }
-        //     if (currentCat === '') {
-        //         activateButton(controls.querySelector('[data-filter="all"]'), filters);
-        //     }
-        // });
-
-        // if (window.location.href.indexOf('#') > -1) {
-        //     $(window).trigger('hashchange');
-        // }
+        const activeCategoryFilter = urlHash ? urlHash.substring(1) : 'all';
+        const activeFilterButton = document.querySelector(`[data-filter="${activeCategoryFilter}"]`);
+        const filtered = filterResults(activeCategoryFilter, searchValue);
+        activateButton(activeFilterButton, filters);
+        showResults(filtered);
     }
+}
 
     // const containerEl = document.querySelector('#rules .list-group');
     // const inputSearch = document.querySelector('[data-ref="search"]');
