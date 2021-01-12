1a0ded301efe6e8d73a6272d6316ad2054baa579
bgdeutsch <bgdeutsch@gmail.com>
2021-01-12 15:09:47 -0500

    Fixes url hash bug, adds message for empty results set

--- src/scripts/components/security-rules.js
+++ src/scripts/components/security-rules.js
@@ -1,9 +1,12 @@
+import { stringToTitleCase } from '../helpers/string';
+
 export function initializeSecurityRules() {
     const inputSearch = document.querySelector('[data-ref="search"]');
     const controls = document.querySelector('[data-ref="controls"]');
     const allGroupHeaders = Array.prototype.slice.call(document.getElementsByClassName('js-group-header') || []);
     const allRules = document.querySelectorAll('.js-single-rule');
     const allRuleGroups = document.querySelectorAll('.js-group');
+    const jsEmptyResults = document.querySelector('.js-empty-results');
     const urlParams = new URLSearchParams(window.location.search);
     const urlHash = window.location.hash;
     const keyword = urlParams.get('q');
@@ -24,7 +27,7 @@ export function initializeSecurityRules() {
             results = allRules;
         }
 
-        if (searchValue && searchValue.length > 3) {
+        if (searchValue && searchValue.length > 2) {
             results = Array.from(results).filter(item => item.dataset.name.indexOf(searchValue) > -1);
         }
 
@@ -48,6 +51,14 @@ export function initializeSecurityRules() {
         });
     }
 
+    const handleEmptyResultSet = () => {
+        const searchQuery = inputSearch.value;
+        const activeCategoryFilter = stringToTitleCase(document.querySelector('.controls .active').text);
+        const message = `No results found for query "${searchQuery}" in category ${activeCategoryFilter}`;
+        jsEmptyResults.innerText = message;
+        jsEmptyResults.classList.remove('d-none');
+    }
+
     const showResults = (filteredResults) => {
         // Hide all groups, headers, and rules.
         allRules.forEach(element => {
@@ -61,6 +72,14 @@ export function initializeSecurityRules() {
         allRuleGroups.forEach(element => {
             element.style.display = 'none';
         })
+
+        // Handle empty result set
+        if (filteredResults.length < 1) {
+            handleEmptyResultSet();
+        } else {
+            jsEmptyResults.innerText = '';
+            jsEmptyResults.classList.add('d-none');
+        }
 
         // Show headers and groups for all matches
         filteredResults.forEach(element => {
@@ -77,19 +96,22 @@ export function initializeSecurityRules() {
         if (event.target.classList.contains('active') || !event.target.getAttribute('data-filter'))
             return;
             
-        const searchValue = inputSearch.value.length > 3 ? inputSearch.value.toLowerCase().trim() : '';
+        const searchValue = inputSearch.value.length > 2 ? inputSearch.value.toLowerCase().trim() : '';
         const filtered = filterResults(event.target.dataset.filter, searchValue);
         activateButton(event.target, filters);
         showResults(filtered);
     }
 
     const handleKeyup = () => {
-        const searchValue = inputSearch.value.length > 3 ? inputSearch.value.toLowerCase().trim() : '';
+        const searchValue = inputSearch.value.length > 2 ? inputSearch.value.toLowerCase().trim() : '';
         const activeCategoryFilter = document.querySelector('.controls .active').dataset.filter;
+        const { hash } = window.location;
+        const replaceUrl = hash ? `?q=${searchValue}${hash}` : `?q=${searchValue}`
+
         clearTimeout(keyupTimeout);
 
         keyupTimeout = setTimeout(() => {
-            window.history.replaceState({q: searchValue}, '', `?q=${searchValue}`);
+            window.history.replaceState({q: searchValue}, '', replaceUrl);
             const filtered = filterResults(activeCategoryFilter, searchValue);
             showResults(filtered);
         }, 350);
@@ -122,136 +144,3 @@ export function initializeSecurityRules() {
         showResults(filtered);
     }
 }
-
-    // const containerEl = document.querySelector('#rules .list-group');
-    // const inputSearch = document.querySelector('[data-ref="search"]');
-    // const controls = document.querySelector('[data-ref="controls"]');
-    // const ruleList = Array.prototype.slice.call(document.getElementsByClassName('rule-list') || []);
-    // const urlParams = new URLSearchParams(window.location.search);
-    // const keyword = urlParams.get('q');
-    // let keyupTimeout;
-    // let filters;
-    // let mixer;
-
-    // if (!containerEl) return;
-
-    // if(controls) {
-    //    filters = controls.querySelectorAll('[data-ref="filter"]');
-    // }
-
-    // ruleList.forEach(elm => {
-    //     elm.addEventListener("click", e => {
-    //         e.currentTarget.classList.toggle('active');
-    //     });
-    // });
-
-    // function filterByString(searchValue) {
-    //     if (searchValue) {
-    //         // Use an attribute wildcard selector to check for matches
-    //         mixer.filter(`[data-name*="${searchValue}"]`);
-    //         activateButton(controls.querySelector('[data-filter="all"]'), filters);
-    //     } else {
-    //         // If no searchValue, treat as filter('all')
-    //         mixer.filter('all');
-    //     }
-    // }
-
-    // function activateButton(activeButton, siblings) {
-    //     let button;
-    //     let i;
-
-    //     if (activeButton && siblings) {
-    //         for (i = 0; i < siblings.length; i++) {
-    //             button = siblings[i];
-    //             button.classList[button === activeButton ? 'add' : 'remove']('active');
-    //         }
-    //     }
-
-    //     // enable all
-    //     ruleList.forEach(elm => {
-    //         elm.classList.add('active');
-    //     });
-    // }
-
-    // if(containerEl) {
-    //     mixer = mixitup(containerEl, {
-    //         animation: {
-    //             duration: 350,
-    //             enable: false
-    //         },
-    //         callbacks: {
-    //             onMixClick() {
-    //                 // Reset the search if a filter is clicked
-    //                 if (this.matches('[data-filter]')) {
-    //                     inputSearch.value = '';
-    //                 }
-    //             },
-    //             onMixEnd(state){
-    //                 state.hide.forEach((x) => {
-    //                     x.closest(".js-group").style.display = 'none';
-    //                 });
-    //                 state.show.forEach((x) => {
-    //                     x.closest(".js-group").style.display = '';
-    //                 });
-    //             }
-    //         }
-    //     });
-
-    //     if (inputSearch) {
-    //         inputSearch.addEventListener('keyup', function () {
-    //             let searchValue;
-    //             if (inputSearch.value.length < 3) {
-    //                 // If the input value is less than 3 characters, don't send
-    //                 searchValue = '';
-    //             } else {
-    //                 searchValue = inputSearch.value.toLowerCase().trim();
-    //             }
-    //             clearTimeout(keyupTimeout);
-    //             keyupTimeout = setTimeout(function () {
-    //                 window.history.replaceState({q: searchValue}, '', `?q=${searchValue}`);
-    //                 filterByString(searchValue);
-    //             }, 350);
-    //         });
-    //     }
-
-    //     if (controls) {
-    //         controls.addEventListener('click', function (e) {
-    //             inputSearch.value = '';
-    //             const url = window.location.href.replace(window.location.search, '');
-    //             window.history.replaceState({q: ''}, '', url);
-    //             // If button is already active, or an operation is in progress, ignore the click
-    //             if (e.target.classList.contains('active') || !e.target.getAttribute('data-filter'))
-    //                 return;
-    //             activateButton(e.target, filters);
-    //         });
-    //     }
-
-    //     // Set controls the active controls on startup
-    //     if(controls) {
-    //         activateButton(controls.querySelector('[data-filter="all"]'), filters);
-    //     }
-    //     if(keyword) { filterByString(keyword); }
-
-    //     $(window).on('hashchange', function() {
-    //         let currentCat = '';
-    //         if (window.location.href.indexOf('#') > -1) {
-    //             currentCat = window.location.href.substring(
-    //                 window.location.href.indexOf('#')
-    //             );
-    //         }
-    //         const currentSelected = $('.controls .active').attr('href');
-    //         if (currentCat && currentSelected) {
-    //             if (currentCat !== currentSelected) {
-    //                 $(`a[href="${currentCat}"]`).get(0).click();
-    //             }
-    //         }
-    //         if (currentCat === '') {
-    //             activateButton(controls.querySelector('[data-filter="all"]'), filters);
-    //         }
-    //     });
-
-    //     if (window.location.href.indexOf('#') > -1) {
-    //         $(window).trigger('hashchange');
-    //     }
-    // }
-// }
