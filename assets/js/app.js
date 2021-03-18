b2ff1361c7611e2aa52332e911184c1ed97773c7
Luc Perkins <luc.perkins@datadoghq.com>
2021-03-18 12:01:47 -0700

    Integrate most of the JS into the Hugo build
    Signed-off-by: Luc Perkins <luc.perkins@datadoghq.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,28 @@
+import '@babel/polyfill';
 import 'bootstrap';
 
+import './api-redirect';
 import './datadog-docs';
+import './lang-redirects';
+import './region-redirects';
 
 import './components/announcement_banner';
+import './components/api';
+import './components/bootstrap-dropdown-custom';
+import './components/code-languages';
 import './components/copy-code';
+import './components/dd-browser-logs-rum';
 import './components/global-modals';
 import './components/header';
-import './components/platforms';
-import './components/algolia';
-import './components/api';
-import './components/code-languages';
 import './components/language-select';
-import './components/bootstrap-dropdown-custom';
+import './components/platforms';
+
+
+/*
+
+import './components/algolia';
+
+*/
 
 // TODO: split up code from datadog-docs.js into modules after webpack migration
 // import './components/sidenav';
