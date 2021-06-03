5adbae644b05d898c70bf55a411f8bfd258f639b
bgdeutsch <bgdeutsch@gmail.com>
2021-06-03 12:03:36 -0400

    Separates bootstrap modules, remove unnecessary webpack packages

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,5 @@
 const webpack = require('webpack');
 const path = require('path');
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WebpackAssetsManifest = require('webpack-assets-manifest');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
