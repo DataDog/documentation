b02b0ebf18d5b410788926f46ef3c39e76efdb2d
bgdeutsch <bgdeutsch@gmail.com>
2021-08-04 19:08:23 -0400

    Updates

--- local/bin/py/build/process_agent_config.py
+++ local/bin/py/build/process_agent_config.py
@@ -5,18 +5,23 @@ from collections import defaultdict
 
 def process_agent_config(config_yaml):
   separator = '#######'
-  lines = config_yaml.splitlines()
-  headings = []
+  agent_config_array = config_yaml.splitlines()
   agent_config_dict = {}
+  test_arr = []
 
-  print(enumerate(lines))
+  for index, line in enumerate(agent_config_array):
+    if separator in line:
+      heading = agent_config_array[index + 1].replace('#', '').strip()
 
-  # for index, line in enumerate(lines):
-  #   if separator in line:
-  #     heading = lines[index + 1].replace('#', '').strip()
+      if heading != '':
+        test_arr.append('!!!' + heading + '!!!')
+    else:
+      if line.startswith('#') and not line.startswith('##') and line != '#' and not '@param' in line:
+        test_arr.append(line)
+  
+  print(test_arr)
 
-      # if (heading != ''):
-      #   headings.append(heading)
+  # Loop over test_arr if str.startswith('!!!') create an object with that as the key and each index until it hits the next '!!!'.  (or perhaps not contains ('#') to delimit headers?)
   
 # config: {
 #   "basic configuration": [
