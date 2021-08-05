9080f14f3e2a9a3efe9d462313afaeb639543749
bgdeutsch <bgdeutsch@gmail.com>
2021-08-05 10:13:53 -0400

    Updates

--- local/bin/py/build/process_agent_config.py
+++ local/bin/py/build/process_agent_config.py
@@ -8,34 +8,29 @@ def process_agent_config(config_yaml):
   agent_config_array = config_yaml.splitlines()
   agent_config_dict = {}
   test_arr = []
+  current_header = ''
 
   for index, line in enumerate(agent_config_array):
     if separator in line:
       heading = agent_config_array[index + 1].replace('#', '').strip()
 
       if heading != '':
-        test_arr.append('!!!' + heading + '!!!')
+        test_arr.append(heading)
     else:
-      if line.startswith('#') and not line.startswith('##') and line != '#' and not '@param' in line:
+      if '#' in line and not '##' in line and not '@param' in line:
         test_arr.append(line)
-  
-  print(test_arr)
 
-  # Loop over test_arr if str.startswith('!!!') create an object with that as the key and each index until it hits the next '!!!'.  (or perhaps not contains ('#') to delimit headers?)
-  
-# config: {
-#   "basic configuration": [
-#     "site: datadoghq.com",
-#     "dd_url": "zyx.xdif"
-#   ]
-# }
+  for index in test_arr:
+    if '#' not in index:
+      agent_config_dict[index] = ''
+      current_header = index
+    else:
+      agent_config_dict[current_header] += index
 
-# config: {
-#   "basic configuration": [
-#     {"site": "datadoghq.com"},
-#     {"dd_url": "zyx.xdif"}
-#   ]
-# }
+  formatted_agent_config_json = json.dumps(agent_config_dict)
+
+  with open('data/agent_config.json', 'w+') as outfile:
+      outfile.write(formatted_agent_config_json)
 
 
   
