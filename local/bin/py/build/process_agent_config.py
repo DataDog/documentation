22cd1ec29b7816e33856dd2d185cceb06db53f5a
bgdeutsch <bgdeutsch@gmail.com>
2021-08-06 10:53:12 -0400

    Updates

--- local/bin/py/build/process_agent_config.py
+++ local/bin/py/build/process_agent_config.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
 import json
+import yaml
 from collections import defaultdict
 
 def process_agent_config(config_yaml):
   separator = '#######'
   agent_config_array = config_yaml.splitlines()
   agent_config_dict = {}
-  test_arr = []
   current_header = ''
 
   for index, line in enumerate(agent_config_array):
@@ -15,19 +15,18 @@ def process_agent_config(config_yaml):
       heading = agent_config_array[index + 1].replace('#', '').strip()
 
       if heading != '':
-        test_arr.append(heading)
+        agent_config_dict[heading] = '' # use defaultdict.setdefault here?
+        current_header = heading
     else:
       if '#' in line and not '##' in line and not '@param' in line:
-        test_arr.append(line)
-
-  for index in test_arr:
-    if '#' not in index:
-      agent_config_dict[index] = ''
-      current_header = index
-    else:
-      agent_config_dict[current_header] += index
+        formatted_string = line.replace('#', '')
+        formatted_string = formatted_string
+        agent_config_dict[current_header] += formatted_string
 
   formatted_agent_config_json = json.dumps(agent_config_dict)
+  formatted_agent_config_yaml = yaml.dump(agent_config_dict, sort_keys=False)
+
+  print(formatted_agent_config_yaml)
 
   with open('data/agent_config.json', 'w+') as outfile:
       outfile.write(formatted_agent_config_json)
