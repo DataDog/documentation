cedecbf7ffb7722c52b1665bfe74a75e1d2eae6c
bgdeutsch <bgdeutsch@gmail.com>
2021-08-10 16:02:57 -0400

    Updates

--- local/bin/py/build/process_agent_config.py
+++ local/bin/py/build/process_agent_config.py
@@ -11,6 +11,7 @@ def process_agent_config(config_yaml):
     current_header = ''
 
     for index, line in enumerate(agent_config_array):
+      print(line)
       if header_delimiter in line:
         heading = agent_config_array[index + 1].replace('#', '').strip().lower()
 
@@ -18,7 +19,7 @@ def process_agent_config(config_yaml):
           agent_config_dict.setdefault(heading, '')
           current_header = heading
       else:
-        
+
         # if '#' in line and not '##' in line and not '@param' in line and line != '#':
           # formatted_string = line.replace('#', '') + '\n'
 
@@ -26,7 +27,7 @@ def process_agent_config(config_yaml):
           if '##' in line and not line.startswith('###'):
             formatted_string = line + '\n'
           else:
-            formatted_string = line.replace('#', '') + '\n' + '\n'
+            formatted_string = line.replace('#', '') + '\n'
 
           agent_config_dict[current_header] += formatted_string
 
