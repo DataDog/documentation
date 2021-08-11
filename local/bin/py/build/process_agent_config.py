be776551eacb599e2170bbc414dde3e7f164ae68
bgdeutsch <bgdeutsch@gmail.com>
2021-08-11 16:56:37 -0400

    Adding automated documentation, code adjustments

--- local/bin/py/build/process_agent_config.py
+++ local/bin/py/build/process_agent_config.py
@@ -14,30 +14,37 @@ def format_agent_config_string(string):
 
 def process_agent_config(config_yaml):
   try:
-    header_delimiter = '#######'
+    config_type_header_delimiter = '#######'
     agent_config_array = config_yaml.splitlines()
     agent_config_dict = {}
-    current_header = ''
+    current_config_type = ''
+    config_types_string = ''
 
     for index, line in enumerate(agent_config_array):
-      if header_delimiter in line:
-        heading = agent_config_array[index + 1].replace('#', '').strip().lower()
+      if config_type_header_delimiter in line:
+        config_type = agent_config_array[index + 1].replace('#', '').strip().lower()
 
-        if heading != '':
-          agent_config_dict.setdefault(heading, '')
-          current_header = heading
+        if config_type != '':
+          agent_config_dict.setdefault(config_type, '')
+          current_config_type = config_type
+          config_types_string += config_type + '\n'
       else:
+        # Skip any Go template strings or comment boxes used to delineate config types.
         if '{{' in line or line.startswith('##') and line.endswith('##'):
           continue
         else:
           formatted_string = format_agent_config_string(line)
 
-        agent_config_dict[current_header] += formatted_string
-        
+        agent_config_dict[current_config_type] += formatted_string
+
     formatted_agent_config_json = json.dumps(agent_config_dict)
 
-    with open('data/agent_config.json', 'w+') as outfile:
-        outfile.write(formatted_agent_config_json)
+    with open('data/agent_config.json', 'w+') as agent_json_config_outfile:
+      agent_json_config_outfile.write(formatted_agent_config_json)
+
+    # Documenting what config types are available for Docs team to use in agent config shortcode.
+    with open('agent_config_types_map.txt', 'w+') as agent_config_types_map_outfile:
+      agent_config_types_map_outfile.write(config_types_string)
   except Exception as err:
     print('An error occurred:')
     print(err)
