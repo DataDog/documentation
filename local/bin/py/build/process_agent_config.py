6439d1b92ea10c5aceff669167959ab8356a4b5e
bgdeutsch <bgdeutsch@gmail.com>
2021-08-12 16:44:07 -0400

    python code updates

--- local/bin/py/build/process_agent_config.py
+++ local/bin/py/build/process_agent_config.py
@@ -1,53 +1,67 @@
 #!/usr/bin/env python3
 
 import json
+import re
 from collections import defaultdict
 
 def format_agent_config_string(string):
-  # Could try using Regex here, like this? ^[^a-zA-Z0-9#]*#[^a-zA-Z0-9#]*$
-  if string == '#' or string == ' #' or string == '  #':
+  """
+  Takes a string from the agent config template and formats it for output in the agent_config shortcode.
+    - If string contains exactly one '#' and no alphabetic characters, remove the '#' and any new line characters.
+    - If the string contains exactly one '#' and has alphabetic characters, remove the '#' and leave the characters (this represents a config key/value)
+  """
+  # Match any lines containing strictly one '#' character that can have any number of leading or trailing whitespaces, and no words.  Matching lines in the config template file are for spacing only.
+  regex_string = r"[^a-zA-Z0-9#]*#[^a-zA-Z0-9#]*$"
+
+  if re.match(regex_string, string):
     return string.replace('#', '').replace('\n', '')
   elif '#' in string and '##' not in string:
     return string.replace('#', '') + '\n'
   else:
     return string + '\n'
 
-def process_agent_config(config_yaml):
-  try:
-    config_type_header_delimiter = '#######'
-    agent_config_array = config_yaml.splitlines()
-    agent_config_dict = {}
-    current_config_type = ''
-    config_types_string = ''
+def process_agent_config(dd_agent_config_string):
+  """
+  Takes the Datadog Agent Config template as a string, separates it by type, formats the strings, and outputs the results as json to be consumed by the agent_config.html shortcode for display.
+  """
+  config_type_header_delimiter = '#######'
+  agent_config_array = dd_agent_config_string.splitlines()
+  agent_config_dict = {}
+  current_config_type = ''
+  config_types_string = ''
 
-    for index, line in enumerate(agent_config_array):
-      if config_type_header_delimiter in line:
-        config_type = agent_config_array[index + 1].replace('#', '').strip().lower()
+  for index, line in enumerate(agent_config_array):
+    if config_type_header_delimiter in line:
+      config_type = agent_config_array[index + 1].replace('#', '').strip().lower()
 
-        if config_type != '':
-          agent_config_dict.setdefault(config_type, '')
-          current_config_type = config_type
-          config_types_string += config_type + '\n'
+      if config_type != '':
+        agent_config_dict.setdefault(config_type, '')
+        current_config_type = config_type
+        config_types_string += config_type + '\n'
+    else:
+      # Skip any Go template strings or comment boxes used to delineate config types.
+      if '{{' in line or line.startswith('##') and line.endswith('##'):
+        continue
       else:
-        # Skip any Go template strings or comment boxes used to delineate config types.
-        if '{{' in line or line.startswith('##') and line.endswith('##'):
-          continue
-        else:
-          formatted_string = format_agent_config_string(line)
+        formatted_string = format_agent_config_string(line)
 
-        agent_config_dict[current_config_type] += formatted_string
+      agent_config_dict[current_config_type] += formatted_string
+  
+  formatted_agent_config_json = json.dumps(agent_config_dict)
 
-    formatted_agent_config_json = json.dumps(agent_config_dict)
-
+  try:
     with open('data/agent_config.json', 'w+') as agent_json_config_outfile:
       agent_json_config_outfile.write(formatted_agent_config_json)
 
     # Documenting what config types are available for Docs team to use in agent config shortcode.
     with open('agent_config_types_map.txt', 'w+') as agent_config_types_map_outfile:
       agent_config_types_map_outfile.write(config_types_string)
-  except Exception as err:
-    print('An error occurred:')
-    print(err)
+  except IOError as io_error:
+    print('An error occurred writing agent config data to datafile:')
+    print(io_error)
+
+    if getenv("LOCAL") != 'True':
+      sys.exit(1)
 
 
   
