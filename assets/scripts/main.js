631e4b85ad0b01458f14534f06be9f97d70f9de7
bgdeutsch <bgdeutsch@gmail.com>
2021-09-17 08:56:22 -0400

    Region redirects doesnt need to load in document head

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,7 @@
 import 'bootstrap';
 import './api-redirect';
 import './datadog-docs';
+import './region-redirects';
 import './components/copy-code';
 import './components/global-modals';
 import './components/header';
