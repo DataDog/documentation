5a520c120bc92d64e452f5f32045fff48051a704
bgdeutsch <bgdeutsch@gmail.com>
2021-09-27 16:22:32 -0400

    test

--- local/bin/js/build-source-maps.js
+++ local/bin/js/build-source-maps.js
@@ -1,7 +1,7 @@
 // "build:sourcemaps:preview": "esbuild ./assets/scripts/main.js --sourcemap --outdir=public/scripts/",  <- cli
 
 const esBuildModule = require('esbuild');
-const scriptsDir = '../../assets/scripts';
+const scriptsDir = './assets/scripts';
 
 esBuildModule.build({
   entryPoints: [`${scriptsDir}/main.js`, `${scriptsDir}/components/search.js`, `${scriptsDir}/components/dd-browser-logs-rum.js`],
