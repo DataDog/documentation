8d1bd7c8de98d48843c27fb6b7f1f1ec904d23ee
bgdeutsch <bgdeutsch@gmail.com>
2021-09-27 14:53:17 -0400

    cleans up script element in JS partial, try to use esbuild api instead of cli

---
+++ local/bin/js/build-source-maps.js
@@ -0,0 +1,10 @@
+// "build:sourcemaps:preview": "esbuild ./assets/scripts/main.js --sourcemap --outdir=public/scripts/",  <- cli
+
+const esBuildModule = require('esbuild');
+const scriptsDir = '../../public/scripts';
+
+esBuildModule.build({
+  entryPoints: [`${scriptsDir}/main.js`, `${scriptsDir}/components/search.js`, `${scriptsDir}/components/dd-browser-logs-rum.js`],
+  sourcemap: true,
+  outdir: scriptsDir 
+})
