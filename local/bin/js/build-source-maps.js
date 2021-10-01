70d6cdc10ecd68ad47ab6108b26806397e83ff73
bgdeutsch <bgdeutsch@gmail.com>
2021-10-01 14:30:49 -0400

    test

--- local/bin/js/build-source-maps.js
+++ local/bin/js/build-source-maps.js
@@ -2,7 +2,7 @@ const esBuildModule = require('esbuild');
 const scriptsDir = './assets/scripts';
 
 esBuildModule.buildSync({
-  entryPoints: [`${scriptsDir}/main.js`, `${scriptsDir}/components/search.js`, `${scriptsDir}/components/dd-browser-logs-rum.js`],
+  entryPoints: [`${scriptsDir}/main.*.js`, `${scriptsDir}/components/search.js`, `${scriptsDir}/components/dd-browser-logs-rum.js`],
   sourcemap: true,
   outdir: 'public/scripts',
   allowOverwrite: true,
