d557aa8bfde05407e694b9e9af8d188d6fce7ac0
bgdeutsch <bgdeutsch@gmail.com>
2021-10-01 16:12:01 -0400

    test

--- local/bin/js/build-source-maps.js
+++ local/bin/js/build-source-maps.js
@@ -1,3 +1,5 @@
+// This is needed to build and upload JS source maps to Datadog (RUM Error Tracking).
+// Hugo only has support for inlining Source Maps currently.  Here we use esbuild to generate external source maps which are required to get full stack traces in RUM.
 const esBuildModule = require('esbuild');
 const glob = require('glob');
 const scriptsDir = './assets/scripts';
