e68e6c5f44596a316d2cf20686e317eb313c0d1c
bgdeutsch <bgdeutsch@gmail.com>
2021-10-01 15:39:40 -0400

    try installing glob for path wildcards in build script

--- local/bin/js/build-source-maps.js
+++ local/bin/js/build-source-maps.js
@@ -1,6 +1,7 @@
 const esBuildModule = require('esbuild');
+const glob = require('glob');
 const scriptsDir = './assets/scripts';
-const entryPoints =[`${scriptsDir}/main.**.js`, `${scriptsDir}/components/search.js`, `${scriptsDir}/components/dd-browser-logs-rum.js`];
+const entryPoints =[glob(`${scriptsDir}/main.*.js`), `${scriptsDir}/components/search.js`, `${scriptsDir}/components/dd-browser-logs-rum.js`];
 
 esBuildModule.buildSync({
   entryPoints,
