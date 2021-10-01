e28524550f8f04b37cf8e1af153fd0c75eaec8a0
bgdeutsch <bgdeutsch@gmail.com>
2021-10-01 12:24:25 -0400

    try building sourcemaps with external option

--- local/bin/js/build-source-maps.js
+++ local/bin/js/build-source-maps.js
@@ -4,6 +4,7 @@ const scriptsDir = './assets/scripts';
 esBuildModule.build({
   entryPoints: [`${scriptsDir}/main.js`, `${scriptsDir}/components/search.js`, `${scriptsDir}/components/dd-browser-logs-rum.js`],
   sourcemap: true,
+  external: true,
   outdir: 'public/scripts',
   allowOverwrite: true
 })
