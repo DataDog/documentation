04b1b52091f311d0a5f4c421bdf647bb0656c453
guacbot <robot-guacbot-guacamole@datadoghq.com>
2021-12-01 10:28:15 +0000

    initial commit

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module documentation
 
 go 1.14
 
-require github.com/DataDog/websites-modules v1.3.8 // indirect
+require github.com/DataDog/websites-modules v1.3.8-0.20220121135250-a314e0b8ee6f
 
 // replace github.com/DataDog/websites-modules => /Users/carlos.santos/GitHub/websites-modules
---
+++ src/scripts/components/mobile-nav.js
@@ -0,0 +1,46 @@
+// MOBILE MENU
+
+const mobileOverlay = document.querySelector('#mobile-nav-bg');
+const mobileMenu = document.querySelector('#mobile-nav');
+const mobileNavbarTogglers = document.querySelectorAll('.navbar-toggler');
+
+let mobileToggle = false;
+
+function mobileToggleIconToggle(e) {
+    e.preventDefault();
+    mobileToggle = !mobileToggle;
+    mobileOverlay.classList.toggle('mobile-bg-open');
+    mobileMenu.classList.toggle('mobile-menu-open');
+    document.body.classList.toggle('body-noscroll');
+    mobileNavbarTogglers.forEach((mobileNavbarToggler) => {
+        mobileNavbarToggler.classList.toggle('open');
+    });
+}
+
+window.addEventListener('resize', function () {
+    const { innerWidth } = window;
+
+    if (mobileMenu && mobileNavbarTogglers.length) {
+        if (innerWidth >= 991) {
+            mobileOverlay.classList.remove('mobile-bg-open');
+            mobileMenu.classList.remove('mobile-menu-open');
+            document.body.classList.remove('body-noscroll');
+            mobileNavbarTogglers.forEach((mobileNavbarToggler) => {
+                mobileNavbarToggler.classList.remove('open');
+            });
+            mobileMenu.style.right = '-500px';
+        }
+    }
+});
+
+if (mobileNavbarTogglers.length && !document.body.classList.contains('aws-microsite')) {
+    mobileNavbarTogglers.forEach((mobileNavbarToggler) => {
+        mobileNavbarToggler.addEventListener('click', mobileToggleIconToggle);
+    });
+}
+
+const announcementBanner = document.querySelector('div.announcement-banner');
+
+if ((announcementBanner && window.getComputedStyle(announcementBanner).getPropertyValue('display') === 'none') || (!announcementBanner)) {
+  mobileMenu.style.paddingTop = '65px';
+}
