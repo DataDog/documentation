b258baf00b0bc4d8cbe268eee0ac90f28531227f
bgdeutsch <bgdeutsch@gmail.com>
2022-06-09 14:30:17 -0400

    Refactors jquery to vanilla js

--- assets/scripts/components/codetabs.js
+++ assets/scripts/components/codetabs.js
@@ -1,49 +1,22 @@
 import { getQueryParameterByName } from '../helpers/browser';
 
-const codeTabNodeList = document.querySelectorAll('.code-tabs')
+const codeTabsList = document.querySelectorAll('.code-tabs')
+const tabQueryParameter = getQueryParameterByName('tab')
 
-const initTabs = () => {
-    const tab = getQueryParameterByName('tab')
-    // const codeTabNodeList = document.querySelectorAll('.code-tabs')
-
-    // create tabs dynamically
-    createTabs()
-
-    activateTabOnLoad()
-
-    addTabEventListeners()
-
-    // activate tab on page load
-    if (tab) {
-        const selectedLanguageTab = document.querySelector(`a[data-lang="${tab}"]`);
-
-        if (selectedLanguageTab) {
-            selectedLanguageTab.click()
-        }
-    } else {            
-        codeTabNodeList.forEach(codeTab => {
-            const navTabs = codeTab.querySelector('.nav-tabs')
-            const firstTabLink = navTabs.querySelectorAll('a').item(0)
-            activateTab(firstTabLink)
-        })
-    }
-
-    const allTabLinksNodeList = document.querySelectorAll('.code-tabs li a')
-
-    allTabLinksNodeList.forEach(link => {
-        link.addEventListener('click', () => {
-            console.log(link)
-            event.preventDefault()
-            activateTab(link)
-        })
-    })
+const initCodeTabs = () => {
+    renderCodeTabElements()
+    addEventListeners()
+    activateTabsOnLoad()
 }
 
-const createTabs = () => {
-    if (codeTabNodeList.length > 0) {
-        codeTabNodeList.forEach(codeTab => {
-            const navTabsElement = codeTab.querySelector('.nav-tabs')
-            const tabContent = codeTab.querySelector('.tab-content')
+/**
+  * Renders code tabs on the page at run time
+*/
+const renderCodeTabElements = () => {
+    if (codeTabsList.length > 0) {
+        codeTabsList.forEach(codeTabsElement => {
+            const navTabsElement = codeTabsElement.querySelector('.nav-tabs')
+            const tabContent = codeTabsElement.querySelector('.tab-content')
             const tabPaneNodeList = tabContent.querySelectorAll('.tab-pane')
 
             tabPaneNodeList.forEach(tabPane => {
@@ -61,80 +34,83 @@ const createTabs = () => {
     }
 }
 
-const activateTab = (element) => {
-    const activeLang = element.getAttribute('data-lang')
+/**
+  * Adds active class to the selected tab and shows the related tab pane. 
+  * @param {object} tabAnchorElement - Reference to the HTML DOM node representing the anchor tag within each tab.
+*/
+const activateCodeTab = (tabAnchorElement) => {
+    const activeLang = tabAnchorElement.getAttribute('data-lang')
 
     if (activeLang) {
-        const allActiveTabLangs = document.querySelectorAll(`a[data-lang="${activeLang}"]`)
+        codeTabsList.forEach(codeTabsElement => {
+            const tabsList = codeTabsElement.querySelectorAll('.nav-tabs li')
+            const tabPanesList = codeTabsElement.querySelectorAll('.tab-pane')
+            const activeLangTab = codeTabsElement.querySelector(`a[data-lang="${activeLang}"]`)
+            const activePane = codeTabsElement.querySelector(`.tab-pane[data-lang="${activeLang}"]`)
 
-        allActiveTabLangs.forEach(tab => {
-            const parentLi = tab.closest('li')
-            parentLi.classList.add('active')
+            if (activeLangTab && activePane) {
+                // Hide all tab content and remove 'active' class from all tab elements.
+                tabsList.forEach(tab => tab.classList.remove('active'))
+                tabPanesList.forEach(pane => pane.classList.remove('active', 'show'))
+
+                // Show the active content and highlight active tab.
+                activeLangTab.closest('li').classList.add('active')
+                activePane.classList.add('active', 'show')
+            }
+            
+            const currentActiveTab = codeTabsElement.querySelector('.nav-tabs li.active')
+
+            // If we got this far and no tabs are highlighted, activate the first tab in the list of code tabs.
+            if (!currentActiveTab) {
+                const firstTab = tabsList.item(0)
+                const firstTabActiveLang = firstTab.querySelector('a').dataset.lang
+                const firstTabPane = codeTabsElement.querySelector(`.tab-pane[data-lang="${firstTabActiveLang}"]`)
+                firstTab.classList.add('active')
+                firstTabPane.classList.add('active', 'show')
+            }
         })
+    }
 
-        // find closest tab that has the data lang
-        const tabPanes = document.querySelectorAll(`.tab-pane[data-lang="${activeLang}"]`)
+    updateUrl(activeLang)
+}
 
-        tabPanes.forEach(pane => {
-            pane.classList.add('active')
-            pane.classList.add('show')
-        })
+const activateTabsOnLoad = () => {
+    if (tabQueryParameter) {
+        const selectedLanguageTab = document.querySelector(`a[data-lang="${tabQueryParameter}"]`);
+
+        if (selectedLanguageTab) {
+            selectedLanguageTab.click()
+        }
+    } else {
+        const firstTab = document.querySelectorAll('.code-tabs .nav-tabs a').item(0)
+        activateCodeTab(firstTab)
     }
 }
 
-const codeTabs = () => {
-    initTabs()
+const addEventListeners = () => {
+    const allTabLinksNodeList = document.querySelectorAll('.code-tabs li a')
+
+    allTabLinksNodeList.forEach(link => {
+        link.addEventListener('click', () => {
+            event.preventDefault()
+            activateCodeTab(link)
+        })
+    })
 }
 
-// function codeTabs() {
-//     const tab = getQueryParameterByName('tab');
+/**
+  * Append the active lang to the URL as a query parameter. 
+*/
+const updateUrl = (activeLang) => {
+    const url = window.location.href
+        .replace(window.location.hash, '')
+        .replace(window.location.search, '');
 
-//     if ($('.code-tabs').length > 0) {
-//         // clicking a tab open them all
-//         $('.code-tabs .nav-tabs a').click(function (e) {
-//             e.preventDefault();
+    window.history.replaceState(
+        null,
+        null,
+        `${url}?tab=${activeLang}${window.location.hash}`
+    );
+}
 
-//             // find all
-//             const lang = $(this).data('lang');
-//             $('.code-tabs .nav-tabs').each(function () {
-//                 const navtabs = $(this);
-//                 const links = $(this).find('a:first');
-//                 const langLinks = $(this).find(`a[data-lang="${lang}"]`);
-//                 if (langLinks.length) {
-//                     langLinks.each(function () {
-//                         activateTab($(this));
-//                     });
-//                 } else if (navtabs.find('.active').length === 0) {
-//                     // set first lang selected if nothing selected
-//                     links.each(function () {
-//                         activateTab($(this));
-//                     });
-//                 }
-//             });
-
-//             const url = window.location.href
-//                 .replace(window.location.hash, '')
-//                 .replace(window.location.search, '');
-//             window.history.replaceState(
-//                 null,
-//                 null,
-//                 `${url}?tab=${lang}${window.location.hash}`
-//             );
-//         });
-// }
-
-// function activateTab(el) {
-//     const tab = el.parent();
-//     const tabIndex = tab.index();
-//     const tabPanel = el.closest('.code-tabs');
-//     const tabPane = tabPanel.find('.tab-pane').eq(tabIndex);
-//     tabPanel.find('.active').removeClass('active');
-//     tab.addClass('active');
-//     tabPane.addClass('active');
-//     tabPane.addClass('show');
-//     el.closest('.code-tabs')
-//         .find('.nav-tabs-mobile .title-dropdown')
-//         .text(tab.text());
-// }
-
-export default codeTabs;
+export default initCodeTabs;
