2d516577231502ba706476a77082d5ec03a98887
bgdeutsch <bgdeutsch@gmail.com>
2022-07-06 08:58:58 -0400

    log referrer on page load to help debug synthetics

--- assets/scripts/region-redirects.js
+++ assets/scripts/region-redirects.js
@@ -3,6 +3,7 @@ import config from './regions.config';
 
 // need to wait for DOM since this script is loaded in the <head>
 document.addEventListener('DOMContentLoaded', () => {
+    console.log(`Referrer: ${document.referrer}`)
     const regionSelector = document.querySelector('.js-region-select')
     const currentUserSavedRegion = Cookies.get('site')
     const currentReferrerAppRegion = getDDSiteFromReferrer()
