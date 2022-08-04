bee458de70e8b96f764ca04f347d336fbfc7ef93
David Weid II <david.weid@datadoghq.com>
2022-08-04 16:48:33 -0400

    Update algolia-config.js

--- local/bin/js/algolia-config.js
+++ local/bin/js/algolia-config.js
@@ -1,20 +1,16 @@
-/* eslint-disable */
 const algoliasearch = require('algoliasearch');
 
-const ALGOLIA_APP_ID = process.env.ALGOLIA_APP_ID || '';
-const ALGOLIA_ADMIN_KEY = process.env.ALGOLIA_ADMIN_KEY || '';
-let ALGOLIA_INDEX_NAME = process.env.ALGOLIA_INDEX_NAME || '';
-
-// If this is a gitlab run and we don't have an index name already defined lets pull
-// from the config so we can define in one place
 if (process.env.CI_ENVIRONMENT_NAME && !ALGOLIA_INDEX_NAME) {
-    const configDocs = require('../../../assets/scripts/config/config-docs');
+    const configCorp = require('../../../config-corp');
     switch (process.env.CI_ENVIRONMENT_NAME) {
         case 'live':
-            ALGOLIA_INDEX_NAME = configDocs['live'].algoliaConfig.index;
+            ALGOLIA_INDEX_NAME = configCorp['production'].algoliaIndexes.primary_index;
             break;
         case 'preview':
-            ALGOLIA_INDEX_NAME = configDocs['preview'].algoliaConfig.index;
+            ALGOLIA_INDEX_NAME = configCorp['preview'].algoliaIndexes.primary_index;
+            break;
+        case 'staging':
+            ALGOLIA_INDEX_NAME = configCorp['staging'].algoliaIndexes.primary_index;
             break;
         default:
             ALGOLIA_INDEX_NAME = '';
@@ -28,65 +24,76 @@ if (ALGOLIA_APP_ID === '' || ALGOLIA_INDEX_NAME === '' || ALGOLIA_ADMIN_KEY === 
 }
 
 const replicas = {};
-replicas[`${ALGOLIA_INDEX_NAME}_api`] = {
-    attributesToIndex: [
-      "unordered(hierarchy_radio_camel.lvl0)",
-      "unordered(hierarchy_radio.lvl0)",
-      "unordered(hierarchy_radio_camel.lvl1)",
-      "unordered(hierarchy_radio.lvl1)",
-      "unordered(hierarchy_radio_camel.lvl2)",
-      "unordered(hierarchy_radio.lvl2)",
-      "unordered(hierarchy_radio_camel.lvl3)",
-      "unordered(hierarchy_radio.lvl3)",
-      "unordered(hierarchy_radio_camel.lvl4)",
-      "unordered(hierarchy_radio.lvl4)",
-      "unordered(hierarchy_camel.lvl0)",
-      "unordered(hierarchy.lvl0)",
-      "unordered(hierarchy_camel.lvl1)",
-      "unordered(hierarchy.lvl1)",
-      "unordered(hierarchy_camel.lvl2)",
-      "unordered(hierarchy.lvl2)",
-      "unordered(hierarchy_camel.lvl3)",
-      "unordered(hierarchy.lvl3)",
-      "unordered(hierarchy_camel.lvl4)",
-      "unordered(hierarchy.lvl4)",
-      "content",
-      "unordered(tags)"
-    ],
-    attributesToRetrieve: [
-      "anchor",
-      "content",
-      "hierarchy",
-      "tags",
-      "url"
-    ],
-    attributesForFaceting: [
-      "language",
-      "searchable(tags)"
-    ],
-    customRanking: [
-      "asc(tags)",
-      "desc(weight.page_rank)",
-      "desc(weight.level)",
-      "asc(weight.position)"
-    ]
+replicas[`${ALGOLIA_INDEX_NAME}_API`] = {
+    ranking: ['typo', 'geo', 'words', 'filters', 'proximity', 'attribute', 'exact', 'custom'],
+    customRanking: ['desc(rank)']
 };
 
-const settings = { replicas: Object.keys(replicas) };
+const settings = {
+    searchableAttributes: ['title', 'relpermalink', 'type, tags', 'unordered(description), unordered(content)'],
+    ranking: ['typo', 'geo', 'words', 'filters', 'proximity', 'attribute', 'exact', 'custom'],
+    customRanking: ['desc(rank)'],
+    replicas: Object.keys(replicas),
+    attributesToHighlight: ['title', 'description', 'content', 'type', 'tags'],
+    indexLanguages: ['ja', 'en', 'fr'],
+    queryLanguages: ['ja', 'en', 'fr'],
+    attributeForDistinct: 'relpermalink',
+    distinct: 1
+};
+
+const synonyms = [
+    {
+        objectID: 'agent',
+        type: 'synonym',
+        synonyms: ['agent', 'datadog agent']
+    },
+    {
+        objectID: 'azure',
+        type: 'synonym',
+        synonyms: ['microsoft azure', 'azure']
+    },
+    {
+        objectID: 'gcp',
+        type: 'synonym',
+        synonyms: ['gcp', 'google cloud platform']
+    },
+    {
+        objectID: 'aws',
+        type: 'synonym',
+        synonyms: ['aws', 'amazon web service']
+    },
+    {
+        objectID: 'rum',
+        type: 'synonym',
+        synonyms: ['RUM', 'rum', 'real user monitoring']
+    },
+    {
+        objectID: 'permissions',
+        type: 'synonym',
+        synonyms: ['permissions', 'Role Permissions']
+    }
+];
 
 const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_ADMIN_KEY);
 console.log(`Updating primary index configuration on ${ALGOLIA_INDEX_NAME}..`);
+
 const index = client.initIndex(ALGOLIA_INDEX_NAME);
+
 index.setSettings(settings, { forwardToReplicas: true }).then((response) => {
     console.log(`Index ${ALGOLIA_INDEX_NAME} configuration update complete..`);
 });
 
-/*
-  First time replica creation will likely needs a second run of this script as it’s impossible
-  to forward settings and create your replicas at the same time.
-  https://www.algolia.com/doc/api-reference/api-methods/set-settings/#method-param-forwardtoreplicas
-*/
+index
+    .saveSynonyms(synonyms, {
+        forwardToReplicas: true,
+        replaceExistingSynonyms: true
+    })
+    .then(() => {
+        console.log(`Index ${ALGOLIA_INDEX_NAME} synonyms update complete..`);
+    });
+
 console.log('Updating replicas..');
+
 Object.entries(replicas).forEach(([replicaIndexName, replicaSettings]) => {
     console.log(`Updating replica ${replicaIndexName}..`);
     const index = client.initIndex(replicaIndexName);
