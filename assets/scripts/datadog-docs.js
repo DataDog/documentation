9f50eb2e555c1e20eb6df273aa6793bac4ec6129
StefonSimmons <stefon.simmons@datadoghq.com>
2022-09-28 18:20:12 -0400

    set `.sidenav-nav` max-height via jquery

--- assets/scripts/datadog-docs.js
+++ assets/scripts/datadog-docs.js
@@ -422,11 +422,14 @@ window.addEventListener(
 function scrollActiveNavItemToTop(){
     // Scroll the open top level left nav item into view below Docs search input
     if (document.querySelector('.sidenav:not(.sidenav-api)')) {
-        const headerHeight = $('body .main-nav').height();
+        const headerHeight = document.querySelector('body .main-nav').style.height;
         const padding = 200;
 
-        // set max height of side nav
-        document.querySelector('.sidenav-nav').style.maxHeight = document.documentElement.clientHeight - headerHeight - padding
+        // set max height of side nav. vanilla js here doesnt enable you to set the max height
+        $('.sidenav-nav').css(
+            'maxHeight',
+            document.documentElement.clientHeight - headerHeight - padding
+        );
     
         const leftSideNav = document.querySelector('.sidenav:not(.sidenav-api) .sidenav-nav');
         const sideNavActiveMenuItem = leftSideNav.querySelector('li.open');
