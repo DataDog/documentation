c0669e24daae290cf47c097921c2ed34d39a53c7
Brian Deutsch <brian.deutsch@datadoghq.com>
2022-10-14 14:19:54 -0400

    Separates cloud siem rules

--- local/bin/py/build/actions/security_rules.py
+++ local/bin/py/build/actions/security_rules.py
@@ -143,7 +143,10 @@ def security_rules(content, content_dir):
 
                         # new categorization
                         if any(sub_path in relative_path for sub_path in ['security-monitoring', 'cloud-siem']):
-                            page_data['rule_category'].append('Cloud SIEM')
+                            if 'signal-correlation/production' in relative_path:
+                                page_data['rule_category'].append('Cloud SIEM (Correlated)')
+                            else:
+                                page_data['rule_category'].append('Cloud SIEM (Logs)')
 
                         if 'posture-management' in relative_path:
                             if 'cloud-configuration' in relative_path:
