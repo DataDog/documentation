b82c6d8b1d028365e20407dc5f1a06ec63eca6cf
Brian Deutsch <brian.deutsch@datadoghq.com>
2022-11-08 16:54:22 -0500

    Test outputting api action as markdown content for indexing

--- assets/scripts/build-api-pages.js
+++ assets/scripts/build-api-pages.js
@@ -127,6 +127,24 @@ const createPages = (apiYaml, deref, apiVersion) => {
     // create a copy in /latest/
     indexYamlStr = yaml.safeDump(baseFrontMatter);
     indexYamlStr = `---\n${indexYamlStr}---\n`;
+
+    // create markdown containing summary and description for each endpoint
+    // this is for compiling the search index, it is not used for rendering content.
+    if (tag.name === 'CI Visibility Tests') {
+      // just get this sections data
+      const data = Object.keys(deref.paths)
+        .filter((path) => isTagMatch(deref.paths[path], tag.name))
+        .map((path) => deref.paths[path]);
+
+      data.forEach((actionObj) => {
+        Object.entries(actionObj).forEach(([actionKey, action]) => {
+          const { summary, description } = action
+
+          indexYamlStr += `## ${summary}\n\n${description}\n\n`
+        })
+      })
+    }
+
     fs.mkdirSync(`./content/en/api/latest/${newDirName}`, {recursive: true});
     fs.writeFileSync(`./content/en/api/latest/${newDirName}/_index.md`, indexYamlStr, 'utf8');
 
--- content/en/api/latest/ci-visibility-tests/_index.md
+++ content/en/api/latest/ci-visibility-tests/_index.md
@@ -1,3 +1,21 @@
 ---
 title: CI Visibility Tests
 ---
+## Aggregate tests events
+
+The API endpoint to aggregate CI Visibility test events into buckets of computed metrics and timeseries.
+
+## Get a list of tests events
+
+List endpoint returns CI Visibility test events that match a log search query.
+[Results are paginated similarly to logs](https://docs.datadoghq.com/logs/guide/collect-multiple-logs-with-pagination).
+
+Use this endpoint to see your latest test events.
+
+## Search tests events
+
+List endpoint returns CI Visibility test events that match a log search query.
+[Results are paginated similarly to logs](https://docs.datadoghq.com/logs/guide/collect-multiple-logs-with-pagination).
+
+Use this endpoint to build complex events filtering and search.
+
