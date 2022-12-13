4ac9e4ad9489938b6c7eb7a8333dfc146725c256
Brian Deutsch <brian.deutsch@datadoghq.com>
2022-12-13 15:39:29 -0500

    Bump atomic version

--- local/bin/js/algolia-index-sync.js
+++ local/bin/js/algolia-index-sync.js
@@ -117,16 +117,16 @@ const updateReplicas = (client, indexName) => {
 const updateIndex = (indexName) => {
     const localAlogliaSearchIndex = require('../../../public/algolia.json');
 
-    // const cb = (error, result) => {
-    //     if (error) {
-    //         console.error(error);
-    //         throw error;
-    //     }
+    const cb = (error, result) => {
+        if (error) {
+            console.error(error);
+            throw error;
+        }
 
-    //     console.log(result);
-    // };
+        console.log(result);
+    };
 
-    atomicalgolia(indexName, localAlogliaSearchIndex, { verbose: true });
+    atomicalgolia(indexName, localAlogliaSearchIndex, { verbose: true }, cb);
 };
 
 const sync = () => {
