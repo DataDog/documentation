62b2457763821a6e17be6faa2bf2372576006a87
Brian Deutsch <brian.deutsch@datadoghq.com>
2023-01-25 15:29:02 -0500

    Fix merge conflict

--- assets/scripts/components/algolia.js
+++ assets/scripts/components/algolia.js
@@ -50,7 +50,6 @@ if (searchResultsPage) {
     hitComponent = searchpageHits;
 }
 
-<<<<<<< HEAD
 // No searchBoxContainer means no instantSearch
 if (searchBoxContainer) {
     console.log('searchBoxContainer')
@@ -88,80 +87,6 @@ if (searchBoxContainer) {
                     hitsContainerContainer.classList.add('d-none');
                 } else {
                     helper.search();
-=======
-const { algoliaConfig } = getConfig();
-
-let algoliaTimer;
-
-const isApiPage = () => document.body.classList.value.includes('api');
-const alogliaApiDocsearchInput = document.getElementById('search-input-api');
-
-const handleSearchPageRedirect = () => {
-    const docsearchInput = document.querySelector('.docssearch-input.ds-input');
-
-    if (docsearchInput.value !== '') {
-        let inputValue = docsearchInput.value;
-
-        if (inputValue.indexOf('#') > -1) {
-            inputValue = inputValue.replace("#", encodeURIComponent("#"));
-        }
-
-        window.location = `${baseUrl}/search/?s=${inputValue}`;
-    }
-}
-
-const appendHomeLinkToAutocompleteWidget = () => {
-    const headers = document.querySelectorAll('.algolia-autocomplete .algolia-docsearch-suggestion--category-header');
-    const autocompleteHeaderElement = headers[0];
-    const searchPageLink = document.createElement('a');
-
-    searchPageLink.className = "font-regular text-underline ps-2 js-api-search";
-    searchPageLink.innerText = 'Click here to search the full docs';
-    searchPageLink.href = `${baseUrl}/search/`;
-
-    if (alogliaApiDocsearchInput && alogliaApiDocsearchInput.value !== '') {
-        searchPageLink.href += `?s=${alogliaApiDocsearchInput.value}`;
-    }
-
-    if (autocompleteHeaderElement) {
-        autocompleteHeaderElement.appendChild(searchPageLink)
-    }
-}
-
-const enhanceApiResultStyles = () => {
-    const headers = document.querySelectorAll('.algolia-autocomplete .algolia-docsearch-suggestion--category-header');
-
-    headers.forEach(header => {
-        if (header.textContent.toLowerCase().includes('api')) {
-            header.style.fontWeight = '800';
-            header.style.color = '#632ca6';
-        }
-    })
-}
-
-// Transforms API-specific results to show API endpoint as title on both left & right-hand columns of autocomplete widget.
-const setApiEndpointAsSubcategory = () => {
-    const searchResultSubcategoryNodeList = document.querySelectorAll('.algolia-docsearch-suggestion--subcategory-column-text');
-
-    searchResultSubcategoryNodeList.forEach(subcategory => {
-        const parentAnchor = subcategory.closest('a');
-
-        if (parentAnchor) {
-            const categoryHeader = parentAnchor.querySelector('.algolia-autocomplete .algolia-docsearch-suggestion--category-header');
-
-            if (categoryHeader && categoryHeader.textContent.toLowerCase().includes('api')) {
-                const subcategoryContainer = subcategory.closest('.algolia-docsearch-suggestion--subcategory-column');
-
-                if (subcategoryContainer && subcategoryContainer.nextElementSibling) {
-                    const content = subcategoryContainer.nextElementSibling;
-
-                    if (content) {
-                        const apiEndpointTitle = content.querySelector('.algolia-docsearch-suggestion--title');
-                        subcategory.textContent = apiEndpointTitle.textContent;
-                        subcategory.style.fontWeight = '800';
-                        subcategory.style.color = '#000';
-                    }
->>>>>>> master
                 }
             }
         }
