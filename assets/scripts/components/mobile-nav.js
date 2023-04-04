cecfda090cf8afe839f6ae581b7e44336e66c913
Brooklyne Finni <Brooklyne Finni>
2023-04-04 13:28:24 -0400

    removed commented out code

--- assets/scripts/components/mobile-nav.js
+++ assets/scripts/components/mobile-nav.js
@@ -76,16 +76,6 @@ export function closeMobileNav(){
 export function setMobileNav () {
     const dataPath = window.location.pathname.slice(1,-1)
     const mobileSelection = document.querySelector(`#mobile-nav a[data-path="${dataPath}"]`) || false
-    // redirect the AGENT/aggregating agent path to observability_pipelines/integrations/... on mobile nav
-    // if(dataPath.includes('observability_pipelines/production_deployment_overview/integrate_datadog_and_the_observability_pipelines_worker')){
-    //     const observabilityPipelineMobile = document.querySelector('#mobile-nav a[data-path$="observability_pipelines"]');
-    // 
-    //    mobileSelection = observabilityPipelineMobile.nextElementSibling.querySelector(
-    //        'a[data-path*="observability_pipelines/production_deployment_overview/integrate_datadog_and_the_observability_pipelines_worker"]'
-    //     );
-    // }else{
-    //     mobileSelection = document.querySelector(`#mobile-nav a[data-path="${dataPath}"]`) || false
-    // }
     const subMenu = document.querySelector(`#mobile-nav a[data-path="${dataPath}"] + ul.d-none`)
 
     if (mobileSelection) {
