beaa2ad0165a715365c1e717ff8749df723b0565
StefonSimmons <s.j.simmons@spartans.nsu.edu>
2023-05-23 16:24:37 -0400

    adjust on scroll letter highlighting

--- assets/scripts/components/glossary.js
+++ assets/scripts/components/glossary.js
@@ -1,11 +1,13 @@
-let menuSection = document.querySelectorAll('a.nav-link');
+// Controls highlighting glossary letters in the glossary nav
+const menuSection = document.querySelectorAll('a.nav-link');
+const mainSection = document.querySelectorAll('#mainContent h2.glossary-letter');
+const glossaryNavHeight = document.querySelector('#glossary-nav').getBoundingClientRect().height - 25
 
 window.onscroll = (()=> {
-  let mainSection = document.querySelectorAll('#mainContent h2');
-
   mainSection.forEach((v,i)=> {
-    let distanceFromTop = v.getBoundingClientRect().top
-    if(distanceFromTop < 100){
+    const letterDistanceFromTop = v.getBoundingClientRect().top
+    if(letterDistanceFromTop < glossaryNavHeight){
+      // glossary letter header is directly under glossary nav
       menuSection.forEach(v=> v.classList.remove('active'))
       menuSection[i].classList.add('active')
     }
