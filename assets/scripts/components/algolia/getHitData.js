08d7bd3b2f93e44912f77074b8182ec78e1b894f
david.jones <david.jones@datadoghq.com>
2023-06-20 11:25:42 +0100

    improve regex

--- assets/scripts/components/algolia/getHitData.js
+++ assets/scripts/components/algolia/getHitData.js
@@ -2,8 +2,7 @@ export function getHitData(hit, qry = '') {
     const title = hit.title ? hit.title : hit.type;
     const cleanRelPermalink =
         hit.language == 'en' ? hit.relpermalink : hit.relpermalink.replace(`/${hit.language}/`, '');
-    const words = qry.split(' ');
-    const orMatches = words.map((word, index) => (index === words.length - 1) ? `${word}` : `${word}|`).join('');
+    const orMatches = qry.split(' ').filter(word => word).join('|');
     const regexQry = new RegExp(`(${orMatches})`, 'gi');
     const highlightedTitle = (hit._highlightResult.title.value || title).replace(regexQry, '<mark>$1</mark>');
     const highlightedContent = (hit._highlightResult.content.value || '').replace(regexQry, '<mark>$1</mark>');
