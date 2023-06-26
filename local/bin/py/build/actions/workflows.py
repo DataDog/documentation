a1abd29e539f46a801faa042e18dd564c51e71a0
Heston Hoffman <heston.hoffman@datadoghq.com>
2023-06-26 13:09:09 -0700

    (maint) Fix casing in workflow redirects (#18690)
    * (maint) Fix casing in workflow redirects
    
    * lower file name
    
    * disable cache temporarily
    
    * Update local/bin/py/build/configurations/pull_config_preview.yaml
    
    ---------
    
    Co-authored-by: david.jones <david.jones@datadoghq.com>

--- local/bin/py/build/actions/workflows.py
+++ local/bin/py/build/actions/workflows.py
@@ -47,7 +47,7 @@ def workflows(content, content_dir):
                         action_data['bundle'] = data.get('name')
                         action_data['bundle_title'] = data.get('title').strip()
                         action_data['source'] = data.get('icon', {}).get('integration_id', '')
-                        action_data['aliases'] = [f'/workflows/actions_catalog/{output_file_name}_{action_name}']
+                        action_data['aliases'] = [f'/workflows/actions_catalog/{output_file_name}_{action_name}'.lower()]
                         # if this is a dd. bundle then lets use the datadog integration id
                         if not action_data['source'] and 'datadog' in action_data['bundle_title'].lower():
                             action_data['source'] = '_datadog'
@@ -55,7 +55,7 @@ def workflows(content, content_dir):
                         output_content = TEMPLATE.format(front_matter=yaml.dump(action_data, default_flow_style=False).strip(), content=content)
                         dest_dir = Path(f"{content_dir}{dest_path}")
                         dest_dir.mkdir(exist_ok=True)
-                        name = f"{output_file_name}_{action_name}"
+                        name = f"{output_file_name}_{action_name}".lower()
                         dest_file = dest_dir.joinpath(name).with_suffix('.md')
                         with open(dest_file, mode='w', encoding='utf-8') as out_file:
                             out_file.write(output_content)
