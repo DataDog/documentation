d5469a33f85897e9c1b45c8b8b00012a40fe5e95
StefonSimmons <s.j.simmons@spartans.nsu.edu>
2023-06-28 16:56:10 -0400

    use global alpine store for modal

--- assets/scripts/alpine.js
+++ assets/scripts/alpine.js
@@ -1,5 +1,43 @@
 import Alpine from 'alpinejs'
  
 window.Alpine = Alpine
+
+Alpine.store('integrationsModal', {
+    isOpen: false,
+    activeSlide: null,
+    openModal(activeSlide) {
+        this.isOpen = true
+        this.activeSlide = activeSlide
+
+    },
+    closeModal() {
+        this.isOpen = false
+    },
+    playVideo (refs) {
+        // Plays active video
+        const {iframe, playBtn, playOverlay} = refs
+        const player = new Vimeo.Player(iframe);
+
+        playBtn.classList.add('d-none');
+        playOverlay.classList.add('d-none');
+        iframe.classList.remove('d-none');
+
+        player.play()
+    },
+    pauseVideo (refs) {
+        // Pauses active video
+        const {iframe, playBtn, playOverlay} = refs
+        if(iframe?.parentElement.classList.contains('active')){
+            const player = new Vimeo.Player(iframe);
+            player.pause().then(() => {
+                playBtn.classList.remove('d-none');
+                playOverlay.classList.remove('d-none');
+                iframe.classList.add('d-none');
+            }).catch((err) => {
+                console.log(err)
+            })
+        }
+    }
+})
  
 Alpine.start()
