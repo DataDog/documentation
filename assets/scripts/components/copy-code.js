0e5cda305f2d54fbd909770e495846db34d53742
StefonSimmons <s.j.simmons@spartans.nsu.edu>
2023-08-09 16:11:59 -0400

    comment and change class name

--- assets/scripts/components/copy-code.js
+++ assets/scripts/components/copy-code.js
@@ -1,28 +1,8 @@
-// Script to add copy-clipboard functionality to copy buttons within code-block shortcodes, fenced codeblocks and highlight hugo functions
+// Script to add copy-clipboard functionality to copy buttons
+// Script to add copy buttons to markdown fenced (```), and {{ highlight }} hugo function code blocks
 
 function initCopyCode () {
-    // Add copy button to certain codeblocks of specific languages
-    const fencedLang = ['shell']
-    const pulledLang = ['java', 'go', 'python', 'ruby', 'typescript', 'java'] // using the highlight hugo function
-    const highlights = document.querySelectorAll("div.highlight")
-
-    highlights.forEach(highlightEl => {
-        const codeLang = highlightEl.querySelector('[data-lang]').dataset.lang
-        const isNotInCodeBlockShortcode = !highlightEl.parentElement.classList.contains('code-snippet')
-
-        const shouldAddCopyBtn = [...fencedLang, ...pulledLang].includes(codeLang) && isNotInCodeBlockShortcode
-        if(shouldAddCopyBtn){
-            highlightEl.classList.add('code-snippet', 'fenced')
-            const copyBtn = `
-            <div class="code-button-wrapper position-absolute">
-                <button class="btn text-primary js-copy-button">Copy</button>
-            </div>
-            `
-            highlightEl.insertAdjacentHTML('beforeend', copyBtn)
-        }
-    })
-
-
+    addCopyButton()
 
     // Add Event Listener
     const copyButtons = document.querySelectorAll('.js-copy-button');
@@ -33,28 +13,51 @@ function initCopyCode () {
         });
     }
 
+}
 
 
-    // EVENT FUNCTION
-    function copyCode (event, btn){
-        const codeSnippetElement = event.target
-        .closest('.code-snippet')
-        .querySelector('.chroma');
-        
-        // Create a range object
-        const range = document.createRange();
-        // Select the node
-        range.selectNode(codeSnippetElement);
-        // create system clipboard object
-        const Clipboard = navigator.clipboard
-        // write code snippet text
-        Clipboard.writeText(codeSnippetElement.innerText).then(() => {
-            btn.textContent = "Copied!";
-            setTimeout(function() {
-                btn.textContent = "Copy"
-            }, 1000)
-        })
-    }
+// Adds copy button to code examples that use the highlight hugo function or are fenced code blocks in markdown
+function addCopyButton () {
+    const fencedLang = ['shell'] // target specific fenced codeblocks by language
+    const pulledLang = ['java', 'go', 'python', 'ruby', 'typescript', 'json', 'yaml'] // target specific highlight hugo functions by language 
+    const highlights = document.querySelectorAll("div.highlight")
+
+    highlights.forEach(highlightEl => {
+        const codeLang = highlightEl.querySelector('[data-lang]').dataset.lang
+        const isNotInCodeBlockShortcode = !highlightEl.parentElement.classList.contains('code-snippet')
+
+        const shouldAddCopyBtn = [...fencedLang, ...pulledLang].includes(codeLang) && isNotInCodeBlockShortcode
+        if(shouldAddCopyBtn){
+            highlightEl.classList.add('code-snippet', 'appended-copy-btn')
+            const copyBtn = `
+            <div class="code-button-wrapper position-absolute">
+                <button class="btn text-primary js-copy-button">Copy</button>
+            </div>
+            `
+            highlightEl.insertAdjacentHTML('beforeend', copyBtn)
+        }
+    })
+}
+
+// EVENT FUNCTION for copy functionality
+function copyCode (event, btn){
+    const codeSnippetElement = event.target
+    .closest('.code-snippet')
+    .querySelector('.chroma');
+    
+    // Create a range object
+    const range = document.createRange();
+    // Select the node
+    range.selectNode(codeSnippetElement);
+    // create system clipboard object
+    const Clipboard = navigator.clipboard
+    // write code snippet text
+    Clipboard.writeText(codeSnippetElement.innerText).then(() => {
+        btn.textContent = "Copied!";
+        setTimeout(function() {
+            btn.textContent = "Copy"
+        }, 1000)
+    })
 }
 
 initCopyCode()
