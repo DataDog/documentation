db9a6e04c3683733a588963b9a6698e722cebf32
StefonSimmons <s.j.simmons@spartans.nsu.edu>
2023-08-30 14:34:34 -0400

    reduce iterations on roles data

--- local/bin/py/build/pull_rbac.py
+++ local/bin/py/build/pull_rbac.py
@@ -41,22 +41,32 @@ def pull_rbac():
 
     if permissions_json and roles_json:
         permissions_data = permissions_json.get('data', [])
-        roles_data = roles_json.get('data', [])
+        # gets default roles (admin, standard, and read-only)
+        # ignores UI template roles ('Datadog Billing Admin Role' and 'Datadog Security Admin Role')
+        default_roles_data = roles_json.get('data', [])[0:3] 
+        dr_data_len = len(default_roles_data)
 
         for permission in permissions_data:
             group_name = permission['attributes']['group_name']
             permission_name = permission['attributes']['name']
             permission_role_name = ''
 
-            for role in roles_data:
+            for x in range(dr_data_len):
+                if permission_role_name:
+                    break  # reduce number of iterations of looping actions
+                role = default_roles_data[(dr_data_len-1)-x] # get role starting from the 'read-only' -> ''
                 role_name = role['attributes']['name']
                 role_permissions = role['relationships']['permissions']['data']
-                # Ignore UI template roles
-                if role_name != 'Datadog Billing Admin Role' and role_name != 'Datadog Security Admin Role':
-                    # Compare role id to permission id
-                    for role_permission in role_permissions:
-                        if role_permission['id'] == permission['id']:
-                            permission_role_name = role_name.replace('Read Only', 'Read-Only') # assigning permission_role_name relies on the order of admin, standard, and read-only role objects in the `roles/templates` api.
+                # # Ignore UI template roles
+                # if role_name != 'Datadog Billing Admin Role' and role_name != 'Datadog Security Admin Role':
+                for role_permission in role_permissions:
+                    # Match permission id to a role. assign permission_role_name
+                    # assign permission_role_name - relies on order of admin, standard, and read-only role objects in the `roles/templates` api.
+                    if role_permission['id'] == permission['id']:
+                        permission_role_name = role_name.replace('Read Only', 'Read-Only')
+                        if permission_role_name:
+                            break
+        
             # Ignore legacy logs permissions from dictionary before converting to JSON.  These legacy permissions are hard-coded in rbac-permissions-table partial until they can be deprecated.
             if permission_name in ('logs_live_tail', 'logs_read_index_data'):
                 continue
