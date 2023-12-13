a111a1d89303d3895a6af28055fc4e63677559b9
David Weid II <david.weid@datadoghq.com>
2023-12-13 11:51:06 -0500

    Remove jQuery from Documentation Site - Round 1 (#20825)
    * Remove jQuery from "table-of-contents.js"
    
    * reorg table-of-contents functions
    
    create helpers
    
    * Remove jQ from datadog-docs onload function
    
    * Remove jQ from datadog-docs.js + format file
    
    * replace "js-expand-all" with JS
    
    test on api retention filters page
    
    * Update api.js
    
    * remove jQuery from api.js
    
    * Update navbar.js
    
    * remove jQuery from navbar.js and helpers.js
    
    * start removal of jQ from global-modals.js

--- assets/scripts/components/global-modals.js
+++ assets/scripts/components/global-modals.js
@@ -1,17 +1,16 @@
-$(document).ready(function () {
+import { DOMReady } from '../helpers/documentReady';
 
-    const signupModal = document.getElementById('signupModal')
-    signupModal.addEventListener('show.bs.modal', function(e) {
-        //$('body').css('overflow', 'hidden');
-        //$('.modal').css('overflow', 'scroll');
+const doOnLoad = () => {
+    const signupModal = document.getElementById('signupModal');
+    signupModal.addEventListener('show.bs.modal', () => {
         var regURL = 'https://app.datadoghq.com/signup_corp';
         var mobileURL = 'https://app.datadoghq.com/signup_corp?mobile=true';
         var lang_param = '';
         var lang = '';
 
-        if(document.documentElement.lang){
+        if (document.documentElement.lang) {
             lang = document.documentElement.lang;
-        } else{
+        } else {
             lang = ddc.lang;
         }
 
@@ -20,8 +19,8 @@ $(document).ready(function () {
         } else {
             lang_param = '';
         }
-        // Detect Mobile Devices
 
+        // Detect Mobile Devices
         var isMobile = {
             Android: function () {
                 return navigator.userAgent.match(/Android/i);
@@ -39,60 +38,69 @@ $(document).ready(function () {
                 return navigator.userAgent.match(/IEMobile/i);
             },
             any: function () {
-                return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+                return (
+                    isMobile.Android() ||
+                    isMobile.BlackBerry() ||
+                    isMobile.iOS() ||
+                    isMobile.Opera() ||
+                    isMobile.Windows()
+                );
             }
         };
+
         // Trigger conditional URL
-
         if (isMobile.any()) {
-            $('#signUpIframe').attr('src', mobileURL);
+            document.querySelector('#signUpIframe').setAttribute('src', mobileURL);
         } else {
-            $('#signUpIframe').attr('src', regURL+lang_param);
+            document.querySelector('#signUpIframe').setAttribute('src', regURL + lang_param);
         }
-    })
-
-    signupModal.addEventListener('hide.bs.modal', function(e) {
-        $('#signUpIframe').attr('src', '');
-        //$('body').css('overflow', '');
-        //$('.modal').css('overflow', '');
     });
 
+    signupModal.addEventListener('hide.bs.modal', function () {
+        document.querySelector('#signUpIframe').setAttribute('src', '');
+    });
 
-    const imageModal = document.getElementById('popupImageModal')
+    const imageModal = document.getElementById('popupImageModal');
     var naturalWidth = 0;
     var naturalHeight = 0;
-    var isShowing = false;
-    imageModal.addEventListener('show.bs.modal', function(e) {
-      $('#popupImageModal').on('show.bs.modal', function (e) {
-        $('#popupImageModal .modal-content').css({'background': 'transparent', 'border': 'none'});
-        $('#popupImageModal .modal-body, #popupImageModal .modal-dialog, #popupImageModal .modal-content').css('height', '100%');
-        $('#popupImageModal .modal-body').html('<div class="loader" style="position:absolute;top:50%;margin:-50px 0 0 -50px;height:100px;width:100px;"></div>');
-      })
+
+    imageModal.addEventListener('show.bs.modal', function () {
+        $('#popupImageModal').on('show.bs.modal', function () {
+            $('#popupImageModal .modal-content').css({ background: 'transparent', border: 'none' });
+            $('#popupImageModal .modal-body, #popupImageModal .modal-dialog, #popupImageModal .modal-content').css(
+                'height',
+                '100%'
+            );
+            $('#popupImageModal .modal-body').html(
+                '<div class="loader" style="position:absolute;top:50%;margin:-50px 0 0 -50px;height:100px;width:100px;"></div>'
+            );
+        });
     });
-    imageModal.addEventListener('shown.bs.modal', function(e) {
-        $('body').removeClass('modal-open');
-        var modal = $(this);
-        //modal.hide();
+
+    imageModal.addEventListener('shown.bs.modal', function (e) {
+        document.querySelector('body').classList.remove('modal-open');
+        var modal = imageModal;
         var url = e.relatedTarget.href;
+
         // try set modal popup imgix to cap out at browser width/height
-        url += '&w='+$(window).width() + '&h='+$(window).height();
+        url += '&w=' + $(window).width() + '&h=' + $(window).height();
         var img = new Image();
-        var srcseturl = url+", "+url+"&dpr=2 2x";
-        var imgEl = $('<img src="'+url+'" srcset="'+srcseturl+'" alt="" class="img-fluid" />');
-        img.onload  = function() {
+        var srcseturl = url + ', ' + url + '&dpr=2 2x';
+        var imgEl = $('<img src="' + url + '" srcset="' + srcseturl + '" alt="" class="img-fluid" />');
+        img.onload = function () {
             /* Store naturalWidth & height for IE8 */
             naturalWidth = img.width;
             naturalHeight = img.height;
-            $('#popupImageModal .modal-content').css({'background': '', 'border': ''});
+            $('#popupImageModal .modal-content').css({ background: '', border: '' });
             $('#popupImageModal .modal-body').html(imgEl);
             resize(naturalWidth, naturalHeight);
-            if($('#popupImageModal').is(':visible')) {
+            if ($('#popupImageModal').is(':visible')) {
                 modal.fadeIn();
             }
         };
         img.src = url;
     });
-    imageModal.addEventListener('hide.bs.modal', function(e) {
+    imageModal.addEventListener('hide.bs.modal', function (e) {
         $('#popupImageModal .modal-body').empty();
     });
 
@@ -107,36 +115,30 @@ $(document).ready(function () {
             p.css('width', '').css('height', '');
             /* Calculate the worst ratio so that dimensions fit */
             /* Note: -1 to avoid rounding errors */
-            var ratio = Math.max(
-                w  / (parentW-1),
-                h / (parentH-1));
+            var ratio = Math.max(w / (parentW - 1), h / (parentH - 1));
             /* Resize content */
             if (ratio > 1) {
                 ratio = h / Math.floor(h / ratio); /* Round ratio down so height calc works */
                 el.css('width', '' + w / ratio + 'px').css('height', '' + h / ratio + 'px');
                 p.css('width', '' + w / ratio + 'px').css('height', '' + h / ratio + 'px');
-                //el.animate({width: w/ratio+'px', height: h/ratio+'px'}, 200);
-                //p.animate({width: w/ratio+'px', height: h/ratio+'px'}, 200);
             } else {
-                el.css('width', '' + w  + 'px').css('height', '' + h + 'px');
+                el.css('width', '' + w + 'px').css('height', '' + h + 'px');
                 p.css('width', '' + w + 'px').css('height', '' + h + 'px');
-                //el.animate({width: w+'px', height: h+'px'}, 200);
-                //p.animate({width: w+'px', height: h+'px'}, 200);
             }
-
         }
     }
 
-    $(window).on('resize', function() {
-        if($('#popupImageModal').is(':visible')) {
+    window.addEventListener('resize', () => {
+        if ($('#popupImageModal').is(':visible')) {
             resize(naturalWidth, naturalHeight);
         }
     });
 
-    $(window).on('scroll', function() {
-        if($('#popupImageModal').is(':visible')) {
+    window.addEventListener('scroll', () => {
+        if ($('#popupImageModal').is(':visible')) {
             $('#popupImageModal').modal('hide');
         }
     });
+};
 
-});
+DOMReady(doOnLoad);
