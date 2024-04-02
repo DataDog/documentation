eb371ce929d3c2c559a52e9935aaa2a227135b9a
Nick Sollecito <nsollecito@users.noreply.github.com>
2024-04-01 20:17:04 -0400

    Revert "[WEB-4688] fixes instant search console warnings (#22424)" (#22445)
    This reverts commit 842ca26ec1c9b3026eb553b362ec0894371dcf77.

--- assets/scripts/components/algolia.js
+++ assets/scripts/components/algolia.js
@@ -2,7 +2,6 @@ import { getConfig } from '../helpers/getConfig';
 import algoliasearch from 'algoliasearch/lite';
 import instantsearch from 'instantsearch.js';
 import { configure, searchBox } from 'instantsearch.js/es/widgets';
-import { history } from 'instantsearch.js/es/lib/routers';
 import { searchbarHits } from './algolia/searchbarHits';
 import { searchpageHits } from './algolia/searchpageHits';
 import { customPagination } from './algolia/customPagination';
@@ -101,15 +100,8 @@ function loadInstantSearch(currentPageWasAsyncLoaded) {
         const search = instantsearch({
             indexName,
             searchClient,
-            future: {
-                preserveSharedStateOnUnmount: true
-            },
-
             // routing handles search state URL sync
             routing: {
-                router: history({
-                    cleanUrlOnDispose: false
-                }),
                 stateMapping: {
                     stateToRoute(uiState) {
                         const indexUiState = uiState[indexName];
@@ -123,18 +115,13 @@ function loadInstantSearch(currentPageWasAsyncLoaded) {
                                 query: routeState.s
                             }
                         };
-                    },
+                    }
                 }
             },
-
-            onStateChange({ uiState, setUiState }) {
-                const query = uiState[indexName].query
-
-                setUiState({
-                    [indexName]: { query }
-                })
-                                
-                if (query) {
+            // Handle hitting algolia API based on query and page
+            searchFunction(helper) {
+                if (helper.state.query) {
+                    helper.search();
                     if (!searchResultsPage) {
                         searchBoxContainerContainer.classList.add('active-search');
                         hitsContainerContainer.classList.remove('d-none');
@@ -143,6 +130,8 @@ function loadInstantSearch(currentPageWasAsyncLoaded) {
                     if (!searchResultsPage) {
                         searchBoxContainerContainer.classList.remove('active-search');
                         hitsContainerContainer.classList.add('d-none');
+                    } else {
+                        helper.search();
                     }
                 }
             }
