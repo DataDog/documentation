2cf502338326a7b6bf9052250f0e8755d4a0c200
Devin Ford <devin.ford@datadoghq.com>
2024-04-23 15:25:44 -0400

    feat: update local translation script, update image pointer

--- local/bin/py/placehold_translations.py
+++ local/bin/py/placehold_translations.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
-from glob import glob
 import ntpath
-from optparse import OptionParser
 import os
 import re
-import logging
-import sys
+import argparse
+from glob import glob
+from shutil import copyfile
 from functools import partial
 from multiprocessing import cpu_count
 from multiprocessing.pool import Pool
+import logging
+
 import yaml
 
 DEFAULT_LANGUAGE = "en"
@@ -20,17 +21,14 @@ TEMPLATE = """\
 {content}
 """
 
-logger = logging.getLogger(__name__)
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-
 
 def get_disclaimer_from_params(config_location, key):
     try:
-        with open(os.path.dirname(config_location) + f'/params.{key}.yaml') as f:
+        with open('{0}/params.{1}.yaml'.format(os.path.dirname(config_location), key)) as f:
             c_yaml = yaml.safe_load(f.read())
             return c_yaml.get('disclaimer', '') or ''
     except:
-        print("\x1b[31mERROR\x1b[0m: Error getting disclaimer")
+        logging.error("Error getting disclaimer")
 
 
 def get_languages(config_location):
@@ -41,8 +39,9 @@ def get_languages(config_location):
         if 'en' in c_yaml:
             # this is languages.yaml
             d = c_yaml
-            for key, data in d.items():
-              data['disclaimer'] = get_disclaimer_from_params(config_location, key)
+            # uncomment this when we move disclaimer to params.yaml files like docs
+            # for key, data in d.items():
+            #    data['disclaimer'] = get_disclaimer_from_params(config_location, key)
         else:
             # this is config.yaml
             for l in c_yaml["languages"]:
@@ -53,9 +52,8 @@ def get_languages(config_location):
 def create_glob(files_location, lang, disclaimer="", lang_as_dir=False, ignores=None):
     if ignores is None:
         ignores = []
-    print(f'Ignores are: {ignores}')
-    all_files = [f for f in glob(files_location + '**/*.md', recursive=True) if
-                 all([not os.path.dirname(f).startswith(ignore) for ignore in ignores])]
+    logging.info(f'Ignores are: {ignores}')
+    all_files = [f for f in glob(files_location + '**/*.md', recursive=True) if all([not os.path.dirname(f).startswith(ignore) for ignore in ignores])]
     if lang == 'en':
         g = [f for f in all_files if len(f.split('.')) == 2]
     else:
@@ -66,27 +64,36 @@ def create_glob(files_location, lang, disclaimer="", lang_as_dir=False, ignores=
     return {"name": lang, "glob": g, "disclaimer": disclaimer}
 
 
+def create_data_glob(data_location, lang, disclaimer="", lang_as_dir=False, ignores=None):
+    if ignores is None:
+        ignores = []
+    location = data_location if data_location.endswith(os.path.sep) else '{}{}'.format(data_location, os.path.sep)
+    location += lang + os.path.sep
+    all_files = [f for f in glob(location + '**/*.yaml', recursive=True) if all([not os.path.dirname(f).startswith(ignore) for ignore in ignores])]
+    if lang == 'en':
+        g = [f for f in all_files if len(f.split('.')) == 2]
+    else:
+        if lang_as_dir:
+            g = [f for f in all_files if '.yaml'.format(lang) in f]
+        else:
+            g = [f for f in all_files if '.{0}.yaml'.format(lang) in f]
+    return {"name": lang, "glob": g, "disclaimer": disclaimer}
+
+
 def diff_globs(base, compare, lang_as_dir=False):
     if lang_as_dir:
-        return [f for f in base['glob'] if f.replace('/%s/' % base['name'], '/%s/' % compare['name']) not in compare['glob']]
+        return [f for f in base['glob'] if
+                f.replace('/%s/' % base['name'], '/%s/' % compare['name']) not in compare['glob']]
     else:
         return [f for f in base['glob'] if f.replace('.md', '.%s.md' % compare['name']) not in compare['glob']]
 
 
-def md_update_links(this_lang_code, content):
-    """ Update footer links in markdown to be language relative """
-    result = content
-    try:
-        common_lang_codes = ["en/", "es/", "de/", "fr/", "es/", "ja/", "ko/", "resources/"]
-        exclude_common_langs = "|".join(list(map(lambda code: f"{code}",common_lang_codes)))
-        relative_regex = re.compile("^(\\[[0-9]+]\:\\s*)(\/(?!" + exclude_common_langs + ").*)$", re.MULTILINE | re.IGNORECASE)
-        substitute = "\g<1>/" + this_lang_code.lower() + "\g<2>"
-        result = relative_regex.sub(substitute, content)
-    except Exception as e:
-        result = content
-        logger.exception("fail to update md links")
-    finally:
-        return result
+def diff_data_globs(base, compare, lang_as_dir=False):
+    if lang_as_dir:
+        return [f for f in base['glob'] if
+                f.replace('/%s/' % base['name'], '/%s/' % compare['name']) not in compare['glob']]
+    else:
+        return [f for f in base['glob'] if f.replace('.yaml', '.%s.yaml' % compare['name']) not in compare['glob']]
 
 
 def create_placeholder_file(template, new_glob, lang_as_dir, files_location):
@@ -97,85 +104,158 @@ def create_placeholder_file(template, new_glob, lang_as_dir, files_location):
         sub_path = sub_path if not sub_path or sub_path.endswith('/') else sub_path + '/'
         # remove leading slash if we have one
         sub_path = sub_path[1:] if sub_path.startswith('/') else sub_path
-        new_dest = "{content_path}{sub_path}{template}".format(content_path=content_path, sub_path=sub_path, template=ntpath.basename(template))
+        new_dest = "{content_path}{sub_path}{template}".format(content_path=content_path, sub_path=sub_path,
+                                                               template=ntpath.basename(template))
     else:
-        new_dest = os.path.dirname(template) + '/' + ntpath.basename(template).replace('.md', '.%s.md' % new_glob['name'])
+        new_dest = os.path.dirname(template) + '/' + ntpath.basename(template).replace('.md',
+                                                                                       '.%s.md' % new_glob['name'])
 
     with open(template) as o_file:
         content = o_file.read()
-        boundary = re.compile(r'^-{3,}$', re.MULTILINE)
+        boundary = re.compile(r'^-{3,}\s*$', re.MULTILINE)
+        # U+FEFF is the Byte Order Mark character, which should only occur at the start of a document.
+        content = content.replace(u'\ufeff', '').strip()
         split = boundary.split(content, 2)
         new_yml = {}
-        if len(split) == 3:
+        split_len = len(split)
+        if split_len == 3:
             _, fm, content = split
-            new_yml = yaml.safe_load(fm)
-        elif len(split) == 1:
+            try:
+                new_yml = yaml.safe_load(fm)
+            except yaml.scanner.ScannerError as e:
+                new_yml = {}
+                logging.error("Something went wrong while parsing yaml in {}".format(template))
+                logging.error(e)
+        elif split_len == 1:
             content = split[0]
             new_yml = {}
+        elif split_len == 2:
+            fm, content = split
+            try:
+                new_yml = yaml.safe_load(fm)
+            except yaml.scanner.ScannerError as e:
+                new_yml = {}
+                logging.error("Something went wrong while parsing yaml in {}".format(template))
+                logging.error(e)
         new_content = content
+        if not new_yml:
+            new_yml = {}
+            logging.warning("new_yml was none for {}".format(template))
         if new_yml.get('aliases', None):
             new_aliases = []
             for alias in new_yml.get('aliases'):
-                # if alias is relative e.g no leading slash we need to resolve its abs path to be able to prepend /lang/
-                # e.g alias 13a-810-14c in security_monitor/default_rules/file.md
-                # becomes /ja/security_monitor/default_rules/13a-810-14c/
-                if not alias.startswith("/"):
-                    alias = f"/{sub_path}{alias}"
                 new_aliases.append('/{0}{1}'.format(new_glob['name'], alias))
             new_yml['aliases'] = new_aliases
+        if new_yml.get('url', None):
+            new_yml['url'] = '/{0}/{1}'.format(new_glob['name'], new_yml['url'].strip("/"))
         if new_glob["disclaimer"]:
-            disclaimer = "<div class='alert alert-info'>%s</div>\n\n" % new_glob["disclaimer"]
-            new_content = disclaimer + content
+            # disclaimer = "<div class='alert alert-info'>%s</div>\n\n" % new_glob["disclaimer"]
+            new_content = content
             new_yml['placeholder'] = True
-
-        new_content = md_update_links(new_glob['name'], new_content.strip())
-
+            new_yml['noindex'] = True
+            new_yml['disclaimer'] = "%s" % new_glob["disclaimer"]
         content = TEMPLATE.format(front_matter=yaml.dump(new_yml, default_flow_style=False).strip(),
                                   content=new_content.strip())
 
     os.makedirs(os.path.dirname(new_dest), exist_ok=True)
     if os.path.exists(new_dest):
-        logger.info("overwriting {}".format(new_dest))
+        logging.info("overwriting {}".format(new_dest))
     with open(new_dest, 'w') as o_file:
-            o_file.write(content)
+        o_file.write(content)
+        logging.info("creating placeholder for {0} at {1}".format(template, new_dest))
 
     return new_dest
 
 
+def create_data_placeholder_file(template, new_glob, lang_as_dir, files_location):
+    src = template
+    dst = src.replace('/en/', '/{}/'.format(new_glob['name']))
+    os.makedirs(os.path.dirname(dst), exist_ok=True)
+    if not os.path.exists(dst):
+        logging.info("creating data placeholder for {0} at {1}".format(src, dst))
+        copyfile(src, dst)
+    else:
+        logging.info('data file {0} exists skipping'.format(dst))
+
+
 def main():
-    parser = OptionParser(usage="usage: %prog [options] create placeholder pages for multi-language")
-    parser.add_option("-c", "--config_location", help="location of site config")
-    parser.add_option("-f", "--files_location", help="location of site content files", default="")
-    parser.add_option("-d", "--lang_as_dir", help="use dir lang instead of suffix", default=True)
-    parser.add_option("-i", "--ignore", help="paths to ignore", default=["content/en/meta"])
+    print("Starting placeholder creation for translations")
+    
+    # Set up logger
+    root_dir = os.path.abspath(os.sep)
+    log_dir = os.path.join(root_dir, 'logs')
+    os.makedirs(log_dir, exist_ok=True)
+    log_file = os.path.join(log_dir, 'translations-logs.txt')
+    # print path for CI
+    log_file = os.path.join(log_dir, 'translations-logs.txt')
+    print("Log file path:", log_file)
+
+    logging.basicConfig(
+        level=logging.WARNING,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler(log_file),
+            logging.StreamHandler()
+        ]
+    )
+    
+    parser = argparse.ArgumentParser(usage="usage: %prog [options] create placeholder pages for multi-language")
+    parser.add_argument("-c", "--config_location", help="location of site config")
+    parser.add_argument("-f", "--files_location", help="location of site content files", default="")
+    parser.add_argument("-d", "--data_location", help="location of site data files", default="")
+    parser.add_argument("-l", "--lang_as_dir", help="use dir lang instead of suffix", default=True)
+    parser.add_argument("-i", "--ignore", help="paths to ignore", type=str, nargs="+", default=[])
 
     pool_size = cpu_count()
 
-    (options, args) = parser.parse_args()
-    options = vars(options)
+    args = parser.parse_args()
+    options = vars(args)
 
     lang = get_languages(options["config_location"])
-    default_glob = create_glob(options["files_location"] or "content/en/", DEFAULT_LANGUAGE, lang_as_dir=options["lang_as_dir"], ignores=options["ignore"])
+    default_glob = create_glob(options["files_location"] or "content/en/", DEFAULT_LANGUAGE,
+                               lang_as_dir=options["lang_as_dir"], ignores=options["ignore"])
+    default_data_glob = create_data_glob(options["data_location"] or "data/", DEFAULT_LANGUAGE,
+                                         lang_as_dir=options["lang_as_dir"], ignores=options["ignore"])
+    default_content_resources_glob = create_data_glob("content/", DEFAULT_LANGUAGE,
+                               lang_as_dir=options["lang_as_dir"], ignores=options["ignore"])
     del lang[DEFAULT_LANGUAGE]
     for l in lang:
-        info = lang[l]
-        if options["files_location"]:
-            files_location = options["files_location"]
+        info = lang[l].get("params")
+        if info.get("fullTranslation", False) or False:
+            if options["files_location"]:
+                files_location = options["files_location"]
+            else:
+                files_location = info.get('contentDir', 'content/{}/'.format(l))
+                files_location = files_location if files_location.endswith('/') else files_location + '/'
+            # markdown
+            # lang_glob = create_glob(files_location=options["files_location"], lang=l, disclaimer=info.get("disclaimer", ""))
+            lang_glob = create_glob(files_location=files_location, lang=l, disclaimer=info["disclaimer"],
+                                    lang_as_dir=options["lang_as_dir"])
+            diff = diff_globs(base=default_glob, compare=lang_glob, lang_as_dir=options["lang_as_dir"])
+            logging.info("building {0} placeholder pages for {1} ".format(len(diff), l))
+            with Pool(processes=pool_size) as pool:
+                # call the function for each item in parallel
+                pool.map(partial(create_placeholder_file, new_glob=lang_glob, lang_as_dir=options["lang_as_dir"],
+                                        files_location=files_location), diff)
+
+            # content page resources e.g content/en/product/apm/pricing.yaml
+            lang_content_resources_glob = create_data_glob(data_location=files_location, lang=l, disclaimer=info.get("disclaimer", ""))
+            content_resources_diff = diff_data_globs(base=default_content_resources_glob, compare=lang_content_resources_glob, lang_as_dir=options["lang_as_dir"])
+            logging.info("building {0} placeholder page resource files for {1} ".format(len(content_resources_diff), l))
+            with Pool(processes=pool_size) as pool:
+                pool.map(partial(create_data_placeholder_file, new_glob=lang_content_resources_glob, lang_as_dir=options["lang_as_dir"], files_location=files_location), content_resources_diff)
+
+            # data
+            lang_data_glob = create_data_glob(data_location=options["data_location"], lang=l,
+                                              disclaimer=info.get("disclaimer", ""))
+            data_diff = diff_data_globs(base=default_data_glob, compare=lang_data_glob, lang_as_dir=options["lang_as_dir"])
+            logging.info("building {0} placeholder data files for {1} ".format(len(data_diff), l))
+            with Pool(processes=pool_size) as pool:
+                pool.map(partial(create_data_placeholder_file, new_glob=lang_data_glob, lang_as_dir=options["lang_as_dir"], files_location=files_location), data_diff)
         else:
-            files_location = info.get('contentDir', 'content/{lang_code}/'.format(lang_code=l))
-            files_location = files_location if files_location.endswith('/') else files_location + '/'
-        lang_glob = create_glob(files_location=files_location, lang=l, disclaimer=info["disclaimer"], lang_as_dir=options["lang_as_dir"], ignores=options["ignore"])
-        diff = diff_globs(base=default_glob, compare=lang_glob, lang_as_dir=options["lang_as_dir"])
-        print("\x1b[32mINFO\x1b[0m: building {0} placeholder pages for {1} ".format(len(diff), l))
-        with Pool(processes=pool_size) as pool:
-            # call the function for each item in parallel
-            pool.map(partial(create_placeholder_file, new_glob=lang_glob, lang_as_dir=options["lang_as_dir"], files_location=files_location), diff)
+            logging.info("skipping {0} as fullTranslation set to false".format(l))
+    print('Placeholder creation complete')
 
 
 if __name__ == "__main__":
-    # if in gitlab we need to exit with failure
-    # if in local we skip building the placeholders
-    if os.getenv("CI_COMMIT_REF_NAME"):
-        main()
-    else:
-        print('\x1b[32mINFO\x1b[0m: Skipping placeholders locally')
+    main()
