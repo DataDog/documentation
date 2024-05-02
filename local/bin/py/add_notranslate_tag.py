54c9d7d2fbcff2937a75981f00df6d763f9f67de
Colin Cole <colin.cole@datadoghq.com>
2024-05-02 16:55:54 -0400

    add notranslate to job

--- local/bin/py/add_notranslate_tag.py
+++ local/bin/py/add_notranslate_tag.py
@@ -1,8 +1,20 @@
 import json
 import requests
+import os
+
 api_url = 'https://rest.api.transifex.com'
-bearer_token = '' # DO NOT COMMIT
-string_collection_id = 'o:datadog:p:websites-modules:r:config__default_menus_menus_en_yaml' # Hardcoded collection id pointing to menus.en.yamml
+string_collection_ids = [
+    'o:datadog:p:documentation_loc:r:config__default_menus_main_en_yaml',
+    'o:datadog:p:documentation_loc:r:config__default_menus_api_en_yaml'
+]
+
+def get_bearer_authorization_header():
+    """
+    Return authorization header containing Transifex API key
+    to be used when making network calls to Transifex
+    """
+    api_key = os.environ['transifex_api_key']
+    return {'Authorization': f'Bearer {api_key}'}
 
 # Build payload for updating resource string
 def build_payload(string_id):
@@ -20,13 +32,11 @@ def build_payload(string_id):
     return json.dumps(payload)
 
 # Update resource string
-def update_resource_string(string_id, token):
+def update_resource_string(string_id):
     try:
         url = f"{api_url}/resource_strings/{string_id}"
-        headers = {
-            "Content-Type": "application/vnd.api+json",
-            "Authorization": f"Bearer {token}"
-        }
+        headers = get_bearer_authorization_header()
+        headers['Content-Type'] = 'application/vnd.api+json'
         payload = build_payload(string_id)
         response = requests.patch(url, headers=headers, data=payload)
         return response
@@ -35,12 +45,9 @@ def update_resource_string(string_id, token):
         return None
 
 # Fetch string collection
-def get_string_collection(string_collection_id, token):
+def get_string_collection(url):
     try:
-        url = f"{api_url}/resource_strings?filter[resource]={string_collection_id}"
-        headers = {
-            "Authorization": f"Bearer {token}"
-        }
+        headers = get_bearer_authorization_header()
         response = requests.get(url, headers=headers)
         return response.json()
     except requests.exceptions.RequestException as e:
@@ -52,25 +59,27 @@ def get_string_collection(string_collection_id, token):
 def iterate_collection(collection):
     for entry in collection['data']:
         string_id = entry['id']
-        key_parts = entry['attributes']['key'].split('.') # assumption that key value will be formatted like "main_left.[1].url"
+        key_parts = entry['attributes']['key'].split('.') # assumption that the last value in array will be the key name
         key_val = key_parts[len(key_parts) - 1]
         if key_val != 'name':
-            resp = update_resource_string(string_id, bearer_token)
+            resp = update_resource_string(string_id)
             if resp is not None:
                 print('Successfully updated resource string ' + string_id)
             else:
                 print('Failed to update resource string' + string_id)
 
 # Get string collection and store as JSON object
-def main():
-    data = get_string_collection(string_collection_id, bearer_token)
-    if data is not None:
-        iterate_collection(data)
-    else:
-        print('Failed to fetch string collection')
-
-main()
-
-
-
-
+def add_tag():
+    for collection_id in string_collection_ids:
+        next_page_url = f"{api_url}/resource_strings?filter[resource]={collection_id}"
+        
+        while next_page_url:
+            data = get_string_collection(next_page_url)
+            if data is not None:
+                iterate_collection(data)
+            else:
+                print('Failed to fetch string collection ' + collection_id)
+            
+            next_page_url = data.get('links', {}).get('next', '')
+            
+    print('Job complete')
