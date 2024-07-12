51665e45ef2b1c0034d4242037d1adc554285292
Jen Gilbert <jen.gilbert@datadoghq.com>
2024-07-12 11:11:51 -0500

    Successfully validate all potential option set names generated from placeholders

--- markdoc-hugo-integration/test/integration/happyPath.test.ts
+++ markdoc-hugo-integration/test/integration/happyPath.test.ts
@@ -7,21 +7,22 @@ const SNAPSHOTS_DIR = __dirname + '/../__snapshots__';
 describe('MarkdocToHugoCompiler', () => {
   const compiler = new MarkdocToHugoCompiler({
     prefOptionsConfigDir: TEST_SITE_DIR + '/preferences_config/options',
-    sitewidePrefsFilepath: TEST_SITE_DIR + '/preferences_config/sitewide_preferences.yaml',
+    sitewidePrefsFilepath:
+      TEST_SITE_DIR + '/preferences_config/sitewide_preferences.yaml',
     contentDir: TEST_SITE_DIR + '/content',
     partialsDir: TEST_SITE_DIR + '/partials'
   });
 
   test('ingests pref options', () => {
-    expect(JSON.stringify(compiler.prefOptionsConfig, null, 2)).toMatchFileSnapshot(
-      `${SNAPSHOTS_DIR}/prefOptionsConfig.snap.json`
-    );
+    expect(
+      JSON.stringify(compiler.prefOptionsConfig, null, 2)
+    ).toMatchFileSnapshot(`${SNAPSHOTS_DIR}/prefOptionsConfig.snap.json`);
   });
 
   test('ingests sitewide pref names', () => {
-    expect(JSON.stringify(compiler.sitewidePrefNames, null, 2)).toMatchFileSnapshot(
-      `${SNAPSHOTS_DIR}/sitewidePrefNames.snap.json`
-    );
+    expect(
+      JSON.stringify(compiler.sitewidePrefNames, null, 2)
+    ).toMatchFileSnapshot(`${SNAPSHOTS_DIR}/sitewidePrefNames.snap.json`);
   });
 
   test('detects Markdoc files', () => {
