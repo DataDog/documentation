79a06bd42f139d87b6f4fbf6568e9dc10fed9429
Jen Gilbert <jen.gilbert@datadoghq.com>
2024-07-19 11:31:22 -0500

    Add comments

--- markdoc/markdoc-hugo-integration/src/helperModules/FileParser.ts
+++ markdoc/markdoc-hugo-integration/src/helperModules/FileParser.ts
@@ -11,12 +11,21 @@ import { Frontmatter, FrontmatterSchema } from '../schemas/yaml/frontMatter';
 import { ConfigProcessor } from './ConfigProcessor';
 import { PrefOptionsConfig } from '../schemas/yaml/prefOptions';
 
+/**
+ * An object representing an error encountered
+ * while parsing a Markdoc file to an AST,
+ * along with context such as the line number.
+ */
 export type ParsingErrorReport = {
   error: ValidationError;
   file: string;
   lines: number[];
 };
 
+/**
+ * An object representing a parsed Markdoc file,
+ * including the ASTs of any partials referenced in the file.
+ */
 interface ParsedFile {
   ast: Node;
   frontmatter: Frontmatter;
