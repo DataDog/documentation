3b02c7c3b6e3e152957e9fd6b0fcbf527a1c8d39
hestonhoffman <hestonhoffman@gmail.com>
2024-07-25 14:36:23 -0700

    Remove status code

--- local/bin/py/synthetics_worker_version.py
+++ local/bin/py/synthetics_worker_version.py
@@ -18,7 +18,6 @@ def get_data():
     }
     response = requests.get(url, headers=headers)
     if response.status_code != 200:
-        print(response.status_code)
         print(response.text)
         raise Exception('Failed to get tags')
     return response.json()
