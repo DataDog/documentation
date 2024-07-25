0d046b7a90d34c66dd42e33975e535458604eda5
StefonSimmons <s.j.simmons@spartans.nsu.edu>
2024-07-25 15:12:59 -0400

    update stringto TitleCase logic

--- assets/scripts/helpers/string.js
+++ assets/scripts/helpers/string.js
@@ -1,7 +1,7 @@
 const stringToTitleCase = (string) => {
   if(string.length <= 0) return string;
   return string.split(' ')
-    .map(word => word[0].toUpperCase() + word.substr(1).toLowerCase())
+    .map(word => (word === 'CSM' ? word : word[0].toUpperCase()) + word.substr(1).toLowerCase())
     .join(' ');
 }
 
