871b5798ec7ef3500b6a17e7c25fb4caff1232e4
Jen Gilbert <jen.gilbert@datadoghq.com>
2024-08-08 11:47:30 -0500

    Add titles to tabs

--- markdoc/markdoc-hugo-integration/src/components/tab.ts
+++ markdoc/markdoc-hugo-integration/src/components/tab.ts
@@ -2,16 +2,24 @@ import { CustomHtmlComponent, Tag, Config } from 'markdoc-static-compiler';
 
 export const tabDefinition = {
   render: 'Tab',
-  children: ['paragraph']
-  /*
-  attributes: {
-    level: {
-      type: String,
-      default: 'info',
-      matches: ['info', 'warning']
+  children: ['paragraph'],
+  transform(node, config) {
+    console.log('annotations', node.annotations);
+    const label = node.annotations[0].value;
+    if (!label) {
+      console.log('Tab node:', JSON.stringify(node, null, 2));
+      throw new Error('Tab must have a label');
     }
+
+    return {
+      $$mdtype: 'Tag',
+      name: 'Tab',
+      attributes: {
+        label
+      },
+      children: node.transformChildren(config)
+    };
   }
-    */
 };
 
 export class Tab extends CustomHtmlComponent {
@@ -29,7 +37,6 @@ export class Tab extends CustomHtmlComponent {
   }
 
   render() {
-    console.log('rendering tab');
-    return `<div class="tab-pane fade" role="tabpanel">${this.contents}</div>`;
+    return `<div class="tab-pane fade" role="tabpanel" title="${this.tag.attributes.label}">${this.contents}</div>`;
   }
 }
