4fc113320e9cea2e7cf0b0a231c0c0df9b99923e
Jen Gilbert <jen.gilbert@datadoghq.com>
2024-08-22 13:26:11 -0500

    Exclude irrelevant files from test coverage report

--- markdoc/markdoc-hugo-integration/vitest.config.ts
+++ markdoc/markdoc-hugo-integration/vitest.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
         '**/markdoc-client-renderer.js',
         '**/debugBookmarklet.js',
         '**/compiledScripts/**',
+        '**/scripts/**',
         '**/test/**'
       ]
     }
