2157d8a8753674cd8ae57ea7c4dbff3bcc1f3488
Jen Gilbert <jen.gilbert@datadoghq.com>
2024-08-22 13:21:06 -0500

    Delete old code

--- markdoc/markdoc-hugo-integration/debugBookmarklet.js
+++
@@ -1,7 +0,0 @@
-javascript: (function () {
-    const hiddenElements = document.getElementsByClassName('markdoc__hidden');
-    for (let i = 0; i < hiddenElements.length; i++) {
-        hiddenElements[i].style.backgroundColor = 'lightgray';
-        hiddenElements[i].classList.remove('markdoc__hidden');
-    }
-})();
