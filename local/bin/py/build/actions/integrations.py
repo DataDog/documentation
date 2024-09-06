8e6248a0ad11010a7005f6b4005c59dafb5bd7aa
Jamie Jackson <james.jackson@datadoghq.com>
2024-09-06 10:11:39 -0400

    Update integrations.py

--- local/bin/py/build/actions/integrations.py
+++ local/bin/py/build/actions/integrations.py
@@ -29,8 +29,8 @@ from actions.format_link import format_link_file
 from actions.comment_conversion import replace_comments
 
 try:
-    from assetlib.classifiers import get_customer_facing_classifiers, get_non_deprecated_classifiers
-    CLASSIFIER_TAGS = get_customer_facing_classifiers()
+    from assetlib.classifiers import get_all_classifier_names, get_customer_facing_classifiers
+    CLASSIFIER_TAGS = get_all_classifier_names()
 except ImportError:
     CLASSIFIER_TAGS = []
     if getenv("CI_COMMIT_REF_NAME"):
@@ -42,7 +42,7 @@ finally:
     else:
         file_content = ['| Name | Description |\n| --- | --- |\n']
         with open('layouts/shortcodes/integration_categories.md', 'w') as file:
-            for tag in get_non_deprecated_classifiers():
+            for tag in get_customer_facing_classifiers():
                 file_content.append(f'| {tag["name"]} | {tag["description"]} |\n')
             file.write(''.join(file_content) + '\n')
 
