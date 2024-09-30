4e75c8dd6aac0036f1b61e202bff6615e936b793
Jen Gilbert <jen.gilbert@datadoghq.com>
2024-09-30 12:08:03 -0500

    Add tests for invalid allowlists

--- markdoc/markdoc-hugo-integration/src/schemas/yaml/allowlist.ts
+++ markdoc/markdoc-hugo-integration/src/schemas/yaml/allowlist.ts
@@ -1,19 +1,23 @@
 import { z } from 'zod';
 import { PREF_ID_REGEX } from '../regexes';
 
-export const AllowlistEntrySchema = z.object({
-  id: z.string().regex(PREF_ID_REGEX),
-  display_name: z.string()
-});
+export const AllowlistEntrySchema = z
+  .object({
+    id: z.string().regex(PREF_ID_REGEX),
+    display_name: z.string()
+  })
+  .strict();
 
 export const AllowlistSchema = z.array(AllowlistEntrySchema);
 
 export type AllowlistEntry = z.infer<typeof AllowlistEntrySchema>;
 export type Allowlist = z.infer<typeof AllowlistSchema>;
 
-export const AllowlistsByTypeSchema = z.object({
-  prefs: AllowlistSchema,
-  options: AllowlistSchema
-});
+export const AllowlistsByTypeSchema = z
+  .object({
+    prefs: AllowlistSchema,
+    options: AllowlistSchema
+  })
+  .strict();
 
 export type AllowlistsByType = z.infer<typeof AllowlistsByTypeSchema>;
--- markdoc/markdoc-hugo-integration/test/helperModules/YamlConfigParser/loadAllowlistsFromDir.test.ts
+++ markdoc/markdoc-hugo-integration/test/helperModules/YamlConfigParser/loadAllowlistsFromDir.test.ts
@@ -1,6 +1,9 @@
 import { describe, test, expect } from 'vitest';
-import { VALID_PREFS_CONFIG_DIR, SNAPSHOTS_DIR } from '../../config/constants';
+import { VALID_PREFS_CONFIG_DIR, SNAPSHOTS_DIR, MOCKS_DIR } from '../../config/constants';
 import { YamlConfigParser } from '../../../src/helperModules/YamlConfigParser';
+import fs from 'fs';
+
+const INVALID_ALLOWLISTS_DIR = `${MOCKS_DIR}/invalid/allowlistsDirs`;
 
 describe('YamlConfigParser.loadAllowlistsFromDir', () => {
   test('loads allowlists from a prefs config directory', () => {
@@ -10,5 +13,26 @@ describe('YamlConfigParser.loadAllowlistsFromDir', () => {
     expect(JSON.stringify(allowlistsByType, null, 2)).toMatchFileSnapshot(
       `${SNAPSHOTS_DIR}/helperModules/YamlConfigParser/valid/ingestedAllowlists.snap.json`
     );
+  });
+
+  const invalidDirs = fs.readdirSync(INVALID_ALLOWLISTS_DIR);
+
+  invalidDirs.forEach((invalidDir) => {
+    test(`throws an error when processing the '${invalidDir}' directory`, () => {
+      expect(() =>
+        YamlConfigParser.loadAllowlistsFromDir(`${INVALID_ALLOWLISTS_DIR}/${invalidDir}`)
+      ).toThrow();
+    });
+
+    test(`throws an error that matches the snapshot for the '${invalidDir}' directory`, () => {
+      try {
+        YamlConfigParser.loadAllowlistsFromDir(`${INVALID_ALLOWLISTS_DIR}/${invalidDir}`);
+      } catch (error) {
+        const sanitizedErrorMessage = error.message.replace(INVALID_ALLOWLISTS_DIR, '');
+        expect(sanitizedErrorMessage).toMatchFileSnapshot(
+          `${SNAPSHOTS_DIR}/helperModules/YamlConfigParser/invalid/ingestedAllowlistsErrors/${invalidDir}.snap.txt`
+        );
+      }
+    });
   });
 });
