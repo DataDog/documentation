4c802c28b48091c5b8e835b05faef4be61e779b4
Devin Ford <devin.ford@datadoghq.com>
2024-10-23 14:28:28 -0400

    fix: update rule to just have security as well

--- local/bin/py/build/actions/security_rules.py
+++ local/bin/py/build/actions/security_rules.py
@@ -148,6 +148,7 @@ def security_rules(content, content_dir):
                 "aliases": [
                     # f"{data.get('defaultRuleId', '').strip()}",
                     f"/security_monitoring/default_rules/{data.get('defaultRuleId', '').strip()}",
+                    f"/security/default_rules/{data.get('defaultRuleId', '').strip()}",
                     f"/security_monitoring/default_rules/{p.stem.lower()}"
                 ],
                 "rule_category": [],
