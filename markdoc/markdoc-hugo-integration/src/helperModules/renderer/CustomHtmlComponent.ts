9896bd5563ff45fd008d653af1163026976e0ab8
Jen Gilbert <jen.gilbert@datadoghq.com>
2024-10-28 13:46:06 -0500

    Validate the Hugo config fields inside the custom HTML component

--- markdoc/markdoc-hugo-integration/src/helperModules/renderer/CustomHtmlComponent.ts
+++ markdoc/markdoc-hugo-integration/src/helperModules/renderer/CustomHtmlComponent.ts
@@ -1,25 +1,35 @@
 import { render } from '.';
 import { Tag, Config } from 'markdoc-static-compiler';
+import { HugoConfig, HugoConfigSchema } from '../../schemas/hugoConfig';
 import MarkdownIt from 'markdown-it';
 const { escapeHtml, unescapeAll } = MarkdownIt().utils;
 
 export abstract class CustomHtmlComponent {
   contents = '';
   tag: Tag;
-  config: Config | undefined;
+  markdocConfig: Config | undefined;
+  hugoConfig: HugoConfig;
   // TODO: What kind of type should be used for components?
   components: Record<string, any> | undefined;
 
   constructor(
     tag: Tag,
-    config?: Config,
+    markdocConfig: Config,
     components?: Record<string, CustomHtmlComponent>
   ) {
-    this.config = config;
+    this.markdocConfig = markdocConfig;
+
+    // Validate that the Hugo config is present
+    // and contains the required fields
+    if (!this.markdocConfig.variables?.hugoConfig) {
+      throw new Error('Hugo config is not defined');
+    }
+    this.hugoConfig = HugoConfigSchema.parse(this.markdocConfig.variables.hugoConfig);
+
     this.components = components;
     this.tag = tag;
     if (tag.children.length > 0) {
-      this.contents = render(tag.children, config, components);
+      this.contents = render(tag.children, markdocConfig, components);
     }
   }
 
