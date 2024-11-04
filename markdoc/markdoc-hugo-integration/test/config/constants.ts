745a9efaf9fdfdceb1428f85339c2e4a1979fdbe
Jen Gilbert <jen.gilbert@datadoghq.com>
2024-11-04 17:11:18 -0600

    Update mock test sites to adhere to conventions

--- markdoc/markdoc-hugo-integration/test/config/constants.ts
+++ markdoc/markdoc-hugo-integration/test/config/constants.ts
@@ -8,9 +8,9 @@ export const MOCKS_DIR = path.join(__dirname, '..', 'mocks');
 
 // Valid example site
 export const VALID_SITE_DIR = path.join(__dirname, '..', 'mocks', 'validSite');
-export const VALID_PREFS_CONFIG_DIR = VALID_SITE_DIR + '/config/preferences';
+export const VALID_PREFS_CONFIG_DIR = VALID_SITE_DIR + '/config/_default/preferences/';
 export const VALID_CONTENT_DIR = VALID_SITE_DIR + '/content';
-export const VALID_PARTIALS_DIR = VALID_SITE_DIR + '/partials';
+export const VALID_PARTIALS_DIR = VALID_SITE_DIR + '/layouts/partials';
 
 // Invalid example site
 export const INVALID_SITE_DIR = path.join(__dirname, '..', 'mocks', 'invalidSite');
