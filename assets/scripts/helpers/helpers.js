13e9ba1c92c625bd67c06a44cdf8864245a44f07
Matthew Fitzsimmons <matt@fitzage.com>
2024-11-08 08:18:32 -0700

    rough test of removal of chrome fix

--- assets/scripts/helpers/helpers.js
+++ assets/scripts/helpers/helpers.js
@@ -53,14 +53,6 @@ const getCookieByName = (name) => {
 };
 
 const chromeHashFix = () => {
-    const isChrome = /Chrome/.test(navigator.userAgent);
-    if (window.location.hash && isChrome) {
-        setTimeout(function () {
-            const hash = window.location.hash;
-            window.location.hash = '';
-            window.location.hash = hash;
-        }, 300);
-    }
 }
 
 const bodyClassContains = (string) => document.body.classList.contains(string);
