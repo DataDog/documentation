6a0750520417c2a83f87f902212dd7552f769c36
Jen Gilbert <jen.gilbert@datadoghq.com>
2024-12-02 12:04:51 -0600

    Add types and validations for HugoGlobalConfigBuilder

--- markdoc/markdoc-hugo-integration/src/helperModules/HugoGlobalConfigBuilder.ts
+++ markdoc/markdoc-hugo-integration/src/helperModules/HugoGlobalConfigBuilder.ts
@@ -1,10 +1,22 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as yaml from 'js-yaml';
-import { HugoGlobalConfig, HugoSubdirsByType } from '../schemas/config/hugo';
+import {
+  HugoEnv,
+  HugoGlobalConfig,
+  HugoLanguagesConfig,
+  HugoLanguagesConfigSchema,
+  HugoSiteConfig,
+  HugoSiteConfigSchema,
+  HugoSiteParams,
+  HugoSiteParamsSchema,
+  HugoSubdirsByType,
+  i18nConfig,
+  i18nConfigSchema
+} from '../schemas/config/hugo';
 
 class HugoGlobalConfigBuilder {
-  static build(baseSiteDir: string, env: string): HugoGlobalConfig {
+  static build(baseSiteDir: string, env: HugoEnv): HugoGlobalConfig {
     const siteParams = this.loadSiteParams(baseSiteDir, env);
     const siteConfig = this.loadSiteConfig(baseSiteDir, env);
     const languages = this.loadLanguages(baseSiteDir);
@@ -30,43 +42,59 @@ class HugoGlobalConfigBuilder {
     };
   }
 
-  private static loadSiteParams(baseSiteDir: string, env: string) {
+  private static loadSiteParams(baseSiteDir: string, env: HugoEnv): HugoSiteParams {
     const defaultSiteParamsFile = baseSiteDir + '/config/_default/params.yaml';
     const defaultSiteParams = yaml.load(fs.readFileSync(defaultSiteParamsFile, 'utf8'));
 
     const envSiteParamsFile = baseSiteDir + `/config/${env}/params.yaml`;
     const envSiteParams = yaml.load(fs.readFileSync(envSiteParamsFile, 'utf8'));
 
-    const siteParams = Object.assign({}, defaultSiteParams, envSiteParams);
+    const siteParams = Object.assign(
+      {},
+      defaultSiteParams,
+      envSiteParams
+    ) as HugoSiteParams;
+    HugoSiteParamsSchema.parse(siteParams);
+
     return siteParams;
   }
 
-  private static loadSiteConfig(baseSiteDir: string, env: string) {
+  private static loadSiteConfig(baseSiteDir: string, env: HugoEnv): HugoSiteConfig {
     const defaultSiteConfigFile = baseSiteDir + '/config/_default/config.yaml';
     const defaultSiteConfig = yaml.load(fs.readFileSync(defaultSiteConfigFile, 'utf8'));
 
     const envSiteConfigFile = baseSiteDir + `/config/${env}/config.yaml`;
     const envSiteConfig = yaml.load(fs.readFileSync(envSiteConfigFile, 'utf8'));
 
-    const siteConfig = Object.assign({}, defaultSiteConfig, envSiteConfig);
+    const siteConfig = Object.assign(
+      {},
+      defaultSiteConfig,
+      envSiteConfig
+    ) as HugoSiteConfig;
+    HugoSiteConfigSchema.parse(siteConfig);
+
     return siteConfig;
   }
 
-  private static loadLanguages(baseSiteDir: string) {
+  private static loadLanguages(baseSiteDir: string): HugoLanguagesConfig {
     const languagesFile = baseSiteDir + '/config/_default/languages.yaml';
     const languagesConfig = yaml.load(fs.readFileSync(languagesFile, 'utf8'));
+    // @ts-ignore, data is validated by the schema on the next line
     const languages = Object.keys(languagesConfig);
+    HugoLanguagesConfigSchema.parse(languages);
     return languages;
   }
 
-  static loadI18n(baseSiteDir: string) {
+  static loadI18n(baseSiteDir: string): i18nConfig {
     const i18n = {};
     const i18nDir = baseSiteDir + '/i18n';
     const files = fs.readdirSync(i18nDir);
     files.forEach((file) => {
       const lang = file.replace('.json', '');
+      // @ts-ignore, data is validated by the schema on the next line
       i18n[lang] = yaml.load(fs.readFileSync(path.resolve(i18nDir, file), 'utf8'));
     });
+    i18nConfigSchema.parse(i18n);
     return i18n;
   }
 }
--- markdoc/markdoc-hugo-integration/src/schemas/config/hugo.ts
+++ markdoc/markdoc-hugo-integration/src/schemas/config/hugo.ts
@@ -1,5 +1,4 @@
 import { z } from 'zod';
-import { IntegrationConfigSchema } from './integration';
 
 /**
  * A mapping of commonly accessed Hugo subdirectories
@@ -22,6 +21,41 @@ export const HugoSubdirsByTypeSchema = z
  */
 export type HugoSubdirsByType = z.infer<typeof HugoSubdirsByTypeSchema>;
 
+export const HugoSiteParamsSchema = z.object({
+  img_url: z.string().url(),
+  branch: z.string().optional() // required if env is "preview"
+});
+
+export type HugoSiteParams = z.infer<typeof HugoSiteParamsSchema>;
+
+export const HugoSiteConfigSchema = z.object({
+  baseURL: z.string().url()
+});
+
+export type HugoSiteConfig = z.infer<typeof HugoSiteConfigSchema>;
+
+export const HugoEnvSchema = z.union([
+  z.literal('development'),
+  z.literal('preview'),
+  z.literal('live')
+]);
+
+export type HugoEnv = z.infer<typeof HugoEnvSchema>;
+
+export const HugoLanguagesConfigSchema = z.array(z.string());
+
+export type HugoLanguagesConfig = z.infer<typeof HugoLanguagesConfigSchema>;
+
+export const i18nConfigSchema = z.record(
+  z.record(
+    z.object({
+      other: z.string()
+    })
+  )
+);
+
+export type i18nConfig = z.infer<typeof i18nConfigSchema>;
+
 /**
  * Any Hugo config that applies to the entire site,
  * such as the list of supported languages and the base URL.
@@ -33,21 +67,12 @@ export type HugoSubdirsByType = z.infer<typeof HugoSubdirsByTypeSchema>;
  */
 export const HugoGlobalConfigSchema = z
   .object({
-    siteParams: z.object({
-      img_url: z.string().url(),
-      branch: z.string().optional() // required if env is "preview"
-    }),
-    siteConfig: z.object({ baseURL: z.string().url() }),
-    env: z.union([z.literal('development'), z.literal('preview'), z.literal('live')]),
-    languages: z.array(z.string()),
+    siteParams: HugoSiteParamsSchema,
+    siteConfig: HugoSiteConfigSchema,
+    env: HugoEnvSchema,
+    languages: HugoLanguagesConfigSchema,
     siteDir: z.string().min(1),
-    i18n: z.record(
-      z.record(
-        z.object({
-          other: z.string()
-        })
-      )
-    ),
+    i18n: i18nConfigSchema,
     dirs: HugoSubdirsByTypeSchema
   })
   .refine((config) => {
