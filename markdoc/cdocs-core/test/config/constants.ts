6dd8097d74f8b30634de8d64d5f1641ef3dd5972
Jen Gilbert <jen.gilbert@datadoghq.com>
2025-01-08 16:11:23 -0600

    Remove unused code

--- markdoc/cdocs-core/test/config/constants.ts
+++ markdoc/cdocs-core/test/config/constants.ts
@@ -6,11 +6,9 @@ export const SNAPSHOTS_DIR = __dirname + '/../__snapshots__';
 // Where all mock data should be stored
 export const MOCKS_DIR = path.join(__dirname, '..', 'mocks');
 
-// Valid example site
-export const VALID_SITE_DIR = path.join(__dirname, '..', 'mocks', 'validSite');
-export const VALID_FILTERS_CONFIG_DIR = VALID_SITE_DIR + '/content_filters/';
-export const VALID_CONTENT_DIR = VALID_SITE_DIR + '/content';
-export const VALID_PARTIALS_DIR = VALID_SITE_DIR + '/layouts/partials';
+// Where all valid mock data should be stored
+export const VALID_MOCKS_DIR = path.join(__dirname, '..', 'mocks', 'valid');
 
-// Invalid example site
-export const INVALID_SITE_DIR = path.join(__dirname, '..', 'mocks', 'invalidSite');
+// Valid configuration data
+export const VALID_FILTERS_CONFIG_DIR = VALID_MOCKS_DIR + '/content_filters/';
+export const VALID_CONTENT_DIR = VALID_MOCKS_DIR + '/content';
