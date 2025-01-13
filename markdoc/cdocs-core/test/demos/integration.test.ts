5bfe63026f85b7a532e7fc7fe2e4036a606b855f
Jen Gilbert <jen.gilbert@datadoghq.com>
2025-01-13 12:01:09 -0600

    [wip] Deprecate old functions/types

--- markdoc/cdocs-core/test/demos/integration.test.ts
+++ markdoc/cdocs-core/test/demos/integration.test.ts
@@ -1,20 +1,7 @@
-import { GlossarySchema } from '../../src/schemas/glossary';
-import { FilterOptionsConfigSchema } from '../../src/schemas/filterOptions';
 import { describe, test, expect } from 'vitest';
 import { SNAPSHOTS_DIR } from '../config/constants';
-import { z } from 'zod';
+import { VALID_FILTERS_CONFIG_DIR } from '../config/constants';
 import { CdocsDataManager } from '../../src/modules/CdocsDataManager';
-import { ContentFiltersConfigByLangSchema } from '../../src/schemas/contentFiltersConfig';
-
-/**
- * TODO: Add a loadContentFiltersConfig that takes the top-level
- * content filters config directory and loads all required config.
- */
-
-/**
- * TODO: Rename the FilterOptionsConfig to OptionListGlossary,
- * so we have a FilterGlossary, OptionGlossary, and OptionListGlossary.
- */
 
 const configDir = __dirname + '/example_content_filters_config';
 const langs = ['en', 'ja']; // TODO: Change to piglatin
@@ -22,7 +9,7 @@ const langs = ['en', 'ja']; // TODO: Change to piglatin
 describe('Demo', () => {
   test('parses the content filter configuration from YAML', async () => {
     const { contentFiltersConfigByLang } = CdocsDataManager.loadContentFiltersConfig({
-      configDir,
+      configDir: VALID_FILTERS_CONFIG_DIR,
       langs,
     });
 
