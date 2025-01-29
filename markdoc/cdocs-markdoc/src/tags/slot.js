1e56d160edebb2c9e926c7dcc20360ff2600addb
Jen Gilbert <jen.gilbert@datadoghq.com>
2025-01-29 17:00:32 -0600

    Remove compiled files

--- markdoc/cdocs-markdoc/src/tags/slot.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.slot = void 0;
-exports.slot = {
-    attributes: {
-        primary: { type: String, required: true },
-    },
-};
--- markdoc/cdocs-markdoc/src/tags/table.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.table = void 0;
-exports.table = {
-    children: ['table'],
-    inline: false,
-};
