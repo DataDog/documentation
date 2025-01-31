ca500ffb4e58e7b7665a51ba6ecd486f913133b2
Jen Gilbert <jen.gilbert@datadoghq.com>
2025-01-31 15:43:43 -0600

    Remove commented-out code

--- markdoc/cdocs-markdoc/src/ast/variable.ts
+++ markdoc/cdocs-markdoc/src/ast/variable.ts
@@ -32,25 +32,3 @@ export default class Variable implements AstType {
     };
   }
 }
-
-/* ORIGINAL FILE ----------------------------------------------------
-
-import type { Config, AstType } from '../types';
-
-export default class Variable implements AstType {
-  readonly $$mdtype = 'Variable';
-
-  path;
-
-  constructor(path: (string | number)[] = []) {
-    this.path = path;
-  }
-
-  resolve({ variables }: Config = {}) {
-    return variables instanceof Function
-      ? variables(this.path)
-      : this.path.reduce((obj = {}, key) => obj[key], variables);
-  }
-}
-  
------------------------------------------------------------------- */
--- markdoc/cdocs-markdoc/src/tags/conditional.ts
+++ markdoc/cdocs-markdoc/src/tags/conditional.ts
@@ -5,6 +5,16 @@
  * with its rendered value. The version below keeps conditional data,
  * allowing the client to re-resolve the value when the variables change.
  *
+ * The most complex updates are around supporting `else` tags that are nested
+ * inside an `if` tag, with each `else` tag possibly containing its own condition
+ * (functioning as an `else if` in practice). The cdocs solution follows
+ * the same processing pattern as the original Markdoc, but because all content
+ * must be retained whether it should be displayed or not, the logic is more complex.
+ *
+ * To support this complexity, Markdoc's original Condition type
+ * has been replaced with the ConditionalContent,
+ * TransformedConditionalContent, and ResolvedConditionalContent types.
+ *
  * Any conditional content is wrapped in an enclosing span or div
  * with the added buildEnclosingTag() function. Each enclosing tag
  * in the renderable tree has a display attribute that is set to 'true'
@@ -12,14 +22,6 @@
  *
  * In rendered HTML, the visibility of this enclosing tag can be toggled
  * as the user's customizations selections change.
- *
- * The most complex updates are around supporting `else` tags that are nested
- * inside an `if` tag, with each `else` tag possibly containing its own condition
- * (functioning as an `else if` in practice). The cdocs solution follows
- * the same processing pattern as the original Markdoc, but because all content
- * must be retained whether it should be displayed or not, the logic is more complex.
- * The original Markdoc simply returns early as soon as it finds a truthy condition
- * in the set of if/else tags.
  */
 
 import { isPromise } from '../utils';
@@ -33,7 +35,6 @@ import {
   RenderableTreeNodes,
   Schema
 } from '../types';
-import { join } from 'path';
 
 type ConditionalContent = {
   condition: ClientFunction | ClientVariable;
@@ -110,6 +111,9 @@ function negateCondition(condition: ClientFunction | ClientVariable): ClientFunc
   };
 }
 
+/**
+ * The cdocs analog to Markdoc's renderConditions function.
+ */
 function buildConditionalContentEntries(node: Node): ConditionalContent[] {
   const contentEntries: ConditionalContent[] = [];
 
@@ -125,9 +129,14 @@ function buildConditionalContentEntries(node: Node): ConditionalContent[] {
     if (child.type === 'tag' && child.tag === 'else') {
       let condition: ClientFunction | ClientVariable;
 
+      // Check whether the else tag has its own condition
+      // (i.e. it functions as an `else if`)
       const elseCondition =
         'primary' in child.attributes ? child.attributes.primary : null;
 
+      // Negate and add any preceding conditions,
+      // since the contents of an else tag should only show if
+      // nothing else has shown so far
       if (elseCondition) {
         condition = joinConditions([
           ...contentEntries.map((entry) => negateCondition(entry.condition)),
@@ -160,6 +169,8 @@ export const tagIf: Schema = {
   },
 
   transform(node, config) {
+    // Wrap any conditional content in an enclosing span or div
+    // whose visibility can be toggled on and off in the browser
     const buildEnclosingTag = (resolvedCondition: ResolvedConditionalContent) => {
       const enclosingTag: Tag = {
         $$mdtype: 'Tag',
@@ -212,69 +223,3 @@ export const tagElse: Schema = {
     primary: { type: Object, render: false }
   }
 };
-
-/* ORIGINAL FILE --------------------------------------------------
-
-import { isPromise } from '../utils';
-
-import {
-  MaybePromise,
-  Node,
-  RenderableTreeNode,
-  RenderableTreeNodes,
-  Schema,
-  Value,
-} from '../types';
-
-type Condition = { condition: Value; children: Node[] };
-
-export function truthy(value: any) {
-  return value !== false && value !== undefined && value !== null;
-}
-
-function renderConditions(node: Node) {
-  const conditions: Condition[] = [
-    { condition: node.attributes.primary, children: [] },
-  ];
-  for (const child of node.children) {
-    if (child.type === 'tag' && child.tag === 'else')
-      conditions.push({
-        condition:
-          'primary' in child.attributes ? child.attributes.primary : true,
-        children: [],
-      });
-    else conditions[conditions.length - 1].children.push(child);
-  }
-
-  return conditions;
-}
-
-export const tagIf: Schema = {
-  attributes: {
-    primary: { type: Object, render: false },
-  },
-
-  transform(node, config) {
-    const conditions = renderConditions(node);
-    for (const { condition, children } of conditions)
-      if (truthy(condition)) {
-        const nodes = children.flatMap<MaybePromise<RenderableTreeNodes>>(
-          (child) => child.transform(config)
-        );
-        if (nodes.some(isPromise)) {
-          return Promise.all(nodes).then((nodes) => nodes.flat());
-        }
-        return nodes as RenderableTreeNode[];
-      }
-    return [];
-  },
-};
-
-export const tagElse: Schema = {
-  selfClosing: true,
-  attributes: {
-    primary: { type: Object, render: false },
-  },
-};
-
--------------------------------------------------------------------*/
