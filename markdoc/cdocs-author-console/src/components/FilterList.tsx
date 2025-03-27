48f3a86c4f09b667f572509659b084a9bb66a66b
Jen Gilbert <jen.gilbert@datadoghq.com>
2025-03-27 13:10:45 -0500

    Sketch in trait form

--- markdoc/cdocs-author-console/src/components/FilterList.tsx
+++ markdoc/cdocs-author-console/src/components/FilterList.tsx
@@ -1,8 +1,122 @@
 import { useState } from 'react';
-import { FilterConfig } from 'cdocs-data';
+import { CustomizationConfig, FilterConfig, FilterConfigSchema } from 'cdocs-data';
+import TraitSelector from './selectors/TraitSelector';
+import OptionGroupSelector from './selectors/OptionGroupSelector';
 
-function FilterList() {
-  return <div>Filter list goes here.</div>;
+function FilterList({ customizationConfig }: { customizationConfig: CustomizationConfig }) {
+  return (
+    <div>
+      <p>Filter list goes here.</p>
+      <FilterForm customizationConfig={customizationConfig} onEdit={() => {}} />
+    </div>
+  );
+}
+
+function FilterSummary({
+  filterConfig,
+  customizationConfig
+}: {
+  filterConfig: FilterConfig;
+  customizationConfig: CustomizationConfig;
+}) {
+  if (!filterConfig.trait_id || !filterConfig.option_group_id) {
+    return <span>Filter incomplete so far.</span>;
+  }
+  const optionLabels = customizationConfig.optionGroupsById[filterConfig.option_group_id]?.map(
+    (option) => option.label
+  );
+
+  return (
+    <span>
+      {filterConfig.label}: {optionLabels.join(', ')}
+    </span>
+  );
+}
+
+function FilterForm({
+  filter,
+  customizationConfig,
+  onEdit
+}: {
+  filter?: FilterConfig;
+  customizationConfig: CustomizationConfig;
+  onEdit: (filter: FilterConfig) => void;
+}) {
+  const blankFilter: FilterConfig = {
+    label: '',
+    trait_id: '',
+    option_group_id: ''
+  };
+
+  const [filterConfig, setFilterConfig] = useState<FilterConfig>(blankFilter);
+
+  const [newCustomizationConfig, setNewCustomizationConfig] = useState<CustomizationConfig>({
+    traitsById: {},
+    optionsById: {},
+    optionGroupsById: {}
+  });
+
+  return (
+    <div>
+      <p>
+        <FilterSummary filterConfig={filterConfig} customizationConfig={customizationConfig} />
+      </p>
+      <h2>Trait</h2>
+      <TraitForm
+        customizationConfig={customizationConfig}
+        onUpdate={({
+          traitId,
+          newTraitConfig
+        }: {
+          traitId: string;
+          newTraitConfig?: { id: string; label: string; internal_notes?: string };
+        }) => {
+          // If a new trait config was passed, add it to the new customization config
+          if (newTraitConfig) {
+            setNewCustomizationConfig({
+              ...newCustomizationConfig,
+              traitsById: {
+                ...newCustomizationConfig.traitsById,
+                [newTraitConfig.id]: newTraitConfig
+              }
+            });
+          }
+
+          // Update the filter config with the newly chosen trait ID
+          setFilterConfig({
+            ...filterConfig,
+            trait_id: traitId,
+            label: customizationConfig.traitsById[traitId].label
+          });
+        }}
+      />
+      <h2>Options</h2>
+      <OptionGroupSelector
+        customizationConfig={customizationConfig}
+        onSelect={(optionGroupId) => setFilterConfig({ ...filterConfig, option_group_id: optionGroupId })}
+      />
+    </div>
+  );
 }
 
 export default FilterList;
+
+function TraitForm({
+  customizationConfig,
+  onUpdate
+}: {
+  customizationConfig: CustomizationConfig;
+  onUpdate: ({
+    traitId,
+    newTraitConfig
+  }: {
+    traitId: string;
+    newTraitConfig?: { id: string; label: string; internal_notes?: string };
+  }) => void;
+}) {
+  const handleExistingTraitSelect = (traitId: string) => {
+    onUpdate({ traitId });
+  };
+
+  return <TraitSelector customizationConfig={customizationConfig} onSelect={handleExistingTraitSelect} />;
+}
--- markdoc/cdocs-author-console/src/components/PageWizard.tsx
+++ markdoc/cdocs-author-console/src/components/PageWizard.tsx
@@ -4,25 +4,34 @@ import { CustomizationConfig } from 'cdocs-data';
 
 function PageWizard({ customizationConfig }: { customizationConfig: CustomizationConfig }) {
   return (
-    <div style={{ display: 'flex', height: '100vh', gap: '1rem' }}>
-      <div style={{ flex: 1, borderRight: '1px solid #e0e0e0' }}>
+    <div style={{ display: 'flex', height: '100vh' }}>
+      <div style={{ flex: 1, padding: '1rem', borderRight: '1px solid #e0e0e0' }}>
         <h1>Filters</h1>
-        <FilterList />
+        <FilterList customizationConfig={customizationConfig} />
       </div>
-      <div style={{ flex: 1 }}>
+      <div style={{ flex: 1, padding: '1rem' }}>
         <h1>Setup instructions</h1>
-        <h2>Add the new sitewide config</h2>
-        <p>This won't actually show unless there's new sitewide config to be added.</p>
-        <h3>Add the trait</h3>
-        <p>This won't actually show unless there's a new trait to add.</p>
-        <h3>Add the options</h3>
-        <p>This won't actually show unless there are new options to add.</p>
-        <h3>Add the new option groups</h3>
-        <p>This won't actually show unless there are new option groups to add.</p>
+        <p>You can copy any markup by clicking it.</p>
+        {false && (
+          <div>
+            <h2>Add the new sitewide config</h2>
+            <p>This won't actually show unless there's new sitewide config to be added.</p>
+            <h3>Add the trait</h3>
+            <p>This won't actually show unless there's a new trait to add.</p>
+            <h3>Add the options</h3>
+            <p>This won't actually show unless there are new options to add.</p>
+            <h3>Add the new option groups</h3>
+            <p>This won't actually show unless there are new option groups to add.</p>
+          </div>
+        )}
         <h2>Create the page</h2>
         <p>
-          Create a <code>{'<YOUR_FILENAME>'}.mdoc.md</code> file somewhere in the <code>content/en</code> folder. Add
-          the markup below to it:
+          <ol>
+            <li>
+              Create a <code>{'<YOUR_FILENAME>'}.mdoc.md</code> file somewhere in the <code>content/en</code> folder.
+            </li>
+            <li>Add the markup below to it.</li>
+          </ol>
         </p>
         <MarkdocTemplate />
       </div>
