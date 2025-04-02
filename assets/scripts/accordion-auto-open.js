c5489b797deb60875e8293cadc60c9aa55498b18
Janine Chan <64388808+janine-c@users.noreply.github.com>
2025-04-02 12:40:32 -0600

    Make accordions anchorable (theoretically)

---
+++ assets/scripts/accordion-auto-open.js
@@ -0,0 +1,14 @@
+// Automatically opens an accordion when the URL contains an anchor that matches the accordion's ID - adapted from https://stackoverflow.com/questions/37033406/automatically-open-details-element-on-id-call
+
+function openAccordion() {
+	var hash = location.hash.substring(1);
+	if (hash) var details = document.getElementById(hash);
+	if (details && details.tagName.toLowerCase() === 'details') {
+			details.setAttribute('open', '');
+	}
+}
+window.addEventListener('hashchange', openAccordion);
+window.addEventListener('load', openAccordion);
+openAccordion();
+
+export {openAccordion};
