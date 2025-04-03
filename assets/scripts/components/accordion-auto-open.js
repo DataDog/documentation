a72e4389be75280e2544fccf48e4a9138ff0f529
Janine Chan <64388808+janine-c@users.noreply.github.com>
2025-04-03 12:26:02 -0400

    Take Brian's suggestion
    Co-authored-by: Brian Deutsch <bgdeutsch@gmail.com>

--- assets/scripts/components/accordion-auto-open.js
+++ assets/scripts/components/accordion-auto-open.js
@@ -1,7 +1,7 @@
 // Automatically opens an accordion when the URL contains an anchor that matches the accordion's ID - adapted from https://stackoverflow.com/questions/37033406/automatically-open-details-element-on-id-call
 
 function openAccordion() {
-	var hash = location.hash.substring(1);
+	const hash = location.hash.substring(1);
 	if (hash) var details = document.getElementById(hash);
 	if (details && details.tagName.toLowerCase() === 'details') {
 			details.setAttribute('open', '');
