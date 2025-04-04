193a38d4524090db315949e34b51b310f65352f1
Brian Deutsch <brian.deutsch@datadoghq.com>
2025-04-04 11:22:51 -0400

    fixes scroll top
    Signed-off-by: Brian Deutsch <brian.deutsch@datadoghq.com>

--- assets/scripts/components/accordion-auto-open.js
+++ assets/scripts/components/accordion-auto-open.js
@@ -1,12 +1,15 @@
 function openAccordion() {
 	const hash = location.hash.substring(1);
+
 	if (hash) {
-	  var details = document.getElementById(hash)
-	}
-	if (details && details.tagName.toLowerCase() === 'details') {
+		const details = document.getElementById(hash);
+
+		if (details && details.tagName.toLowerCase() === 'details') {
 			details.setAttribute('open', '');
+		}
 	}
 }
+
 window.addEventListener('hashchange', openAccordion);
 openAccordion();
 
