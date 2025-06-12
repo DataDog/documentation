4f6586dc688d2dfbdd99f703337a1eeafc690e4e
Seth Samuel <seth.samuel@datadoghq.com>
2025-06-12 16:14:17 -0400

    WIP

--- static/resources/yaml/dbm/rds-auto-install/deploy.sh
+++ static/resources/yaml/dbm/rds-auto-install/deploy.sh
@@ -3,9 +3,9 @@
 aws cloudformation deploy \
 --template-file cloudformation.yaml \
 --stack-name rds-auto-install \
---capabilities CAPABILITY_IAM \
---parameters \
-"ParameterKey=VPC,ParameterValue=$(aws cloudformation describe-stacks --stack-name "rds-auto-install-demo" --query "Stacks[0].Outputs[?ExportName=='VPCId'].OutputValue" --output text)" \
-"ParameterKey=SubnetId,ParameterValue=$(aws cloudformation describe-stacks --stack-name "rds-auto-install-demo" --query "Stacks[0].Outputs[?ExportName=='SubnetAId'].OutputValue" --output text)" \
-ParameterKey=LambdaBucket,ParameterValue=rds-auto-install-demo \
-"ParameterKey=LambdaKey,ParameterValue=$(tar c -C lambda index.js package.json package-lock.json | md5 -q).zip"
+--parameter-overrides \
+"VPC=$(aws cloudformation describe-stacks --stack-name "rds-auto-install-demo" --query "Stacks[0].Outputs[?OutputKey=='VPCId'].OutputValue" --output text)" \
+"SubnetId=$(aws cloudformation describe-stacks --stack-name "rds-auto-install-demo" --query "Stacks[0].Outputs[?OutputKey=='SubnetAId'].OutputValue" --output text)" \
+"LambdaBucket=rds-auto-install-demo" \
+"LambdaKey=$(tar c -C lambda index.js package.json package-lock.json | md5 -q).zip" \
+--capabilities CAPABILITY_IAM
