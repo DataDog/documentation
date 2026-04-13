a1416cb1a9f6167cf6c0459367061e438eafa633
Jen Gilbert <jen.gilbert@datadoghq.com>
2026-04-13 11:33:34 -0500

    Remove generated examples from version control

--- astro/src/data/api/examples/v1/GetMonitor.rb
+++
@@ -1,9 +0,0 @@
-require 'dogapi'
-
-api_key = '<DATADOG_API_KEY>'
-app_key = '<DATADOG_APPLICATION_KEY>'
-
-dog = Dogapi::Client.new(api_key, app_key)
-
-# Get a monitors's details
-dog.get_monitor(91_879, group_states: 'all')
