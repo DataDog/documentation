fa8402396399a9ab646710d67d55aa46af4aff6d
Jen Gilbert <jen.gilbert@datadoghq.com>
2026-04-24 18:02:09 -0500

    Tweak components table

--- astro/src/content.config.ts
+++ astro/src/content.config.ts
@@ -6,6 +6,7 @@ const docs = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string().optional(),
+    type: z.enum(['static', 'interactive']).optional(),
   }),
 });
 
