be99450f70cfe360e68a2c218b470dd04dcabbbc
Jen Gilbert <jen.gilbert@datadoghq.com>
2026-06-15 13:53:20 -0500

    Example reorg

--- content/en/api/v1/downtimes/CancelDowntimesByScope.rb
+++
@@ -1,9 +0,0 @@
-require 'dogapi'
-
-api_key = '<DATADOG_API_KEY>'
-app_key = '<DATADOG_APPLICATION_KEY>'
-
-dog = Dogapi::Client.new(api_key, app_key)
-
-# Cancel all downtimes with the given scope
-dog.cancel_downtime_by_scope('env:testing')
