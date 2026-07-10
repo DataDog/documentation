218f70d7cd29c2b2656a78bcf4017ccff0c6941e
Jen Gilbert <jen.gilbert@datadoghq.com>
2026-07-10 11:42:28 -0500

    Tweak script

--- astro_reorg/create_test_prs.py
+++ astro_reorg/create_test_prs.py
@@ -25,7 +25,7 @@ from pathlib import Path
 # ---------------------------------------------------------------------------
 
 # Branch the test PRs target. Create this on the remote before running.
-MOCK_BASE_BRANCH = "jen.gilbert/astro-reorg-mock-base"
+MOCK_BASE_BRANCH = "jen.gilbert/astro-reorg-demo-7-10-114044"
 
 REPO = "DataDog/documentation"
 REPO_ROOT = Path(__file__).parent.parent
